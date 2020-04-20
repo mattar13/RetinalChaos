a5c9ecaf9c33785870e5ef204d3da57b82203189
mattar13 <mtarchick13@gmail.com>
2020-04-20 14:13:50 -0400

    Update RetinalChaos.jl

--- src/RetinalChaos.jl
+++ src/RetinalChaos.jl
@@ -18,6 +18,9 @@ include("plotting.jl")
 include("fitting.jl")
 include("wave_extraction.jl")
 
+export read_JSON
+export run_model
+export append_modeldata
 ###### The main simulation loop is here#########################################
 """
 This function contains everything you need to run a single instance of the model,
