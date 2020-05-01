9d16db9039fca5f96ece3d225429fb2459c53a51
mattar13 <mtarchick13@gmail.com>
2020-05-01 14:45:35 -0400

    Update models.jl

--- src/models.jl
+++ src/models.jl
@@ -229,7 +229,7 @@ function (PDE::Network{T, :ρ} where T)(dU, U, p, t)
     nothing
 end
 
-lansdell_pars = [:E_Ca, :E_K, :E_Leak, :E_ACh, :V1, :V2, :V3, :V4, :g_Ca, :g_K, :g_Leak, :g_noise, :g_ACh, :δ, :C_m, :τr, :τs, :τACh, :γ, :α, :β, :κ, :V0, :D]
+lansdell_pars = [:I_app, :E_Ca, :E_K, :E_Leak, :E_ACh, :V1, :V2, :V3, :V4, :g_Ca, :g_K, :g_Leak, :g_noise, :g_ACh, :δ, :C_m, :τr, :τs, :τACh, :γ, :α, :β, :κ, :V0, :D]
 lansdell_conds = [:v, :r, :s, :a, :W]
 
 #This is the Lansdell version of the SAC model
@@ -246,7 +246,7 @@ function (PDE::Network{T, :Lansdell} where T)(dU, U, p, t)
     da = view(dU, :, :, 4)
     dW = view(dU, :, :, 5)
     
-    (E_Ca, E_K, E_leak, E_ACh, V1, V2, V3, V4, g_Ca, g_K, g_leak, g_noise, g_ACh, δ, C_m, τr, τs, τACh, γ, α, β, κ, V0, D) = p
+    (I_app, E_Ca, E_K, E_leak, E_ACh, V1, V2, V3, V4, g_Ca, g_K, g_leak, g_noise, g_ACh, δ, C_m, τr, τs, τACh, γ, α, β, κ, V0, D) = p
 
     @. dv = (
               fI(g_leak,  1.0, v, E_leak)
