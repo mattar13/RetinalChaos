dc6e0424ee33972a2c81d5860254567847285725
mattar13 <mtarchick13@gmail.com>
2020-05-01 13:59:17 -0400

    Update models.jl

--- src/models.jl
+++ src/models.jl
@@ -59,6 +59,48 @@ function diffuse(lattice, D, PDE::Network)
     return lattice + DA
 end
 
+#Version 0: Model without nullout
+function (PDE::Network{T, :Default} where T)(dU, U, p, t)
+    v = view(U, :, :, 1)
+    n = view(U, :, :, 2)
+    c = view(U, :, :, 3)
+    a = view(U, :, :, 4)
+    b = view(U, :, :, 5)
+    ACh = view(U, :, :, 6)
+    W = view(U, :, :, 7)
+
+    dv = view(dU, :, :, 1)
+    dn = view(dU, :, :, 2)
+    dc = view(dU, :, :, 3)
+    da = view(dU, :, :, 4)
+    db = view(dU, :, :, 5)
+    dACh = view(dU, :, :, 6)
+    dW = view(dU, :,:,7)
+
+    (g_leak, E_leak, g_Ca, V1, V2, E_Ca, g_K, E_K, g_TREK, g_ACh, k_d, E_ACh, I_app, C_m, V3, V4, τn, C_0, λ, δ, τc, α, τa, β, τb, ρ, τACh, k, V0, σ, D, τw) = p
+
+    @. dv = (
+              fI(g_leak,  1.0, v, E_leak)
+            + fI(g_Ca, M_INF(v, V1, V2), v, E_Ca)
+            + fI(g_K, n , v, E_K)
+            + fI(g_TREK, b, v, E_K)
+            + fI(g_ACh, ħ(ACh, k_d), v, E_ACh)
+            + I_app
+            + W
+        )/C_m
+
+    @. dn = (Λ(v, V3, V4) * ((N_INF(v, V3, V4) - n)))/τn
+    @. dc = (C_0 + δ*(-g_Ca*M_INF(v, V1, V2)*(v - E_Ca)) - λ*c)/τc
+    @. da =  (α*c^4*(1-a) - a)/τa
+    @. db =  (β*a^4*(1-b) - b)/τb
+    mul!(PDE.MyA, PDE.My, ACh)
+    mul!(PDE.AMx, ACh, PDE.Mx)
+    @. PDE.DA = D*(PDE.MyA + PDE.AMx)
+    @. dACh = PDE.DA + (ρ*Φ(v, k, V0) - ACh)/τACh
+    @. dW = -W/τw
+    nothing
+end
+
 #Version 1: gTREK nullout
 function (PDE::Network{T, :gTREK} where T)(dU, U, p, t)
     v = view(U, :, :, 1)
