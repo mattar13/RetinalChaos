ebda9660dc6d0dc3824b6d6c10742e5fbb1931bc
mattar13 <mtarchick13@gmail.com>
2020-05-18 13:21:22 -0400

    Add files via upload

---
+++ src/2D_models.jl
@@ -0,0 +1,204 @@
+#Version 0: Model without nullout
+function (PDE::Network{T, :Default} where T)(dU, U, p, t)
+    v = view(U, :, :, 1)
+    n = view(U, :, :, 2)
+    c = view(U, :, :, 3)
+    a = view(U, :, :, 4)
+    b = view(U, :, :, 5)
+    ACh = view(U, :, :, 6)
+    W = view(U, :, :, 7)
+
+    dv = view(dU, :, :, 1)
+    dn = view(dU, :, :, 2)
+    dc = view(dU, :, :, 3)
+    da = view(dU, :, :, 4)
+    db = view(dU, :, :, 5)
+    dACh = view(dU, :, :, 6)
+    dW = view(dU, :,:,7)
+
+    (g_leak, E_leak, g_Ca, V1, V2, E_Ca, g_K, E_K, g_TREK, g_ACh, k_d, E_ACh, I_app, C_m, V3, V4, τn, C_0, λ, δ, τc, α, τa, β, τb, ρ, τACh, k, V0, σ, D, τw) = p
+
+    @. dv = (
+              fI(g_leak,  1.0, v, E_leak)
+            + fI(g_Ca, M_INF(v, V1, V2), v, E_Ca)
+            + fI(g_K, n , v, E_K)
+            + fI(g_TREK, b, v, E_K)
+            + fI(g_ACh, ħ(ACh, k_d), v, E_ACh)
+            + I_app
+            + W
+        )/C_m
+
+    @. dn = (Λ(v, V3, V4) * ((N_INF(v, V3, V4) - n)))/τn
+    @. dc = (C_0 + δ*(-g_Ca*M_INF(v, V1, V2)*(v - E_Ca)) - λ*c)/τc
+    @. da =  (α*c^4*(1-a) - a)/τa
+    @. db =  (β*a^4*(1-b) - b)/τb
+    mul!(PDE.MyA, PDE.My, ACh)
+    mul!(PDE.AMx, ACh, PDE.Mx)
+    @. PDE.DA = D*(PDE.MyA + PDE.AMx)
+    @. dACh = PDE.DA + (ρ*Φ(v, k, V0) - ACh)/τACh
+    @. dW = -W/τw
+    nothing
+end
+
+#Version 1: gTREK nullout
+function (PDE::Network{T, :gTREK} where T)(dU, U, p, t)
+    v = view(U, :, :, 1)
+    n = view(U, :, :, 2)
+    c = view(U, :, :, 3)
+    a = view(U, :, :, 4)
+    b = view(U, :, :, 5)
+    ACh = view(U, :, :, 6)
+    W = view(U, :, :, 7)
+
+    dv = view(dU, :, :, 1)
+    dn = view(dU, :, :, 2)
+    dc = view(dU, :, :, 3)
+    da = view(dU, :, :, 4)
+    db = view(dU, :, :, 5)
+    dACh = view(dU, :, :, 6)
+    dW = view(dU, :,:,7)
+
+    (g_leak, E_leak, g_Ca, V1, V2, E_Ca, g_K, E_K, g_TREK, g_ACh, k_d, E_ACh, I_app, C_m, V3, V4, τn, C_0, λ, δ, τc, α, τa, β, τb, ρ, τACh, k, V0, σ, D, τw) = p
+
+    @. dv = (
+              fI(g_leak,  1.0, v, E_leak)
+            + fI(g_Ca, M_INF(v, V1, V2), v, E_Ca)
+            + fI(g_K, n , v, E_K)
+            + fI(g_TREK, b, v, E_K) .* PDE.null
+            + fI(g_ACh, ħ(ACh, k_d), v, E_ACh)
+            + I_app
+            + W
+        )/C_m
+
+    @. dn = (Λ(v, V3, V4) * ((N_INF(v, V3, V4) - n)))/τn
+    @. dc = (C_0 + δ*(-g_Ca*M_INF(v, V1, V2)*(v - E_Ca)) - λ*c)/τc
+    @. da =  (α*c^4*(1-a) - a)/τa
+    @. db =  (β*a^4*(1-b) - b)/τb
+    mul!(PDE.MyA, PDE.My, ACh)
+    mul!(PDE.AMx, ACh, PDE.Mx)
+    @. PDE.DA = D*(PDE.MyA + PDE.AMx)
+    @. dACh = PDE.DA + (ρ*Φ(v, k, V0) - ACh)/τACh
+    @. dW = -W/τw
+    nothing
+end
+
+#Version 2: gACh nullout
+function (PDE::Network{T, :gACh} where T)(dU, U, p, t)
+    v = view(U, :, :, 1)
+    n = view(U, :, :, 2)
+    c = view(U, :, :, 3)
+    a = view(U, :, :, 4)
+    b = view(U, :, :, 5)
+    ACh = view(U, :, :, 6)
+    W = view(U, :, :, 7)
+
+    dv = view(dU, :, :, 1)
+    dn = view(dU, :, :, 2)
+    dc = view(dU, :, :, 3)
+    da = view(dU, :, :, 4)
+    db = view(dU, :, :, 5)
+    dACh = view(dU, :, :, 6)
+    dW = view(dU, :,:,7)
+
+    (g_leak, E_leak, g_Ca, V1, V2, E_Ca, g_K, E_K, g_TREK, g_ACh, k_d, E_ACh, I_app, C_m, V3, V4, τn, C_0, λ, δ, τc, α, τa, β, τb, ρ, τACh, k, V0, σ, D, τw) = p
+
+    @. dv = (
+              fI(g_leak,  1.0, v, E_leak)
+            + fI(g_Ca, M_INF(v, V1, V2), v, E_Ca)
+            + fI(g_K, n , v, E_K)
+            + fI(g_TREK, b, v, E_K)
+            + fI(g_ACh, ħ(ACh, k_d), v, E_ACh) .* PDE.null
+            + I_app
+            + W
+        )/C_m
+
+    @. dn = (Λ(v, V3, V4) * ((N_INF(v, V3, V4) - n)))/τn
+    @. dc = (C_0 + δ*(-g_Ca*M_INF(v, V1, V2)*(v - E_Ca)) - λ*c)/τc
+    @. da =  (α*c^4*(1-a) - a)/τa
+    @. db =  (β*a^4*(1-b) - b)/τb
+    mul!(PDE.MyA, PDE.My, ACh)
+    mul!(PDE.AMx, ACh, PDE.Mx)
+    @. PDE.DA = D*(PDE.MyA + PDE.AMx)
+    @. dACh = PDE.DA + (ρ*Φ(v, k, V0) - ACh)/τACh
+    @. dW = -W/τw
+    nothing
+end
+
+#Version 3 :Acetylcholine release nullout
+function (PDE::Network{T, :ρ} where T)(dU, U, p, t)
+    v = view(U, :, :, 1)
+    n = view(U, :, :, 2)
+    c = view(U, :, :, 3)
+    a = view(U, :, :, 4)
+    b = view(U, :, :, 5)
+    ACh = view(U, :, :, 6)
+    W = view(U, :, :, 7)
+
+    dv = view(dU, :, :, 1)
+    dn = view(dU, :, :, 2)
+    dc = view(dU, :, :, 3)
+    da = view(dU, :, :, 4)
+    db = view(dU, :, :, 5)
+    dACh = view(dU, :, :, 6)
+    dW = view(dU, :,:,7)
+
+    (g_leak, E_leak, g_Ca, V1, V2, E_Ca, g_K, E_K, g_TREK, g_ACh, k_d, E_ACh, I_app, C_m, V3, V4, τn, C_0, λ, δ, τc, α, τa, β, τb, ρ, τACh, k, V0, σ, D, τw) = p
+
+    @. dv = (
+              fI(g_leak,  1.0, v, E_leak)
+            + fI(g_Ca, M_INF(v, V1, V2), v, E_Ca)
+            + fI(g_K, n , v, E_K)
+            + fI(g_TREK, b, v, E_K)
+            + fI(g_ACh, ħ(ACh, k_d), v, E_ACh)
+            + I_app
+            + W
+        )/C_m
+    @. dn = (Λ(v, V3, V4) * ((N_INF(v, V3, V4) - n)))/τn
+    @. dc = (C_0 + δ*(-g_Ca*M_INF(v, V1, V2)*(v - E_Ca)) - λ*c)/τc
+    @. da =  (α*c^4*(1-a) - a)/τa
+    @. db =  (β*a^4*(1-b) - b)/τb
+    mul!(PDE.MyA, PDE.My, ACh)
+    mul!(PDE.AMx, ACh, PDE.Mx)
+    @. PDE.DA = D*(PDE.MyA + PDE.AMx)
+    @. dACh = PDE.DA + (ρ*Φ(v, k, V0)*PDE.null - ACh)/τACh
+    @. dW = -W/τw
+    nothing
+end
+
+lansdell_pars = [:I_app, :E_Ca, :E_K, :E_Leak, :E_ACh, :V1, :V2, :V3, :V4, :g_Ca, :g_K, :g_Leak, :g_noise, :g_ACh, :δ, :C_m, :τr, :τs, :τACh, :γ, :α, :β, :k, :V0, :D]
+lansdell_conds = [:v, :r, :s, :a, :W]
+
+#This is the Lansdell version of the SAC model
+function (PDE::Network{T, :Lansdell} where T)(dU, U, p, t)
+    v = view(U, :, :, 1)
+    r = view(U, :, :, 2)
+    s = view(U, :, :, 3)
+    a = view(U, :, :, 4)
+    W = view(U, :, :, 5)
+
+    dv = view(dU, :, :, 1)
+    dr = view(dU, :, :, 2)
+    ds = view(dU, :, :, 3)
+    da = view(dU, :, :, 4)
+    dW = view(dU, :, :, 5)
+
+    (I_app, E_Ca, E_K, E_leak, E_ACh, V1, V2, V3, V4, g_Ca, g_K, g_leak, g_noise, g_ACh, δ, C_m, τr, τs, τACh, γ, α, β, k, V0, D) = p
+
+    @. dv = (
+              fI(g_leak,  1.0, v, E_leak)
+            + fI(g_Ca, M_INF(v, V1, V2), v, E_Ca)
+            + fI(g_K, r , v, E_K)
+            + fI(g_ACh, h(a, δ), v, E_ACh)
+            + fI(g_noise, abs(W), v, E_Ca)
+            + I_app
+        )/C_m
+
+    @. dr = (Λ(v, V3, V4)*(N_INF(v, V3, V4) - r) + α*s*(1-r))/τr
+    @. ds = (γ*Φ(v, k, V0)-s)/τs
+    mul!(PDE.MyA, PDE.My, a)
+    mul!(PDE.AMx, a, PDE.Mx)
+    @. PDE.DA = D*(PDE.MyA + PDE.AMx)
+    @. da = (PDE.DA + β*Φ(v, k, V0)-a)/τACh
+    @. dW = -W
+    nothing
+end
---
+++ src/pde_models.jl
@@ -0,0 +1,60 @@
+mutable struct Network{A, N} <: Function
+      Mx::Union{Tridiagonal, A}
+      My::Union{Tridiagonal, A}
+      MyA::A
+      AMx::A
+      DA::A
+      null::A
+end
+
+"""
+This is a constructor for the Network object with a version flag option
+"""
+Network(Mx, My, MyA, AMx, DA, null, null_param::Symbol) = Network{typeof(MyA), null_param}(Mx, My, MyA, AMx, DA, null)
+
+
+
+"""
+This constructs the PDE function so that it can be called
+"""
+function Network(nx::Int64, ny::Int64; gpu::Bool = false, μ::Float64 = 0.75, version = :gACh,
+        DX::Tuple{Float64, Float64} = (-2.0, 1.0), DY::Tuple{Float64, Float64} = (-2.0, 1.0))
+    #Set up x diffusion steps
+    x_dv = repeat([DX[1]], nx)
+    x_uv = repeat([DX[2]], nx-2)
+    x_lv = repeat([DX[2]], nx-2)
+    push!(x_uv, abs(DX[1]))
+    pushfirst!(x_lv, abs(DX[1]))
+
+    #Set up y diffusion steps
+    y_dv = repeat([DY[1]], ny)
+    y_uv = repeat([DY[2]], ny-2)
+    y_lv = repeat([DY[2]], ny-2)
+    push!(y_lv, abs(DY[1]))
+    pushfirst!(y_uv, abs(DY[1]))
+    Mx = Tridiagonal(x_lv, x_dv, x_uv)
+    My = Tridiagonal(y_lv, y_dv, y_uv)
+    MyA = zeros(ny, nx);
+    AMx = zeros(ny, nx);
+    DA = zeros(ny, nx);
+    d = Binomial(1, μ)
+    null = Float64.(rand(d, (ny, nx)))
+    if gpu
+        gMx = CuArray(Float32.(Mx))
+        gMy = CuArray(Float32.(Mx))
+        gAMx = CuArray(Float32.(Mx))
+        gMyA = CuArray(Float32.(Mx))
+        gDA = CuArray(Float32.(Mx))
+        gNull = CuArray(Float32.(null))
+        return Network(gMx, gMy, gMyA, gAMx, gDA, gNull, version)
+    else
+        return Network(Mx, My, MyA, AMx, DA, null, version)
+    end
+end
+
+function diffuse(lattice, D, PDE::Network)
+    mul!(PDE.MyA, PDE.My, lattice)
+    mul!(PDE.AMx, lattice, PDE.Mx)
+    DA = D*(PDE.MyA + PDE.AMx)
+    return lattice + DA
+end
