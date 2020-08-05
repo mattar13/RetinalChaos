900d61ba1ffc0c81fb547e0b56a256237c4e1059
mattar13 <mtarchick13@gmail.com>
2020-08-05 12:57:35 -0400

    Update RetinalChaos.jl

--- src/RetinalChaos.jl
+++ src/RetinalChaos.jl
@@ -40,7 +40,7 @@ This function contains everything you need to run a single instance of the model
     and then save the stats and params.
 """
 function simulation_loop(net::Network, u0::Array{Float64,3}, p::Array{Float64, 1}; 
-        root::String = "D:\\ModellingData\\", sim_name::String = "default", 
+        save_sim::Bool = true, root::String = "D:\\ModellingData\\", sim_name::String = "default", 
         wu_time::Float64 = 60e3, 
         tmax = 120e3, dt::Float64 = 1.0, checkpoint::Float64 = 60e3,    #We will run the solution for 300s with a dt of 1ms, checkpointing every 100s
         )
@@ -59,7 +59,9 @@ function simulation_loop(net::Network, u0::Array{Float64,3}, p::Array{Float64, 1
         println("[$(now())]: Saving warmed-up solution")
         last_time = 0.0
         ic = sol[end]
-        JLD2.@save ic_path ic last_time
+        if save_sim
+            JLD2.@save ic_path ic last_time
+        end
     end
     
     is_dataset, data_size = parse_ic(path, sim_path; data_name = "data_size")
@@ -72,7 +74,9 @@ function simulation_loop(net::Network, u0::Array{Float64,3}, p::Array{Float64, 1
         data_size = (size(ic,1), size(ic,2), length(tsteps))
         #We will simulate each step in chunks based on checkpoint
         println("Saving timsetps and data size")
-        JLD2.@save sim_path tsteps data_size
+        if save_sim
+            JLD2.@save sim_path tsteps data_size
+        end
     end
         
     for t = 1.0:checkpoint:tmax
@@ -87,19 +91,20 @@ function simulation_loop(net::Network, u0::Array{Float64,3}, p::Array{Float64, 1
             println("[$(now())]: Simulating timesteps $t -> $tfin -> [dt:$dt]")
             sol = solve(prob, SOSRI(), abstol = 0.2, reltol = 2e-2, maxiters = 1e7, progress = true, saveat = dt)
             println("[$(now())]: Saving data points")
-            jldopen(sim_path, "a+") do file
-                for t in sol.t
-                    #println("Saving timepoint $t")
-                    file["$t"] = sol(t)[:,:,1]
+            if save_sim
+                jldopen(sim_path, "a+") do file
+                    for t in sol.t
+                        #println("Saving timepoint $t")
+                        file["$t"] = sol(t)[:,:,1]
+                    end
+                end
+                println("[$(now())]: Saving final data point to be reused")
+                jldopen(ic_path, "w") do file
+                    file["ic"] = sol[end]
+                    file["last_time"] = tfin
                 end
             end
-            println("[$(now())]: Saving final data point to be reused")
-            jldopen(ic_path, "w") do file
-                file["ic"] = sol[end]
-                file["last_time"] = tfin
-            end
-        end
-        
+        end     
     end
     ic
 end
