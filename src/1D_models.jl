f4db58fe1640ada9377e453f7a51d080798e5df9
mattar13 <mtarchick13@gmail.com>
2020-08-08 11:26:06 -0400

    Update 1D_models.jl

--- src/1D_models.jl
+++ src/1D_models.jl
@@ -6,15 +6,15 @@
 T_model_eqs = [
           d(v)~ (
                     -g_leak*(v-E_leak)
-                  + -g_Ca*M_INF(v, V1, V2)*(v-E_Ca)
+                  + -g_Ca*R_INF(v, V1, V2)*(v-E_Ca)
                   + -g_K*n*(v-E_K)
                   + -g_TREK*b*(v-E_K)
                   + -g_ACh*ħ(e, k_d)*(v-E_ACh)
                   + I_app
                   + W + 0*σ
                   )/C_m ,
-          d(n) ~ (Λ(v, V3, V4) * ((N_INF(v, V3, V4) - n)))/τn,
-          d(c) ~ (C_0 + δ*(-g_Ca*M_INF(v, V1, V2)* (v - E_Ca)) - λ*c)/τc,
+          d(n) ~ (Λ(v, V3, V4) * ((R_INF(v, V3, V4) - n)))/τn,
+          d(c) ~ (C_0 + δ*(-g_Ca*R_INF(v, V1, V2)* (v - E_Ca)) - λ*c)/τc,
           d(a) ~ (α*c^4*(1-a)-a)/τa,
           d(b) ~ (β*a^4*(1-b)-b)/τb,
           d(e) ~ (-2*D*e) + (ρ*Φ(v, k, V0) - e)/τACh,
@@ -27,16 +27,16 @@ T_sde = SDESystem(T_model_eqs, T_noise_eqs, t, T_ode.states, T_ode.ps)
 #Karvourniari Model
 K_model_eqs = [
           d(v) ~ (-g_leak*(v-E_leak)
-                    + -g_Ca*M_INF(v, V1, V2)*(v-E_Ca)
+                    + -g_Ca*R_INF(v, V1, V2)*(v-E_Ca)
                     + -g_K*n*(v-E_K)
                     + -g_sAHP*r^4*(v-E_K)
                     + I_app
                     + W + 0*σ
                     )/C_m,
-          d(n)~ (Λ(v, V3, V4) * ((N_INF(v, V3, V4) - n)))/τn,
+          d(n)~ (Λ(v, V3, V4) * ((R_INF(v, V3, V4) - n)))/τn,
           d(r) ~ (αr*s*(1-r)-r)/τr,
           d(s) ~ (αs*c^4*(1-s)-s)/τs,
-          d(c) ~ (-(αc/H_x)*c + C_0 + δc*(-g_Ca*M_INF(v, V1, V2)*(v-E_Ca)))/τc,
+          d(c) ~ (-(αc/H_x)*c + C_0 + δc*(-g_Ca*R_INF(v, V1, V2)*(v-E_Ca)))/τc,
           d(W) ~ -W
 ]
 K_noise_eqs = [0.0, 0.0, 0.0, 0.0, 0.0, σ]
