9c6047e1ec473f6c037f1d631054f1a2c72e98e6
mattar13 <mtarchick13@gmail.com>
2020-08-09 07:30:33 -0400

    Update models.jl

--- src/models.jl
+++ src/models.jl
@@ -1,15 +1,19 @@
 """
-This is the Boltzmann equation for channel gating. 
+This is the Boltzmann equation for channel gating.  
 Origingally described in Hodgkin & Huxley Et al 1955, then simplified into a hyperbolic form by Morris & Lecar 1980
 
 The equation for this is as follows: 
 
 \$ R\\_{\\infty} = \\frac{1}{2} * (1 + tanh(\\frac{V\\_t - V\\_S}{V\\_H}))\$
+- INPUTS: 
+    - The membrane voltage (\$V_t\$)
+    - The slope of the function (\$V_S\$)
+    - The half activation point of the function (\$V_H\$)
+- OUTPUT: 
+    - The gating variable (\$R_{\\infty}\$)
 
-For Calcium channel activation we can use: \$M\\_{\\infty}\$
-For Potassium channel activation we can use \$N\\_{\\infty}\$
-
-To use this function pass in the voltage variable, The slope (\$V\\_S\$) and half saturation (\$V\\_H\$)
+- For Calcium channel activation: \$V_1 = V_S,  V_2 = V_H,  M_{\\infty} = R_{\\infty}\$
+- For Potassium channel activation: \$V_3 = V_S,  V_4 = V_H,  N_{\\infty} = R_{\\infty}\$
 """
 R_INF(V, VS, VH) = (1 + tanh((V - VS)/VH))/2;
 R_INF(V, VS::Real, VH::Real) = (1 + tanh((V - VS)/VH))/2;
@@ -28,8 +32,20 @@ R_INF(V::CuArray, VS, VH) = R_INF.(V, VS, VH)
 Φ(v::CuArray, κ, V_0) = Φ.(v, κ, V_0)
 ħ(e::CuArray, K_d) = ħ.(e, K_d)
 
-#This is the generalized current calculation function
-fI(g::Float64, r, v, e::Float64) = -g*r*(v-e)
+"""
+This function is used to calculate the current. It represents the generalized form: 
+
+\$I_n = -g_n * R * (V_t - E_n)\$
+\$n = (Leak, Ca, K, TREK, ACh)\$
+- INPUTS: 
+    - Maximal conductance (\$g_n\$) 
+    - Gating fraction (\$R_\\infty\$)
+    - Membrane Potential (\$V_t\$) 
+    - Reversal Potential (\$E_n\$)
+- OUTPUTS:
+    - Ionic current of type n (\$I_n\$)
+"""
+fI(g::Real, r, v, e::Real) = -g*r*(v-e)
 
 tar_pars = [:g_leak, :E_leak, :g_Ca, :V1, :V2, :E_Ca, :g_K, :E_K, :g_TREK, :g_ACh, :k_d, :E_ACh, :I_app, :C_m, :V3, :V4, :τn, :C_0, :λ, :δ, :τc, :α, :τa, :β, :τb, :ρ, :τACh, :k, :V0, :σ, :D, :τw]
 tar_conds = [:v, :n, :c, :a, :b, :e, :W]
