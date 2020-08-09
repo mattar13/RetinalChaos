d6772bbb493710f870c7716290c1744f9b4e4326
mattar13 <mtarchick13@gmail.com>
2020-08-09 07:42:05 -0400

    Update models.jl

--- src/models.jl
+++ src/models.jl
@@ -13,8 +13,6 @@ This function is used to calculate the current. It represents the generalized fo
 """
 fI(g::T, r, v, e::T) where T = -g*r*(v-e)
 
-
-
 """
 This is the Boltzmann equation for channel gating.  
 Origingally described in Hodgkin & Huxley Et al 1955, then simplified into a hyperbolic form by Morris & Lecar 1980
@@ -36,19 +34,16 @@ R_INF(V, VS::T, VH::T) where T = (1 + tanh((V - VS)/VH))/2;
 CuArrays.@cufunc R_INF(V, VS, VH) = (1 + tanh((V - VS)/VH))/2;
 R_INF(V::CuArray, VS, VH) = R_INF.(V, VS, VH)
 
-
 Λ(V, V3, V4) = cosh((V-V3)/(2*V4));
-Φ(v, κ, V_0) = 1/(1 + exp(-κ * (v - V_0)))
-ħ(e, K_d) = (e^2)/(e^2 + K_d)
-
-
 CuArrays.@cufunc Λ(V, V3, V4) = cosh((V-V3)/(2*V4));
-CuArrays.@cufunc Φ(v, κ, V_0) = 1/(1 + exp(-κ * (v - V_0)))
-CuArrays.@cufunc ħ(e, K_d) = (e^2)/(e^2 + K_d)
-
-
 Λ(V::CuArray, V3, V4) = LAM.(V, V3, V4) 
+
+Φ(v, κ, V_0) = 1/(1 + exp(-κ * (v - V_0)))
+CuArrays.@cufunc Φ(v, κ, V_0) = 1/(1 + exp(-κ * (v - V_0)))
 Φ(v::CuArray, κ, V_0) = Φ.(v, κ, V_0)
+
+ħ(e, K_d) = (e^2)/(e^2 + K_d)
+CuArrays.@cufunc ħ(e, K_d) = (e^2)/(e^2 + K_d)
 ħ(e::CuArray, K_d) = ħ.(e, K_d)
 
 tar_pars = [:g_leak, :E_leak, :g_Ca, :V1, :V2, :E_Ca, :g_K, :E_K, :g_TREK, :g_ACh, :k_d, :E_ACh, :I_app, :C_m, :V3, :V4, :τn, :C_0, :λ, :δ, :τc, :α, :τa, :β, :τb, :ρ, :τACh, :k, :V0, :σ, :D, :τw]
@@ -63,8 +58,6 @@ include("pde_models.jl")
 include("1D_models.jl") 
 include("2D_models.jl")    
 
-sym_ps = map(x -> x |> Symbol, T_ode.ps)
-sym_cs = map(x -> x |> Symbol, T_ode.states)
 
 
 #Noise models
