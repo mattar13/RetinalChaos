be78772d790334d5b8d9593c6b7b27edd984f860
Matthew Tarchick <mtarchick13@gmail.com>
2020-10-26 09:55:32 -0400

    Changes Parameters to settings, moved all JSON files to settings

--- Examples/figures.jl
+++ Examples/figures.jl
@@ -8,6 +8,12 @@ font_title = Plots.font("Arial", 24)
 font_axis = Plots.font("Arial", 12)
 font_legend = Plots.font("Arial", 8)
 pyplot(titlefont=font_title, guidefont = font_axis, legendfont = font_legend)
-
+homedir = pwd()
+println(homedir)
 #%% Figure 1
 
+pars_path = joinpath(homedir, "Settings")
+println(conds_path)
+#Read JSON files for initial conditions and parameters
+u0 = read_JSON(join_path(pars_path,"conds.json")) |> extract_dict;
+p_dict = read_JSON(joinpath(pars_path, "params.json")) 
