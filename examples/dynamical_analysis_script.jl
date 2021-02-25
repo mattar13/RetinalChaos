218efdef0d13c01f0a2513270cbbb26bc0ad3d57
Matt Tarchick <mtarchick13@gmail.com>
2021-02-25 12:20:58 -0500

    scripts from data analysis to making figures

--- examples/dynamical_analysis_script.jl
+++ examples/dynamical_analysis_script.jl
@@ -29,18 +29,20 @@ tspan = (0.0, 30e3);
 prob_eq = ODEProblem(T_ode, u0, tspan, p)
 codim1 = (:I_app)
 c1_lims = (-50.0, 20.0)
-c1_map = codim_map(prob_eq, codim1, c1_lims = c1_lims, eq_res = 10)
+#%%
+print("Codimensional analysis time to complete:")
+@time c1_map = codim_map(prob_eq, codim1, c1_lims = c1_lims, eq_res = 10)
+#%%
 plot(c1_map, xlabel = "Injected Current", ylabel = "Membrane Voltage")
 
 #%% Supplemental figure. Analysis of parameter change
-test_rng = range(0.5, 2.0, length = 50) #this ranges from halving the parameter to doubling it
-p_dict = read_JSON(params_file) 
-p_dict[:I_app] = 10.0
+test_rng = range(0.5, 2.0, length = 100) #this ranges from halving the parameter to doubling it
+p = read_JSON(params_file); #Because of my catch, we can keep these as dictionaries 
+u0 = read_JSON(conds_file);
 #We can set the initial voltage sensitivty here 
-p = p_dict |> extract_dict;
-u0 = read_JSON(conds_file) |> extract_dict;
+p[:I_app] = 10.0
 tspan = (0.0, 60e3)
-prob = ODEProblem(T_ode, u0, tspan, p_dict |> extract_dict);
+prob = ODEProblem(T_ode, u0, tspan, p);
 results = zeros(length(T_ode.ps), length(test_rng), 3)
 #Walk through each parameter
 for (par_idx, par) in enumerate(T_ode.ps)
@@ -52,8 +54,9 @@ for (par_idx, par) in enumerate(T_ode.ps)
     
     print("Time it took to simulate $(tspan[2]/1000)s:")
     @time sim = solve(ensemble_prob, EnsembleThreads(), trajectories = length(test_rng)); 
+    println("Simulation for parameter $par completed")
     for (sol_idx, sol) in enumerate(sim)
-        println(sol |> length)
+        #println(sol |> length)
         dt = 0.1 #set the time differential according to supp figure 1
         t_rng = collect(tspan[1]:dt:tspan[2]) #set the time range
         v_t = map(t -> sol(t)[1], t_rng); #extract according to the interval
@@ -62,14 +65,15 @@ for (par_idx, par) in enumerate(T_ode.ps)
             results[par_idx, sol_idx, i] = sum(ts_analysis[i])/length(ts_analysis[i])
         end
     end
+    println("Statistics for $par calculated")
 end
 #%%
 for (idx, p) in enumerate(T_ode.ps)
     println("Plotting results for $p")
     sfig3i = plot(layout = grid(3,1), size = (1000, 800))
-    plot!(sfig3i[1], test_rng.*p_dict[Symbol(p)], results[idx,:,1], label = "$p")
-    plot!(sfig3i[2], test_rng.*p_dict[Symbol(p)], results[idx,:,2]./1000, label = "$p")
-    plot!(sfig3i[3], test_rng.*p_dict[Symbol(p)], results[idx,:,3]./1000, label = "$p")
+    plot!(sfig3i[1], test_rng, results[idx,:,1], label = "$p", xlabel = "Norm Par", ylabel = "Spike Duration")
+    plot!(sfig3i[2], test_rng, results[idx,:,2]./1000, label = "$p", xlabel = "Norm Par", ylabel = "Burst Duration")
+    plot!(sfig3i[3], test_rng, results[idx,:,3]./1000, label = "$p", xlabel = "Norm Par", ylabel = "IBI")
     savefig(sfig3i, joinpath(save_figs, "gradient_for_param_$p.png"))
 end
-sfig3i
+#sfig3i
