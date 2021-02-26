5bee256ae90a0e5ab1b73245ca28c784692b7cae
Matthew Tarchick <mtarchick13@gmail.com>
2021-02-26 12:02:16 -0500

    added figure 2 to making figures

--- examples/making_figures.jl
+++ examples/making_figures.jl
@@ -1,5 +1,6 @@
 #%% Running and analyzing the model using RetinalChaos.jl
 using RetinalChaos
+import RetinalChaos: Φ, diffuse
 using Dates
 using StatsBase, Statistics
 #Setup the fonts and stuff
@@ -83,3 +84,62 @@ fig1_C = plot(sol, vars = [:c, :a, :b, :v], legend = :none,
 )
 
 fig1 = plot(fig1_A, fig1_B, fig1_C, layout = grid(3,1, heights = [0.2, 0.3, 0.5]), size = (1000, 1000))
+
+#%% Figure2 Acetylcholine Propagation dynamics
+p = read_JSON("params.json") 
+u0 = read_JSON("conds.json");
+p[:I_app] = 2.0
+p[:D] = 0.01
+tspan = (0.0, 60e3);
+prob = ODEProblem(T_ode, u0|> extract_dict, tspan, p|> extract_dict)
+println("Time it took to simulate 60s:")
+@time sol = solve(prob, abstol = 2e-2, reltol = 2e-2, maxiters = 1e7); 
+
+plot(sol, vars = [:v, :e], layout = grid(2, 1))
+
+
+v_rng = LinRange(-90.0, 10.0, 100);
+AChi = 0.0
+ρ = p[:ρ]
+k = p[:k]
+println(k)
+V0 = p_dict[:V0]
+τACh = p_dict[:τACh]
+fach(v) = (ρ * Φ(v, k, V0)) - (AChi)/τACh
+ACh_rng = map(fach, v_rng);
+
+xlims = (38e3, 42e3) 
+xticks = (collect(xlims[1]:500:xlims[2]), collect(0:0.5:round(Int, tspan[end]/1000)))
+
+fig2_Aa = plot(sol, 
+    vars = [:v, :e], 
+    ylabel = ["\$V_t\$ (mV)" "\$E_t\$ (mM)"], 
+    xlabel = ["" "time (s)"], 
+    c = [v_color ach_color], lw = 2.0,
+    legend = :none, 
+    layout = grid(2, 1), 
+    xlims = xlims, 
+    xticks = xticks
+)
+frame_stops = [1.5e3, 2.0e3, 2.5e3, 3.0e3]
+frame_stops .+= xlims[1]
+ach_stops = map(t->sol(t)[6], frame_stops)
+plot!(fig2_Aa[2], frame_stops, ach_stops, label = "Frame stops",
+    seriestype = :scatter, marker = :star, markersize = 10.0)
+
+fig2_Ab = plot(v_rng, ACh_rng, 
+    c = ach_color, linestyle = :dot, lw = 4.0, 
+    title = "Acetylcholine Release", titlefontsize = 12.0,
+    xlabel = "\$V_t\$ (mV)",ylabel = "Inst. \$E_t\$ (mM/s)", label = "")
+
+fig2_A = plot(fig2_Aa, fig2_Ab, 
+    layout = grid(1, 2, widths = [0.75, 0.25]), size = (700, 350))
+title!(fig2_A[1], "A", titlepos = :left)
+
+nx, ny = (50, 50)
+c1x, c1y = (round(Int, nx/2), round(Int, ny/4))
+c2x, c2y = (round(Int, nx/2), round(Int, ny/4*3))
+D = p_dict[:D]
+lattice = zeros(nx, ny)
+lattice[c1x, c1y] = 0.6
+bp_model = Network(nx, ny)
