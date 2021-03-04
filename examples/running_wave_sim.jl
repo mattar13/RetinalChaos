679c86e041998b70eebdf41cc25840ef5a508de7
Matt Tarchick <mtarchick13@gmail.com>
2021-03-04 13:51:18 -0500

    added a wave simulation

--- examples/running_wave_sim.jl
+++ examples/running_wave_sim.jl
@@ -13,15 +13,15 @@ p[:g_K] = 5.0
 p = extract_dict(p, tar_pars);
 u0_network = extract_dict(read_JSON(conds_file), tar_conds, (nx, ny));
 nx = 96; ny = 96
-net = Network(nx, ny; μ = 0.15, version = :Sparse_Activity) #μ is the probability a cell is capable of being active
+net = Network(nx, ny; μ = 0.15) #μ is the probability a cell is capable of being active
 var = 1 #This is the variable we are interested in
 save_idxs = [var*1:var*(nx*ny)...] #This is a list of indexes of all the variables we want to plot
-PDEprob = SDEProblem(net, noise, u0_network, (0.0, 60e3), p)
+PDEprob = SDEProblem(net, noise, u0_network, (0.0, 300e3), p)
 #%% This may take a long time to run
 @time PDEsol = solve(PDEprob, SOSRI(), save_idxs = save_idxs, progress = true, progress_steps = 1)
 
 #%% Plot as a .gif
-t_rng = collect(0.0:500.0:60e3)
+t_rng = collect(10e3:500.0:PDEsol.t[end])
 anim = @animate for t = t_rng
     println("Animating frame $t")
     frame_i = reshape(PDEsol(t), (nx, ny))
@@ -31,4 +31,25 @@ anim = @animate for t = t_rng
     )
 end
 #%%
-gif(anim, "examples\\plot.gif", fps = 20)
+gif(anim, "examples\\g_ACh_Sparse_plot.gif", fps = 20)
+
+#%% Make one knocking out ChAT in x percentage of cells
+net = Network(nx, ny; μ = 0.15, version = :ρ) #μ is the probability a cell is capable of being active
+var = 1 #This is the variable we are interested in
+save_idxs = [var*1:var*(nx*ny)...] #This is a list of indexes of all the variables we want to plot
+PDEprob = SDEProblem(net, noise, u0_network, (0.0, 300e3), p)
+#%% This may take a long time to run
+@time PDEsol = solve(PDEprob, SOSRI(), save_idxs = save_idxs, progress = true, progress_steps = 1)
+
+#%% Plot as a .gif
+t_rng = collect(10e3:500.0:PDEsol.t[end])
+anim = @animate for t = t_rng
+    println("Animating frame $t")
+    frame_i = reshape(PDEsol(t), (nx, ny))
+    heatmap(frame_i, ratio = :equal, grid = false,
+            xaxis = "", yaxis = "", xlims = (0, nx), ylims = (0, ny),
+            c = :curl, clims = (-70.0, 0.0),
+    )
+end
+#%%
+gif(anim, "examples\\rho_Sparse_plot.gif", fps = 20)
