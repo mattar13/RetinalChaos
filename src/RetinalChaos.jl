5f42f53bc84711d3627453b4e0577436391a31fb
Matthew Tarchick <mtarchick13@gmail.com>
2021-03-07 23:33:12 -0500

    added a few plotting elements

--- src/RetinalChaos.jl
+++ src/RetinalChaos.jl
@@ -68,7 +68,7 @@ export pyplot, font, Measures
 
 export plot, plot!, grid, stroke, @animate #Out of the box, I want to be able to plot
 export title!, heatmap!, scatter!, annotate!, savefig
-export heatmap, gif
+export heatmap, contourf, contour!, gif, mov
 #Import some other plotting utilities
 using Colors, ColorSchemes, LaTeXStrings, StatsPlots, Dates
 export colormatch, colormap, colorschemes
