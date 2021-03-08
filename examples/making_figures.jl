0422fe809d42c7afaeb051ee6e50cf77312d213c
Matthew Tarchick <mtarchick13@gmail.com>
2021-03-07 23:33:35 -0500

    adding and updating plotting figures

--- examples/making_figures.jl
+++ examples/making_figures.jl
@@ -58,17 +58,19 @@ fig1_A = plot(sol, vars = [:v, :n,], legend = :none, plotdensity = Int(100e3),
     layout = grid(2, 1),grid = false,
     xlims = xlims, xticks = xticks
 )
+title!(fig1_A[1], "A", titlepos = :left)
 
 xlims = (burst_idxs[2][1]-1500, burst_idxs[2][2]+1500)
 elapsed_time = xlims[2] - xlims[1]
 dx_lims = 500
 xticks = (collect(xlims[1]:dx_lims:xlims[2]), (collect(0:dx_lims/1000:elapsed_time/1000)))
-fig1_B = plot(sol, vars = [:v, :c], legend = :none, 
+fig1_B = plot(sol, vars = [:v, :c], legend = :none, plotdensity = Int64(100e3),
     ylabel = ["\$V_t\$ (mV)" "\$C_t\$"], xlabel = ["" "time (s)"], 
     lw = 2.0, c = [v_color c_color], 
     layout = grid(2, 1),grid = false,
     xlims = xlims, xticks = xticks
 )
+title!(fig1_B[1], "B", titlepos = :left)
 
 dx_lims = 5e3
 xticks = (
@@ -76,91 +78,100 @@ xticks = (
     collect(sol.t[1]/1000:dx_lims/1000:sol.t[end]/1000)
     )
 fig1_C = plot(sol, vars = [:c, :a, :b, :v], legend = :none, 
-    ylabel = ["\$V_t\$ (mV)" "\$C_t\$"], xlabel = ["" "" "" "time (s)"], 
+    ylabel = ["\$C_t\$" "\$A_t\$" "\$B_t\$" "\$V_t\$"], xlabel = ["" "" "" "time (s)"], 
     lw = 2.0, c = [c_color a_color b_color v_color],
     layout = grid(4, 1),grid = false,
     xticks = xticks 
 )
+title!(fig1_C[1], "C", titlepos = :left)
 
-fig1 = plot(fig1_A, fig1_B, fig1_C, layout = grid(3,1, heights = [0.2, 0.3, 0.5]), size = (1000, 1000))
+fig1 = plot(fig1_A, fig1_B, fig1_C, layout = grid(3,1, heights = [0.2, 0.3, 0.5]), size = (1000, 1000), grid = false)
+#savefig(fig1, joinpath(save_figs, "Fig1_Model_Dynamics.png"))
 
 #%% Figure2 Acetylcholine Propagation dynamics
 p = read_JSON(params_file) 
 u0 = read_JSON(conds_file);
-p[:I_app] = 2.0
+p[:I_app] = 10.0
 p[:D] = 0.01
 tspan = (0.0, 60e3);
 prob = ODEProblem(T_ode, u0|> extract_dict, tspan, p|> extract_dict)
 println("Time it took to simulate 60s:")
 @time sol = solve(prob, abstol = 2e-2, reltol = 2e-2, maxiters = 1e7); 
+# Run an anlysis and extract the bursts
+dt = 0.1 #set the time differential
+t_rng = collect(tspan[1]:dt:tspan[2]) #set the time range
+v_t = map(t -> sol(t)[1], t_rng); #extract according to the interval
+sim_thresh = calculate_threshold(v_t)
+spike_array = (v_t .> sim_thresh)
+burst_idxs, dur_list, spb_list, ibi_list = max_interval_algorithim(spike_array, dt = dt);
+burst_lims = burst_idxs[2]
+# Define where the frame stops are placed
 
-plot(sol, vars = [:v, :e], layout = grid(2, 1))
+# Figure 2 A
+dx_lims = 2000.0
+xlims = (burst_lims[1]-2000, burst_lims[2]+2000)
+elapsed_time = xlims[2]-xlims[1]
+xticks = (collect(xlims[1]:dx_lims:xlims[2]), (collect(0:dx_lims/1000:elapsed_time/1000)))
 
-
-v_rng = LinRange(-90.0, 10.0, 100);
-AChi = 0.0
-ρ = p[:ρ]; k = p[:k]
-V0 = p[:V0]; τACh = p[:τACh]
-fach(v) = (ρ * Φ(v, k, V0)) - (AChi)/τACh
-ACh_rng = map(fach, v_rng);
-
-xlims = (38e3, 42e3) 
-xticks = (collect(xlims[1]:500:xlims[2]), collect(0:0.5:round(Int, tspan[end]/1000)))
-
-fig2_Aa = plot(sol, 
-    vars = [:v, :e], 
-    ylabel = ["\$V_t\$ (mV)" "\$E_t\$ (mM)"], 
-    xlabel = ["" "time (s)"], 
-    c = [v_color a_color], lw = 2.0,
-    legend = :none, 
-    layout = grid(2, 1), 
-    xlims = xlims, 
-    xticks = xticks
-)
-frame_stops = [1.5e3, 2.0e3, 2.5e3, 3.0e3]
-frame_stops .+= xlims[1]
+#This is for drawing the diffusion frames
+frame_stops = LinRange(burst_lims[1], burst_lims[2], 3)
 ach_stops = map(t->sol(t)[6], frame_stops)
+
+fig2_Aa = plot(sol, vars = [:v, :e], layout = grid(2, 1), plotdensity = Int64(200e3),
+    c = [v_color e_color], legend = false, 
+    xlabel = ["" "Time (s)"], ylabel = ["\$V_t\$ (mV)" "\$E_t\$"], 
+    xlims = xlims, xticks = xticks
+)
+#Plot the frame stops
 plot!(fig2_Aa[2], frame_stops, ach_stops, label = "Frame stops",
     seriestype = :scatter, marker = :star, markersize = 10.0)
-
-fig2_Ab = plot(v_rng, ACh_rng, 
-    c = a_color, linestyle = :dot, lw = 4.0, 
-    title = "Acetylcholine Release", titlefontsize = 12.0,
-    xlabel = "\$V_t\$ (mV)",ylabel = "Inst. \$E_t\$ (mM/s)", label = "")
-
-fig2_A = plot(fig2_Aa, fig2_Ab, 
-    layout = grid(1, 2, widths = [0.75, 0.25]), size = (700, 350))
+fig2_Ab = plot(v -> Φ(v, p[:k], p[:V0]), -90, 10, legend = false, xlabel = "\$V_t\$", ylabel = "\$\\Phi\$ (Normalized ACh release)")
+fig2_A = plot(fig2_Aa, fig2_Ab, layout = grid(1,2))
 title!(fig2_A[1], "A", titlepos = :left)
+#%%
 
 nx, ny = (50, 50)
 c1x, c1y = (round(Int, nx/2), round(Int, ny/4))
 c2x, c2y = (round(Int, nx/2), round(Int, ny/4*3))
-D = p[:D]
+D = p[:D] #this is the dX per dt
 lattice = zeros(nx, ny)
 lattice[c1x, c1y] = 0.6
 bp_model = Network(nx, ny)
 
-time_range = collect(0:(xlims[end]-xlims[1]))
-println(length(time_range))
+dt = 1.0
+time_range = collect(xlims[1]:dt:xlims[2])
 lattice_c = zeros(nx, ny, length(time_range))
 for (idx, t) in enumerate(time_range)
-    #Each step, the cell releases 0.005 ACh
     if idx == 1
-        lattice_c[c1x, c1y, idx] = sol(t+xlims[1])[6]
+        lattice_c[c1x, c1y, idx] = sol(t)[6]
     else
+        println(sol(t)[6])
         lattice_c[:, :, idx] = diffuse(lattice_c[:,:,idx-1], D, bp_model)
-        lattice_c[c1x, c1y, idx] = sol(t+xlims[1])[6]
+        lattice_c[c1x, c1y, idx] = sol(t)[6]
     end
 end
-
+println(length(time_range))
+#%% This is a supplemental gif for what diffusion looks like
+d_frame = 17
+anim = @animate for fr = 1:d_frame:length(time_range)
+    println("Animating frame $fr")
+    frame_i = lattice_c[:,:, fr]
+    heatmap(frame_i, ratio = :equal, grid = false,
+            xaxis = "", yaxis = "", xlims = (0, nx), ylims = (0, ny),
+            c = :jet, clims = (0.0, maximum(lattice_c)), 
+    )
+    #contour!(frame_i)
+end
+gif(anim, "figures\\ACh_Release.gif", fps = 1000/d_frame)
+#%%
 fig2_B = plot(xlims = (0, nx), ylims = (0, ny), layout = grid(1, length(frame_stops)), size = (1000, 250), 
     #margin = 1mm
 )
 upper_lim = 0.08
 for (idx, frame) in enumerate(frame_stops)
-    frame_idx = Int(frame-xlims[1])
-    #p = plot(xlims = (0.1, nx), ylims = (0.1, ny), 
-    #    xaxis = false, yaxis = false)
+    frame_idx = round(Int, frame-xlims[1])
+    time = round((time_range[frame_idx+1] - frame_stops[1])/1000, digits = 1)
+    println(time)
     if idx == length(frame_stops)
         heatmap!(fig2_B[idx], lattice_c[:,:,frame_idx], c = :thermal, clims = (0.0, upper_lim), 
             aspect_ratio = :equal, grid = false,
@@ -170,7 +181,7 @@ for (idx, frame) in enumerate(frame_stops)
         scatter!(fig2_B[idx], [c1y], [c1x], marker = :circle, c = :cyan, markersize = 10.0, label = "")
         scatter!(fig2_B[idx], [c2y], [c2x], marker = :hexagon, m = (125.0, :transparent, stroke(3.0, :red)), label = "")
         scatter!(fig2_B[idx], [c2y], [c2x], marker = :circle, c = :red, markersize = 10.0, label = "")
-        annotate!(fig2_B[idx], [40], [3], "t = $(time_range[frame_idx+1]/1000)s", :white)
+        annotate!(fig2_B[idx], [40], [3], "t = $(time) ms", :white)
     else
         heatmap!(fig2_B[idx], lattice_c[:,:,frame_idx], c = :thermal, clims = (0.0, upper_lim), 
             aspect_ratio = :equal, grid = false, colorbar = false, 
@@ -180,16 +191,14 @@ for (idx, frame) in enumerate(frame_stops)
         scatter!(fig2_B[idx], [c1y], [c1x], marker = :circle, c = :cyan, markersize = 10.0, label = "")
         scatter!(fig2_B[idx], [c2y], [c2x], marker = :hexagon, m = (125.0, :transparent, stroke(3.0, :red)), label = "")
         scatter!(fig2_B[idx], [c2y], [c2x], marker = :circle, c = :red, markersize = 10.0, label = "")
-        annotate!(fig2_B[idx], [40], [3], "t=$(time_range[frame_idx+1]/1000)s", :white)
+        annotate!(fig2_B[idx], [40], [3], "t=$(time) ms", :white)
     end
 end
-fig2_B
-title!(fig2_B[1], "B", title_pos = :left);
+title!(fig2_B[1], "B", title_pos = :left)
 
-ach_rng = LinRange(0.001, 10e4, 100)
-k_d = p[:k_d]; g_ACh = p[:g_ACh]; E_ACh = p[:E_ACh]
-i_rng = map(a -> ħ(a, k_d), ach_rng);
-
+#%%
+plot(a -> ħ(a, p[:k_d]), 0.001, 6.0)
+#%%
 ach_external = lattice_c[c1x, c1y+1, :];
 i_synaptic = map(a ->ħ(a, k_d), ach_external);
 
