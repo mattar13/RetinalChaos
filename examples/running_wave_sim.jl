ddbe3e54af86f020df9d2da5bf0770247e84700b
Matt Tarchick <mtarchick13@gmail.com>
2021-03-16 16:40:18 -0400

    adjusted for running gACh nullout

--- examples/running_wave_sim.jl
+++ examples/running_wave_sim.jl
@@ -11,7 +11,7 @@ nx = 125; ny = 125
 p = read_JSON(params_file);
 p = extract_dict(p, tar_pars);
 u0_network = extract_dict(read_JSON(conds_file), tar_conds, (nx, ny));
-net = Network(nx, ny; μ = 0.40, version = :gACh) #μ is the probability a cell is capable of being active
+net = Network(nx, ny; μ = 0.50, version = :gACh) #μ is the probability a cell is capable of being active
 var = 1 #This is the variable we are interested in
 save_idxs = [var*1:var*(nx*ny)...] #This is a list of indexes of all the variables we want to plot
 #%% Lets warm up the solution first
@@ -30,15 +30,9 @@ PDEprob = SDEProblem(net, noise, ui_network, (0.0, 120e3), p)
     abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
     save_idxs = save_idxs, progress = true, progress_steps = 1
     )
-#%%
-plt = plot()
-for i in 1:20
-    println(i)
-    plot!(plt, PDEsol.t, PDEsol[i,:])
-end 
-plt
 #%% Plot as a .gif
-t_rng = collect(1.0:50.0:PDEsol.t[end])
+dFrame = 50.0
+t_rng = collect(1.0:dFrame:PDEsol.t[end])
 anim = @animate for t = t_rng
     println("Animating frame $t")
     frame_i = reshape(PDEsol(t), (nx, ny))
@@ -47,7 +41,11 @@ anim = @animate for t = t_rng
             c = :curl, clims = (-70.0, 0.0),
     )
 end
+gif(anim, "examples\\gACh_null_warmed_up2.gif", fps = 20)
 #%%
-gif(anim, "examples\\gHCN_warmed_up2.gif", fps = 20)
-#%%
-contour(net.null)
+plt = plot()
+for i in 1:20
+    println(i)
+    plot!(plt, PDEsol.t, PDEsol[i,:])
+end 
+plt
