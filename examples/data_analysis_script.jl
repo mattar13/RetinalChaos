432eda9831cfb0f4bbdb4cfe3f082a0ced409896
Matthew Tarchick <mtarchick13@gmail.com>
2021-03-21 12:05:31 -0400

    removed HCN analysis

--- examples/data_analysis_script.jl
+++ examples/data_analysis_script.jl
@@ -22,38 +22,6 @@ if isdir(save_figs) == false
 end
 println("Script settings loaded")
 
-#%% Running a comparison between HCN+ cell and HCN- cell
-p_HCNp = read_JSON(params_file)
-p_HCNp[:g_HCN] = 0.75
-p_HCNp[:g_ACh] = 0.0
-p_HCNp[:τa] = 15e3
-p_HCNp[:τb] = 15e3
-
-p_HCNn = read_JSON(params_file)
-p_HCNn[:g_HCN] = 0.0 
-p_HCNn[:g_ACh] = 0.0
-p_HCNn[:τa] = 15e3
-p_HCNn[:τb] = 15e3
-
-u0 = read_JSON(conds_file); tspan = (0.0, 120e3)
-HCNp_prob = SDEProblem(T_sde, u0|>extract_dict, tspan, p_HCNp|>extract_dict);
-HCNn_prob = SDEProblem(T_sde, u0|>extract_dict, tspan, p_HCNn|>extract_dict);
-@time HCNp_sol = solve(HCNp_prob, SOSRI(), abstol = 2e-2, reltol = 2e-2, maxiters = 1e7, progress = true);
-@time HCNn_sol = solve(HCNn_prob, SOSRI(), abstol = 2e-2, reltol = 2e-2, maxiters = 1e7, progress = true);
-plot(HCNp_sol, vars = [:v], label = "HCN+", lw = 1.0, 
-    xlabel = "Time (s)", ylabel = "Voltage (mV)",
-    yticks = collect(-80:10:0),
-    xticks = (collect(0:10e3:tspan[end]), Int64.(collect(0:10:tspan[end]/1000)))
-)
-plot!(HCNn_sol, vars = [:v], label = "HCN-", lw = 1.0, grid = false,
-    xlabel = "Time (s)", ylabel = "Voltage (mV)",
-    yticks = collect(-80:10:0),
-    xticks = (collect(0:10e3:tspan[end]), Int64.(collect(0:10:tspan[end]/1000)))
-)
-savefig("figures\\HCN_Activation.png")
-
-
-
 #%% This supplemental figure compares the dt to the analysis accuracy
 dt_rng = range(0.005, 0.10, length = 50)
 spike_durs = Float64[]; spike_dur_stds = Float64[]
