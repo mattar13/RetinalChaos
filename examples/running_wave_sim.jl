6d97bde71be31018ac8ec548c2fc271242db54d0
Matt Tarchick <mtarchick13@gmail.com>
2021-03-25 10:40:17 -0400

    edited running_wave_sim

--- examples/running_wave_sim.jl
+++ examples/running_wave_sim.jl
@@ -5,17 +5,11 @@ params_file = joinpath(param_root, "params.json")
 conds_file = joinpath(param_root, "conds.json")
 save_path = "C:\\Users\\RennaLabSA1\\Documents\\modelling"
 
-open(joinpath(save_path, "log.txt"), "w") do logf
-    
-println(logf, "[$(Dates.now())]: File set up")
-#%%
-println(logf, "[$(Dates.now())]: Beginning simulion")
 println("[$(Dates.now())]: Beginning simulion")
-for sig in range(0.01, 0.25, length = 10), ga in range(0.3, 2.0, length = 10)
-
-    println(logf, "[$(Dates.now())]:Running test for gACh = $(ga) sigma = $(sig)")
+ga = 1.1
+for sig in range(0.01, 1.0, length = 10)#, ga in range(0.1, 1.1, length = 2)
     println("[$(Dates.now())]:Running test for gACh = $(ga) sigma = $(sig)")
-    nx = ny = 125; 
+    nx = ny = 64; 
     p = read_JSON(params_file);
     p[:σ] = sig
     p[:g_ACh] = ga
@@ -25,9 +19,8 @@ for sig in range(0.01, 0.25, length = 10), ga in range(0.3, 2.0, length = 10)
     var = 1 #This is the variable we are interested in
     save_idxs = [var*1:var*(nx*ny)...] #This is a list of indexes of all the variables we want to plot
 
-    #%% Lets warm up the solution first
-    PDEprob = SDEProblem(net, noise, u0_network, (0.0, 10e3), p)
-    print(logf, "[$(Dates.now())]: Warming up solution:")
+    # Lets warm up the solution first
+    PDEprob = SDEProblem(net, noise, u0_network, (0.0, 60e3), p)
     print("[$(Dates.now())]: Warming up solution:")
     @time PDEsol = solve(PDEprob, SOSRI(), 
         abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
@@ -35,19 +28,17 @@ for sig in range(0.01, 0.25, length = 10), ga in range(0.3, 2.0, length = 10)
         )
     # extract the last solution and use it as the initial condition for future models 
     # This may take a long time to run
-    print(logf, "[$(Dates.now())]: Running model:")
     print("[$(Dates.now())]: Running model:")
     ui_network = PDEsol[end]
-    PDEprob = SDEProblem(net, noise, ui_network, (0.0, 300e3), p)
+    PDEprob = SDEProblem(net, noise, ui_network, (0.0, 120e3), p)
     @time PDEsol = solve(PDEprob, SOSRI(), 
         abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
         save_idxs = save_idxs, progress = true, progress_steps = 1
         )
-    #%% Plot as a .gif
-    save_wave = joinpath(save_path, "sig_$(sig)_gACh_$(ga)_wave_plot.gif")
-    save_plot = joinpath(save_path, "sig_$(sig)_gACh_$(ga)_2D_plot.gif")
+    # Plot as a .gif
+    save_wave = joinpath(save_path, "sig_$(replace(string(sig), "." => "_"))_gACh_$(replace(string(ga), "." => "_"))_wave_plot.gif")
+    save_plot = joinpath(save_path, "sig_$(replace(string(sig), "." => "_"))_gACh_$(replace(string(ga), "." => "_"))_2D_plot.png")
     
-    println(logf, "[$(Dates.now())]: Plotting results")
     println("[$(Dates.now())]: Plotting results")
     dFrame = 50.0
     t_rng = collect(1.0:dFrame:PDEsol.t[end])
@@ -61,12 +52,11 @@ for sig in range(0.01, 0.25, length = 10), ga in range(0.3, 2.0, length = 10)
     end
     gif(anim, save_wave, fps = 20)
 
-    #%%
+    #
     plt = plot()
     for i in 1:20
         println(i)
         plot!(plt, PDEsol.t, PDEsol[i,:])
     end 
     savefig(plt, save_plot)
-end
 end
