abfefb6f1a8ff1bf0c91a00c7df941eb84f19759
Matthew Tarchick <mtarchick13@gmail.com>
2021-04-10 12:20:22 -0400

    added Pluto to make sigures

---
+++ figures/making_figure1_PLUTO.jl
@@ -0,0 +1,21 @@
+### A Pluto.jl notebook ###
+# v0.14.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 1f3a5020-9a18-11eb-3b98-2fa7c5574805
+using RetinalChaos
+
+# ╔═╡ f5cfafe2-c46e-4d06-bac1-6c52ef6e9dce
+md"
+Set up the plotting utilities, fonts and backend
+"
+
+# ╔═╡ 80ce4218-d0b0-4fa6-9784-74fb9b68bc4e
+
+
+# ╔═╡ Cell order:
+# ╠═1f3a5020-9a18-11eb-3b98-2fa7c5574805
+# ╠═f5cfafe2-c46e-4d06-bac1-6c52ef6e9dce
+# ╠═80ce4218-d0b0-4fa6-9784-74fb9b68bc4e
