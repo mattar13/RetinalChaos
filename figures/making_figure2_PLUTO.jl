ccd2a8e0a0a55414781e6c7f8e53f7fe4a51fe3e
Matthew Tarchick <mtarchick13@gmail.com>
2021-04-10 14:11:28 -0400

    made PLUTO script for figure 2

---
+++ figures/making_figure2_PLUTO.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.14.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ fb0b35b0-9a27-11eb-1925-338bb9e3754f
+
+
+# ╔═╡ Cell order:
+# ╠═fb0b35b0-9a27-11eb-1925-338bb9e3754f
