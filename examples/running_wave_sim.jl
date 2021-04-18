3c8eab12d72c0136a356438fe1c0f1f6622f497d
Matthew Tarchick <mtarchick13@gmail.com>
2021-04-18 14:15:34 -0400

    added a wave sim

--- examples/running_wave_sim.jl
+++ examples/running_wave_sim.jl
@@ -3,8 +3,47 @@ using Dates
 param_root = "params\\"
 params_file = joinpath(param_root, "params.json")
 conds_file = joinpath(param_root, "conds.json")
-save_path = "C:\\Users\\RennaLabSA1\\Documents\\modelling"
 
+#%%
+nx = ny = 64; 
+p = read_JSON(params_file);
+p = extract_dict(p, tar_pars);
+u0_network = extract_dict(read_JSON(conds_file), tar_conds, (nx, ny));
+net = Network(nx, ny; μ = 0.15, version = :ρ) #μ is the probability a cell is capable of being active
+var = 1 #This is the variable we are interested in
+save_idxs = [var*1:var*(nx*ny)...] #This is a list of indexes of all the variables we want to plot
+
+# Lets warm up the solution first
+PDEprob = SDEProblem(net, noise, u0_network, (0.0, 60e3), p)
+print("[$(Dates.now())]: Warming up solution:")
+@time PDEsol = solve(PDEprob, SOSRI(), 
+    abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
+    save_everystep = false, progress = true, progress_steps = 1
+    )
+
+#Run the simulation
+ui_network = PDEsol[end]
+PDEprob = SDEProblem(net, noise, ui_network, (0.0, 60e3), p)
+@time PDEsol = solve(PDEprob, SOSRI(), 
+    abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
+    save_idxs = save_idxs, progress = true, progress_steps = 1
+    )
+#%% Plotting stuff
+anim = @animate for t = 1.0:50.0:PDEsol.t[end]
+    println("Animating frame $t")
+    frame_i = reshape(PDEsol(t), (nx, ny))
+    heatmap(frame_i, ratio = :equal, grid = false,
+            xaxis = "", yaxis = "", xlims = (0, nx), ylims = (0, ny),
+            c = :curl, clims = (-70.0, 0.0),
+    )
+end
+gif(anim, save_wave, fps = 20)
+
+
+
+
+#%% Running a multiple sim on the Lab computer
+save_path = "C:\\Users\\RennaLabSA1\\Documents\\modelling"
 println("[$(Dates.now())]: Beginning simulion")
 ga = 1.1
 sig = 0.25
