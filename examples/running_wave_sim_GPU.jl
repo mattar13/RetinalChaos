be1cd073c59b337403c196fa61b891accae1913c
mattar13 <mtarchick13@gmail.com>
2021-07-07 16:13:38 -0400

    updated running wave sim GPU

--- examples/running_wave_sim_GPU.jl
+++ examples/running_wave_sim_GPU.jl
@@ -11,31 +11,29 @@ global_logger(TerminalLogger())
 dotenv("D:\\TelegramAccessEnv\\.env")
 #Activate the GPU
 RetinalChaos.CUDA.allowscalar(false)
+save_file = "C:\\Users\\RennaLabSA1\\Documents\\ModellingData\\$(Date(Dates.now()))_test_mu_18p\\"
+params_file = "params\\params.json"
+conds_file = "params\\conds.json"
 
-#%% Setup the network simulation (Need to do this if accessing the saved file)
+#%% Setup and warmup the simulation
 print("[$(Dates.now())]: Setting up the model...")
 #Save the model, params, conditions, animations here
-param_root = "params\\"
-params_file = joinpath(param_root, "params.json")
-conds_file = joinpath(param_root, "conds.json")
-save_file = "C:\\Users\\RennaLabSA1\\Documents\\ModellingData\\$(Date(Dates.now()))_test_mu_18p\\"
 if isdir(save_file) == false
     #The directory does not exist, we have to make it 
     mkdir(save_file)
 end
-p = read_JSON(params_file) 
-u0 = read_JSON(conds_file);
+p = read_JSON(params_file, is_type = Dict{Symbol, Float32}) 
+u0 = read_JSON(conds_file, is_type = Dict{Symbol, Float32});
 #Save the params and ics to load later. 
 write_JSON(p, "$(save_file)\\params.json") #write the parameters to save for later
 write_JSON(u0, "$(save_file)\\conds.json") #write the ics to save for later
-net = Network(p[:nx]|>Int64, p[:ny]|>Int64; μ = p[:μ], version = :gACh, gpu = true) #This branch uses GPU mode
-p_net = extract_dict(p)# |> cu;
-u0_net = extract_dict(u0, p[:nx]|>Int64, p[:ny]|>Int64) |> cu;
-warmup = (0.0, p[:t_warm])
-tspan  = (0.0, p[:t_run])
+net = Network(p[:nx], p[:ny]; μ = p[:μ], version = :gACh, gpu = true) #This branch uses GPU mode
+p_net = extract_dict(p);
+u0_net = extract_dict(u0, p[:nx], p[:ny]) |> cu;
+warmup = (0f0, p[:t_warm])
+tspan  = (0f0, p[:t_run])
 NetProb = SDEProblem(net, noise, u0_net, warmup, p_net)
 println("Completed")
-BotNotify("{Waves} Model set up complete")
 
 #%% Lets warm up the solution first (using GPU if available)
 print("[$(Dates.now())]: Warming up the solution: ")
@@ -45,25 +43,28 @@ print("[$(Dates.now())]: Warming up the solution: ")
         save_everystep = false
     )
 warmup_ics = NetSol[end]
+JLD2.@save "$(save_file)\\warmup_ics.jld2" warmup_ics
 println("Completed")
 BotNotify("{Waves} Model warmup complete")
 
-#%% Save or load the warmed up solution
-print("[$(Dates.now())]: Loading or saving solution...")
-JLD2.@save "$(save_file)\\warmup_ics.jld2" warmup_ics
-println("Completed")
+#put this statement to clear out all of the memory
+NetSol = nothing; GC.gc(true); CuArrays.reclaim()
 
+#%% Make the new problem
+NetProb = SDEProblem(net, noise, warmup_ics, tspan, p_net)
+
+#%% Or we can load the solution
+TEST = load_model(save_file)
 
 #%% Run the simulation
 print("[$(Dates.now())]: Running the simulation... ")
-NetProb = SDEProblem(net, noise, warmup_ics, tspan, p_net)
 @time NetSol = solve(NetProb, SOSRI(), 
-        abstol = 2e-2, reltol = 0.2, maxiters = 1e7,
+        abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
         save_idxs = [1:((p[:nx]*p[:ny])|>Int64)...], 
         progress = true, progress_steps = 1
     )
 println("Completed")
-BotNotify("Model run complete")
+BotNotify("{Waves} Model run complete")
 
 
 #%% Save the solution, must be on drive first
@@ -106,24 +107,6 @@ mapslices(+, a, dims = 2)
 
 run_loop = false #so far set this section to ignore
 if run_loop
-    #%% We can load the data here 
-    file_root = "C:\\Users\\RennaLabSA1\\Documents\\ModellingData\\2021-04-28_gACh_mu_0.2"
-    params_file = joinpath(param_root, "params.json")
-    conds_file = joinpath(param_root, "conds.json")
-
-    print("[$(Dates.now())]: Loading the model...")
-    p = read_JSON(params_file) 
-    #Set up the initial conditions
-    u0 = read_JSON(conds_file);
-    net = Network(p[:nx], p[:ny]; μ = p[:μ], version = :gACh, gpu = true) #This branch uses GPU mode
-    p_net = extract_dict(p);
-    u0_net = extract_dict(u0, p[:nx], p[:ny]) |> cu;
-    warmup = (0.0|>Float32, 300e3|>Float32)
-    tspan  = (0.0|>Float32 , 60e3|>Float32)
-    NetProb = SDEProblem(net, noise, u0_net, warmup, p_net)
-    JLD2.@load "$(file_root)\\sol.jld2" NetSol
-    println("Completed")
-
     #%% Run multiple samples
     n = 4
     for val in LinRange(1.0, 20.0, 10)
