29d0b7430032e8bb707fbb588e834f83f8c25e00
mattar13 <mtarchick13@gmail.com>
2021-07-08 11:27:19 -0400

    simplified the wave simulation

--- examples/running_wave_sim_GPU.jl
+++ examples/running_wave_sim_GPU.jl
@@ -11,100 +11,24 @@ global_logger(TerminalLogger())
 dotenv("D:\\TelegramAccessEnv\\.env")
 #Activate the GPU
 RetinalChaos.CUDA.allowscalar(false)
-save_file = "C:\\Users\\RennaLabSA1\\Documents\\ModellingData\\$(Date(Dates.now()))_test_mu_18p\\"
-params_file = "params\\params.json"
-conds_file = "params\\conds.json"
 
-#%% Setup and warmup the simulation
-print("[$(Dates.now())]: Setting up the model...")
-#Save the model, params, conditions, animations here
-if isdir(save_file) == false
-    #The directory does not exist, we have to make it 
-    mkdir(save_file)
-end
-p = read_JSON(params_file, is_type = Dict{Symbol, Float32}) 
-u0 = read_JSON(conds_file, is_type = Dict{Symbol, Float32});
-#Save the params and ics to load later. 
-write_JSON(p, "$(save_file)\\params.json") #write the parameters to save for later
-write_JSON(u0, "$(save_file)\\conds.json") #write the ics to save for later
-net = Network(p[:nx], p[:ny]; μ = p[:μ], version = :gACh, gpu = true) #This branch uses GPU mode
-p_net = extract_dict(p);
-u0_net = extract_dict(u0, p[:nx], p[:ny]) |> cu;
-warmup = (0f0, p[:t_warm])
-tspan  = (0f0, p[:t_run])
-NetProb = SDEProblem(net, noise, u0_net, warmup, p_net)
-println("Completed")
-
-#%% Lets warm up the solution first (using GPU if available)
-print("[$(Dates.now())]: Warming up the solution: ")
-@time NetSol = solve(NetProb, SOSRI(), 
-        abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
-        progress = true, progress_steps = 1, 
-        save_everystep = false
-    )
-warmup_ics = NetSol[end]
-JLD2.@save "$(save_file)\\warmup_ics.jld2" warmup_ics
-println("Completed")
-BotNotify("{Waves} Model warmup complete")
-
-#put this statement to clear out all of the memory
-NetSol = nothing; GC.gc(true); CuArrays.reclaim()
-
-#%% Make the new problem
-NetProb = SDEProblem(net, noise, warmup_ics, tspan, p_net)
-
-#%% Or we can load the solution
-TEST = load_model(save_file)
-
-#%% Run the simulation
-print("[$(Dates.now())]: Running the simulation... ")
-@time NetSol = solve(NetProb, SOSRI(), 
-        abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
-        save_idxs = [1:((p[:nx]*p[:ny])|>Int64)...], 
-        progress = true, progress_steps = 1
-    )
-println("Completed")
-BotNotify("{Waves} Model run complete")
-
-
-#%% Save the solution, must be on drive first
-print("[$(Dates.now())]: Saving the simulation...")
-JLD2.@save "$(save_file)\\sol.jld2" NetSol
-println("Completed")
-
-#%% Plotting animation
-println("[$(Dates.now())]:Animating simulation...")
-anim = @animate for t = 1.0:60.0:NetSol.t[end]
-    println("Animating frame $t")
-    frame_i = reshape(NetSol(t) |> Array, (p[:nx]|>Int64, p[:ny]|>Int64))
-    heatmap(frame_i, ratio = :equal, grid = false,
-            xaxis = "", yaxis = "", xlims = (0, p[:nx]), ylims = (0, p[:ny]),
-            c = :curl, clims = (-70.0, 0.0),
-    )
-end
-gif(anim, "$(save_file)\\animation.gif", fps = 40.0)
-BotNotify("Finished with animation")
-BotFigure("$(save_file)\\animation.gif")
+#%% Load the needed files to run the model
+save_file = "C:\\Users\\RennaLabSA1\\Documents\\ModellingData\\$(Date(Dates.now()))_test1\\"
+p_dict = read_JSON("params\\params.json", is_type = Dict{Symbol, Float32}) 
+u_dict = read_JSON("params\\conds.json", is_type = Dict{Symbol, Float32})
+NetSol = load_model(save_file, p_dict, u_dict, reset_model = true)
+#%%
 
 #%%Conduct the threshold analysis
 thresholds = calculate_threshold(NetSol; dt = 500.0) #This takes really long
 @save "$(save_file)\\thresholds.jld2" thresholds
-BotNotify("Finished running threshold analysis")
+BotNotify("{Wave} Finished running threshold analysis")
 
 #%% Work in progress
-#timestamps = Tuple[]
-rng = (NetSol.t[1]|>Float64, NetSol.t[end]|>Float64)
-dt = 100.0
-#%%
-data_array = (NetSol |> Array) .> thresholds
-tsteps = NetSol.t
-t_stamps = findall(data_array)
-tsteps[data_array[1, :]]
-#%%
-arr = collect(1:100)
-a = rand(10, 100)
-mapslices(+, a, dims = 2)
+timestamps = get_timestamps(NetSol, thresholds)
 
+
+#%%
 run_loop = false #so far set this section to ignore
 if run_loop
     #%% Run multiple samples
