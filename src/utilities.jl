76ebf4fbadca9add0fa1eca6ef6db246b9724356
mattar13 <mtarchick13@gmail.com>
2021-07-08 11:27:08 -0400

    added a load_model function

--- src/utilities.jl
+++ src/utilities.jl
@@ -91,12 +91,112 @@ function extract_dict(dict_item::Dict{Symbol, T}, dims...) where T <: Real
     val_map
 end
 
-function load_model(file_root::String)
-    p = read_JSON("$(file_root)\\params.json")
-    p_net = extract_dict(p);
-    net = Network(p[:nx]|> Int64, p[:ny]|>Int64; μ = p[:μ], version = :gACh, gpu = true)
-    JLD2.@load "$(file_root)\\warmup_ics.jld2" warmup_ics
-    NetProb = SDEProblem(net, noise, warmup_ics, (0.0 , p[:t_run]), p_net)
-    return NetProb
-end
+function load_model(file_root::String, p_dict::Dict{Symbol, T}, u_dict::Dict{Symbol, T}; 
+            reset_model::Bool = false, gpu::Bool = true, version = :gACh,
+            abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
+            animate_solution = true, animate_dt = 60.0
+        ) where T <: Real
     
+    if reset_model && isdir(file_root)
+        rm(file_root, force = true, recursive = true)
+    end
+
+    if !isdir(file_root)
+        println("[$(now())]: Making experiment file")
+        mkdir(file_root)
+    end
+
+    #We can step through each section checking to see what level we are at
+    params_file = "$(file_root)\\params.json"
+    iconds_file = "$(file_root)\\conds.json"
+    conds_file = "$(file_root)\\warmup_ics.jld2"
+    sol_file = "$(file_root)\\sol.jld2"
+    animation_file =  "$(file_root)\\animation.gif"
+
+    #We first check to see if there is a param loaded
+    if isfile(params_file)
+        p_dict = read_JSON(params_file, is_type = Dict{Symbol, T}) 
+        p = p_dict |> extract_dict
+    else
+        #In this scenario we need to use a new dictionary
+        println("[$(now())]: Writing a new dictionary")
+        p = p_dict |> extract_dict
+
+        write_JSON(p_dict, params_file) #write the parameters to save for later
+        write_JSON(u_dict, iconds_file) #write the 
+    end
+    
+    if isfile(conds_file)
+        #Load the initial conditions
+        JLD2.@load conds_file warmup_ics
+        #Load the model
+        net = Network(p_dict[:nx], p_dict[:ny]; μ = p_dict[:μ], version = version, gpu = gpu)
+        #Load the ODE problem
+        NetProb = SDEProblem(net, noise, warmup_ics, (0f0 , p_dict[:t_run]), p0)
+    else
+        println("[$(now())]: Model loaded from new")
+        #Load the initial conditions
+        if gpu
+            u0 = extract_dict(u_dict, p_dict[:nx], p_dict[:ny]) |> cu
+        else
+            u0 = extract_dict(u_dict, p_dict[:nx], p_dict[:ny])
+        end
+
+        #Load the model
+        net = Network(p_dict[:nx], p_dict[:ny]; μ = p_dict[:μ], version = version, gpu = gpu)
+        #Load the ODE problem
+        NetProb = SDEProblem(net, noise, u0, (0f0 , p_dict[:t_warm]), p)
+
+        println(u0 |> size)
+        #We may put a section here to warmup the solution
+        print("[$(now())]: Warming up the solution... ")
+        @time NetSol = solve(NetProb, SOSRI(), 
+                abstol = abstol, reltol = reltol, maxiters = maxiters,
+                progress = true, progress_steps = 1, 
+                save_everystep = false
+            )
+        
+        warmup_ics = NetSol[end]
+        JLD2.@save conds_file warmup_ics
+        if gpu
+            NetSol = nothing; GC.gc(true); RetinalChaos.CUDA.reclaim()
+        end
+    end
+    BotNotify("{Waves} Model warmup solution loaded")
+    
+    #Now we want to test whether or not we have run the simulation
+    if isfile(sol_file)
+        #We don't need to rerun the problem
+        JLD2.@load sol_file NetSol
+    else
+        print("[$(now())]: Running the model... ")
+        NetProb = SDEProblem(net, noise, warmup_ics, (0f0 , p_dict[:t_run]), p)
+        #Run the solution to fruition
+        @time NetSol = solve(NetProb, SOSRI(), 
+            abstol = abstol, reltol = reltol, maxiters = maxiters,
+            save_idxs = [1:(Int64(p_dict[:nx]*p_dict[:ny]))...], 
+            progress = true, progress_steps = 1
+        )
+
+        print("[$(now())]: Saving the simulation...")
+        JLD2.@save sol_file NetSol
+        println("Completed")
+
+    end
+    BotNotify("{Waves} Model solution loaded")
+
+    if !isfile(animation_file) && animate_solution
+        println("[$(now())]: Animating simulation...")
+        anim = @animate for t = 1.0:animate_dt:NetSol.t[end]
+            frame_i = reshape(NetSol(t) |> Array, (p_dict[:nx]|>Int64, p_dict[:ny]|>Int64))
+            heatmap(frame_i, ratio = :equal, grid = false,
+                    xaxis = "", yaxis = "", xlims = (0, p_dict[:nx]), ylims = (0, p_dict[:ny]),
+                    c = :curl, clims = (-70.0, 0.0),
+            )
+        end
+        gif(anim, animation_file, fps = 1000.0/animate_dt)
+    end
+
+    #We want to return the solution
+    return NetSol
+end 
