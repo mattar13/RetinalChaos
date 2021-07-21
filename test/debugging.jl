eded4793f3e965976bf0fa1ac4b0905d0c5a8f9e
Matthew Tarchick <mtarchick13@gmail.com>
2021-07-21 15:12:27 -0400

    added debugging

---
+++ test/debugging.jl
@@ -0,0 +1,9 @@
+using RetinalChaos #Load the package
+using GoogleDrive
+
+#%% Testing the Google Drive Upload and Download
+drive_link = "https://drive.google.com/file/d/1fVlljwFJ1gDoF61ehxOPJVWd5GFHyZJP//view?usp=sharing"
+download_loc = "test\\"
+drive_download(drive_link, download_loc)
+#%% I think we can do some stuff in python to actually utilize this functionality
+
