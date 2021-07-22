f5aeca67451bdd3d456a900dde5fdf4101c14940
Matthew Tarchick <mtarchick13@gmail.com>
2021-07-22 18:24:01 -0400

    making figure 5

--- figures/making_figure5.jl
+++ figures/making_figure5.jl
@@ -184,7 +184,7 @@ begin
 	
 		if i == 1
 			plot!(plt_burst_ridge[1], c = :black, 
-				left_margin = 20.0mm,
+				#left_margin = 20.0mm,
 				weights, edges, 
 				fill_z = i,
 				fillrange = [2.0, 2.0], fillcolor = :jet, 
@@ -246,7 +246,102 @@ begin
 	plt_IBI_ridge
 end
 
-# ╔═╡ 39a15789-017b-4489-a370-782f510d3c22
+# ╔═╡ c7a91138-02db-42a6-a42d-138fef07836d
+begin #Load all the paths
+	load_path0 = "E:\\Data\\Modelling\\mu_0\\"
+	p_dict = read_JSON("$(load_path0)\\params.json", is_type = Dict{Symbol, Float32})
+	u_dict = read_JSON("$(load_path0)\\iconds.json", is_type = Dict{Symbol, Float32}) 
+	#Mu = 0%
+	sol_mu0 = load_model(load_path0, p_dict, u_dict, gpu = false)
+	#Mu = 12%
+	load_path12 = "E:\\Data\\Modelling\\mu_12\\"
+	sol_mu12 = load_model(load_path12, p_dict, u_dict, gpu = false)
+	
+	load_path50 = "E:\\Data\\Modelling\\mu_50\\"
+	sol_mu50 = load_model(load_path50, p_dict, u_dict, gpu = false)
+end	
+
+# ╔═╡ 6c2abaec-8214-4c20-94ed-6cda1f216ee0
+begin #Heatmap range
+	nx, ny = (125, 125)
+	plt_grid = plot(layout = grid(4,4), 
+		yticks = false, xticks = false, yaxis = false, xaxis = false,
+		ratio = :equal, cbar = false,
+		xlims = (0, nx), ylims = (0, ny), margin = 0.0mm, 
+		size = (500,500)
+	)
+	plt_trace = plot(layout = grid(4,1), grid = false, xformatter = x -> x/1000)
+	#Plot the 0% mu
+	
+	plot!(plt_trace[1], sol_mu0, vars = collect(1:7), label = false, 
+		c = :rainbow, line_z = collect(1:7)', clims = (1,7), cbar = false,
+		xlabel = "", xticks = false, 
+		ylabel = "Vt (mV)", ylims = (-80.0, 0.0), 
+		yticks = LinRange(-80.0,0.0, 4), yformatter = y -> round(Int64, y)
+	)
+	for (idx, frame) in enumerate(LinRange(40e3, 50e3, 4))
+		grid_sim = reshape(sol_mu0(frame), nx, ny)
+		plot!(plt_grid[1,idx], grid_sim, 
+			c = :curl, st = :heatmap, clims = (-70.0, 0.0)
+		)
+		annotate!(plt_grid[1,idx], round(Int64,125/2), 20, "t", :white)
+	end
+	
+	plot!(plt_trace[2], sol_mu12, vars = collect(1:7), label = false, 
+		c = :rainbow, line_z = collect(1:7)', clims = (1,7), cbar = false,
+		xlabel = "", xticks = false, 
+		ylabel = "Vt (mV)", ylims = (-80.0, 0.0), 
+		yticks = LinRange(-80.0,0.0, 4), yformatter = y -> round(Int64, y)
+	)
+	for (idx, frame) in enumerate(LinRange(10e3, 20e3, 4))
+		grid_sim = reshape(sol_mu12(frame), nx, ny)
+		plot!(plt_grid[2,idx], grid_sim, 
+			c = :curl, st = :heatmap, clims = (-70.0, 0.0)
+		)
+		annotate!(plt_grid[2,idx], round(Int64,125/2), 20, "t", :white)
+	end
+	
+	plot!(plt_trace[3], sol_mu12, vars = collect(1:7), label = false, 
+		c = :rainbow, line_z = collect(1:7)', clims = (1,7), cbar = false,
+		xlabel = "", xticks = false, 
+		ylabel = "Vt(mV)", ylims = (-80.0, 0.0), 
+		yticks = LinRange(-80.0,0.0, 4), yformatter = y -> round(Int64, y)
+	)
+	for (idx, frame) in enumerate(LinRange(4e4, 5e4, 4))
+		grid_sim = rand(125,125)
+		plot!(plt_grid[3,idx], grid_sim, 
+			c = :curl, st = :heatmap, clims = (-70.0, 0.0)
+		)
+		annotate!(plt_grid[3,idx], round(Int64,125/2), 20, "t", :white)
+	end
+	
+	plot!(plt_trace[4], sol_mu50, vars = collect(1:7), label = false, 
+		c = :rainbow, line_z = collect(1:7)', clims = (1,7), cbar = false,
+		xlabel = "Time (s)", 
+		ylabel = "Vt (mV)", ylims = (-80.0, 0.0), 
+		yticks = LinRange(-80.0,0.0, 4), yformatter = y -> round(Int64, y)
+	)
+	
+	for (idx, frame) in enumerate(LinRange(5e3, 15e3, 4))
+		grid_sim = reshape(sol_mu50(frame), nx, ny)
+		plot!(plt_grid[4,idx], grid_sim, 
+			c = :curl, st = :heatmap, clims = (-70.0, 0.0)
+		)
+		annotate!(plt_grid[4,idx], round(Int64,125/2), 20, "t", :white)
+	end
+	
+	fig5_BC = plot(plt_trace, plt_grid, size = (1000, 500))
+end
+
+# ╔═╡ 19cc489d-8df4-4d32-93f0-a0c842fc1f64
+fig5 = plot(
+	plt_burst_ridge, fig5_BC, 
+	
+	bottom_margin = 0.0mm,
+	layout = grid(2,1), size = (1000, 1000)
+	) 
+
+# ╔═╡ e3d313db-b96f-4213-b695-16ddec786806
 
 
 # ╔═╡ Cell order:
@@ -256,9 +351,12 @@ end
 # ╟─aa88f543-633a-476b-b6cb-441d020dd649
 # ╠═a17974ca-1936-4cc9-bba9-9587012e9b3a
 # ╟─c8d0590c-b9f5-4b2d-95eb-19ecaf816cee
-# ╟─ac30fece-ca25-403d-b1a3-e4005c26b910
+# ╠═ac30fece-ca25-403d-b1a3-e4005c26b910
 # ╟─c9db69d3-cc2a-40e6-9f88-c28615868f9d
 # ╟─a32fbb84-c681-492c-a37c-3867db567b7a
 # ╟─9d2d5214-9d52-4048-a99f-71fc5403cdff
 # ╠═29487316-45cd-4630-8472-72c1e8cae38a
-# ╠═39a15789-017b-4489-a370-782f510d3c22
+# ╟─c7a91138-02db-42a6-a42d-138fef07836d
+# ╟─6c2abaec-8214-4c20-94ed-6cda1f216ee0
+# ╠═19cc489d-8df4-4d32-93f0-a0c842fc1f64
+# ╠═e3d313db-b96f-4213-b695-16ddec786806
