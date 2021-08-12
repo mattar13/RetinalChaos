0d6ca4469a50a0fc2bb9620b241b1675ecc989c1
Matthew Tarchick <mtarchick13@gmail.com>
2021-08-12 18:16:49 -0400

    updated debugging (for sharing with other comps)

--- test/debugging.jl
+++ test/debugging.jl
@@ -2,10 +2,22 @@ using Revise
 using RetinalChaos #Load the package
 using BSON
 #%%Lets figure out how to extract the waves
-load_path = "E:\\Data\\Modelling\\mu_50\\"
+import RetinalChaos.param_path
+
+load_path = "E:\\Data\\Modelling\\mu_12\\"
 p_dict = read_JSON("$(load_path)\\params.json", is_type = Dict{Symbol, Float32})
 u_dict = read_JSON("$(load_path)\\iconds.json", is_type = Dict{Symbol, Float32})
 sol_mu12 = load_model(load_path, p_dict, u_dict, gpu = false)
+#%%
+sol = sol_mu12
+new_sol = SciMLBase.build_solution(sol.prob, sol.alg, sol.t, sol.u) #we can use this to build a solution without GPU
+#%%
+#Save everything but the solution
+bson("$(load_path)\\testing_saving.bson", 
+          Dict(:sol_prob => sol.prob, :sol_alg => sol.alg, :sol_t => sol.t)
+     )
+
+#%%
 timestamps = BSON.load("$(load_path)\\timestamps.bson") #Access timestamps
 data = BSON.load("$(load_path)\\data.bson") #Access Thresholds
 #%%
