881800d1c600d7fc80c39d97cbc3ae8e7e60706d
mattar13 <mtarchick13@gmail.com>
2021-08-19 11:42:52 -0400

    added export for animate solution

--- src/RetinalChaos.jl
+++ src/RetinalChaos.jl
@@ -75,7 +75,7 @@ export tar_conds, tar_pars, p_find, u_find
 
 include("utilities.jl")
 export extract_dict, read_JSON, write_JSON #Load the parameter loading functions 
-export run_model, save_solution, load_solution, convert_to_cpu
+export run_model, save_solution, load_solution, convert_to_cpu, animate_solution
 
 # Export functions for dynamical analysis
 include("dynamical_analysis.jl")
