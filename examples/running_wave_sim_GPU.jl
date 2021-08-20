99588fca2e2b886237e244b83416f2d023ba028e
mattar13 <mtarchick13@gmail.com>
2021-08-20 11:58:16 -0400

    updated some debugging statements for running sim

--- examples/running_wave_sim_GPU.jl
+++ examples/running_wave_sim_GPU.jl
@@ -15,22 +15,26 @@ dotenv("D:\\TelegramAccessEnv\\.env")
 RetinalChaos.CUDA.allowscalar(false)
 
 #%% Run a simulation on it's own
+param_test = :μ #Set a single parameter
+x = 0.25 #Set it's value
 try
-    BotNotify("{Waves} Running simulation for 18% started")
+    BotNotify("{Waves} Running simulation for $(param_test) = $x started")
     u_dict = read_JSON(Dict{Symbol, Float32}, "$(param_path)/conds.json") #Load initial conditions
     p_dict = read_JSON(Dict{Symbol, Float32}, "$(param_path)/params.json") #Load the parameters
     ################# If you want to modify parameters do it here ###############
     p_dict[:t_run] = 300e3 #We want to make the run time longer
-    p_dict[:μ] = 0.0
+    p_dict[param_test] = x
     ################# If you want to modify parameters do it here ###############
-    save_path = "C:\\Users\\RennaLabSA1\\Documents\\ModellingData\\example_animations\\mu_18" #Specify the save path
+    save_path = "C:\\Users\\RennaLabSA1\\Documents\\ModellingData\\example_animations\\μ_25" #Specify the save path
+    abs_tol = 2e-2
+    rel_tol = 2e-2 #try changing this to get less instability
     NetSol = run_model(save_path, p_dict, u_dict) #This is a succinct function for running the model
-    BotNotify("{Waves} Running simulation for 18% completed")
+    BotNotify("{Waves} Running simulation for $(param_test) = $x completed")
     save_solution(NetSol, save_path, partitions = 10) #Save the solution in 10 seperate parts
-    BotNotify("{Waves} Saving simulation for 18% completed")
+    BotNotify("{Waves} Saving simulation for $(param_test) = $x completed")
     sol = load_solution(save_path) #Reload the solution for analysis
     animate_solution(sol, save_path) #Animate the solution
-    BotNotify("{Waves} Animating simulation for 18% completed")
+    BotNotify("{Waves} Animating simulation for $(param_test) = $x completed")
     timeseries_analysis(sol, save_path) #conduct a timeseries analysis
     BotNotify("{Waves} Timeseries analysis completed")
 catch 
@@ -42,7 +46,7 @@ end
 param_test = :μ
 param_range = [0.0, 0.125, 0.25, 0.50]
 for x in param_range
-    println("Running simulation for $mu")
+    println("Running simulation for $(param_test) = $x")
     try
         BotNotify("{Waves} Running simulation for $(string(param_test)) = $x started")
         u_dict = read_JSON(Dict{Symbol, Float32}, "$(param_path)/conds.json")
@@ -53,17 +57,16 @@ for x in param_range
         ############### If you want to modify parameters do it here ###############
         save_path = "C:\\Users\\RennaLabSA1\\Documents\\ModellingData\\mu_experiment\\$(string(param_test))_$(round(Int64, x*100))\\"
         NetSol = run_model(save_path, p_dict, u_dict)
-        BotNotify("{Waves} Running simulation for 18% completed")
+        BotNotify("{Waves} Running simulation for $(param_test) = $x completed")
         save_solution(NetSol, save_path, partitions = 10)
-        BotNotify("{Waves} Saving simulation for 18% completed")
+        BotNotify("{Waves} Saving simulation for $(param_test) = $x completed")
         sol = load_solution(save_path)
         animate_solution(sol, save_path)
-        BotNotify("{Waves} Animating simulation for 18% completed")
+        BotNotify("{Waves} Animating simulation for $(param_test) = $x completed")
         timeseries_analysis(sol, save_path)
-        BotNotify("{Waves} Timeseries analysis completed")
+        BotNotify("{Waves} Timeseries analysis for $(param_test) = $x completed")
     catch error
         println(error)
         BotNotify("{Waves} An error has occurred $(typeof(error))")
     end
 end
-
