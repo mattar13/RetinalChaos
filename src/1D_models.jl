4e53cb5a48a484b9a59c22fe9ce965bf38310cf3
Matthew Tarchick <mtarchick13@gmail.com>
2022-01-25 16:38:35 -0500

    added GABA model parameters

--- src/1D_models.jl
+++ src/1D_models.jl
@@ -1,6 +1,5 @@
 function T_ode(dU::AbstractArray{T2}, U::AbstractArray{T2}, p::AbstractArray{T}, t::T) where {T <: Real, T2}
 	#Extract all of the parameters first
-	(g_leak, E_leak, g_Ca, V1, V2, E_Ca, g_K, E_K, g_TREK, g_ACh, k_d, E_ACh, I_app, C_m, V3, V4, τn, C_0, λ, δ, τc, α, τa, β, τb, ρ, τACh, k, V0, D, τw, σ) = p
 	v = view(U, 1)
 	n = view(U, 2)
 	c = view(U, 3)
@@ -57,8 +56,6 @@ function T_sde(dU::AbstractArray{T2}, U::AbstractArray{T2}, p::AbstractArray{T},
 	de = view(dU, 6)
 	dW = view(dU, 7)
 
-	(g_leak, E_leak, g_Ca, V1, V2, E_Ca, g_K, E_K, g_TREK, g_ACh, k_d, E_ACh, I_app, C_m, V3, V4, τn, C_0, λ, δ, τc, α, τa, β, τb, ρ, τACh, k, V0, D, τw, σ) = p
-
 	@.dv = (
 			- g_leak*(v-E_leak)
 			- g_Ca*R_INF(v, V1, V2)*(v-E_Ca)
@@ -79,65 +76,35 @@ function T_sde(dU::AbstractArray{T2}, U::AbstractArray{T2}, p::AbstractArray{T},
 	dU
 end
 
+function GABA_ODE(dU::AbstractArray{T2}, U::AbstractArray{T2}, p::AbstractArray{T}, t::T) where {T<:Real,T2}
+     #Extract all of the parameters first
+     (g_leak, E_leak, g_Ca, V1, V2, E_Ca, g_K, E_K, g_TREK, g_ACh, k_ACh, E_ACh, g_GABA, k_GABA, E_GABA, I_app, C_m, V3, V4, τn, C_0, λ, δ, τc, α, τa, β, τb, ρe, ρi, τACh, τGABA, ke, ki, V0e, V0i, D, τw, σ) = p
+     v = view(U, 1)
+     n = view(U, 2)
+     c = view(U, 3)
+     a = view(U, 4)
+     b = view(U, 5)
+     e = view(U, 6)
+     i = view(U, 7)
+     W = view(U, 8)
 
+     dv = view(dU, 1)
+     dn = view(dU, 2)
+     dc = view(dU, 3)
+     da = view(dU, 4)
+     db = view(dU, 5)
+     de = view(dU, 6)
+     di = view(dU, 7)
+     dW = view(dU, 8)
 
-#Modellling toolkit may not work so well
-######################Modelling Toolkit for Tarchick Model
-#= #Register parameters for ionic currents 
-@parameters g_leak E_leak #Leaky currents
-@parameters g_Ca V1 V2 E_Ca #Cav currents
-@parameters g_K V3 V4 E_K #Kv currents
-@parameters g_TREK g_sAHP #TREK and sAHP currents
-@parameters g_ACh k_d E_ACh #Acetylcholine current parameters
-@parameters I_app C_m  #Extras
-@parameters τn τc τa τb τACh τw τr τs #Time constants
-@parameters C_0 V0
-@parameters λ δ α β ρ k σ D #Relationships in my model
-@parameters δc αs αc αr H_x VS VH g_n R E_n #Relationships in other models
-#Register variables
-@variables t v(t) n(t) c(t) a(t) b(t) e(t) W(t) r(t) s(t)
-#@derivatives d'~ Differential(t) #This is deprecated
-d = Differential(t)
-
-T_model_eqs = [
-					d(v)~ (
-										-g_leak*(v-E_leak)
-									+ -g_Ca*M_INF(v, V1, V2)*(v-E_Ca)
-									+ -g_K*n*(v-E_K)
-									+ -g_TREK*b*(v-E_K)
-									+ -g_ACh*ħ(e, k_d)*(v-E_ACh)
-									+ I_app
-									+ W + 0*σ #This 
-									)/C_m ,
-					d(n) ~ (Λ(v, V3, V4) * (N_INF(v, V3, V4) - n))/τn,
-					d(c) ~ (C_0 + δ*(-g_Ca*M_INF(v, V1, V2)*(v - E_Ca)) - λ*c)/τc,
-					d(a) ~ (α*c^4*(1-a)-a)/τa,
-					d(b) ~ (β*a^4*(1-b)-b)/τb,
-					d(e) ~ (-2*D*e) + (ρ*Φ(v, k, V0) - e)/τACh,
-					d(W) ~ -W/τw
-]
-T_noise_eqs = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, σ]
-T_ode = ODESystem(T_model_eqs)
-T_sde = SDESystem(T_model_eqs, T_noise_eqs, t, T_ode.states, T_ode.ps)
-sym_ps = map(x -> x |> Symbol, T_ode.ps)
-sym_cs = map(x -> x |> Symbol, T_ode.states)
-
-#Karvourniari Model
-K_model_eqs = [
-					d(v) ~ (-g_leak*(v-E_leak)
-										+ -g_Ca*M_INF(v, V1, V2)*(v-E_Ca)
-										+ -g_K*n*(v-E_K)
-										+ -g_sAHP*r^4*(v-E_K)
-										+ I_app
-										+ W + 0*σ
-										)/C_m,
-					d(n)~ (Λ(v, V3, V4) * ((N_INF(v, V3, V4) - n)))/τn,
-					d(r) ~ (αr*s*(1-r)-r)/τr,
-					d(s) ~ (αs*c^4*(1-s)-s)/τs,
-					d(c) ~ (-(αc/H_x)*c + C_0 + δc*(-g_Ca*M_INF(v, V1, V2)*(v-E_Ca)))/τc,
-					d(W) ~ -W
-]
-K_noise_eqs = [0.0, 0.0, 0.0, 0.0, 0.0, σ]
-K_ode = ODESystem(K_model_eqs)
-K_sde = SDESystem(K_model_eqs, K_noise_eqs, t, K_ode.states, K_ode.ps)
- =#
+     @. dv = (-g_leak*(v - E_leak) - g_Ca*R_INF(v, V1, V2)*(v - E_Ca) - g_K*n*(v - E_K) - g_TREK*b*(v - E_K) - g_ACh*ħ(e, k_ACH)*(v - E_ACh) - g_GABA*ħ(i, k_GABA)*(v - E_GABA) + I_app) / C_m
+     @. dn = (Λ(v, V3, V4) * ((R_INF(v, V3, V4) - n))) / τn
+     @. dc = (C_0 + δ * (-g_Ca * R_INF(v, V1, V2) * (v - E_Ca)) - λ * c) / τc
+     @. da = (α * c^4 * (1 - a) - a) / τa
+     @. db = (β * a^4 * (1 - b) - b) / τb
+     @. de = (ρe * Φ(v, ke, V0e) - e) / τACh
+     @. di = (ρi * Φ(v, ki, V0i) - i) / τGABA
+     @. dW = -W / τw
+     #nothing
+     dU
+end
--- src/models.jl
+++ src/models.jl
@@ -12,8 +12,8 @@ This function is used to calculate the current. It represents the generalized fo
 - OUTPUTS:
     - Ionic current of type n (\$I_n\$)
 """
-I_n(g_n, R, v, E_n)= -g_n*R*(v-E_n)
-I_n(g_n::T, R::T, v::T, E_n::T) where T <: Real = -g_n*R*(v-E_n)
+I_n(g_n, R, v, E_n) = -g_n * R * (v - E_n)
+I_n(g_n::T, R::T, v::T, E_n::T) where {T<:Real} = -g_n * R * (v - E_n)
 export I_n
 """
 R_INF(v_t, VS, VH)
@@ -33,14 +33,14 @@ The equation for this is as follows:
 - For Calcium channel activation: \$V_1 = V_S,  V_2 = V_H,  M_{\\infty} = R_{\\infty}\$
 - For Potassium channel activation: \$V_3 = V_S,  V_4 = V_H,  N_{\\infty} = R_{\\infty}\$
 """
-R_INF(v, VS, VH)  = (1 + tanh((v - VS)/VH))/2;
-R_INF(v::T, VS::T, VH::T) where T <: Real  = (1 + tanh((v - VS)/VH))/2;
+R_INF(v, VS, VH) = (1 + tanh((v - VS) / VH)) / 2;
+R_INF(v::T, VS::T, VH::T) where {T<:Real} = (1 + tanh((v - VS) / VH)) / 2;
 #CuArrays.@cufunc R_INF(v, VS, VH) = (1 + tanh((v - VS)/VH))/2;
 R_INF(v::CuArray, VS, VH) = R_INF.(v, VS, VH)
 export R_INF
 
-M_INF(v::T, V1::T, V2::T) where T = (1 + tanh((v - V1)/V2))/2;
-N_INF(v::T, V3::T, V4::T) where T = (1 + tanh((v - V3)/V4))/2;
+M_INF(v::T, V1::T, V2::T) where {T} = (1 + tanh((v - V1) / V2)) / 2;
+N_INF(v::T, V3::T, V4::T) where {T} = (1 + tanh((v - V3) / V4)) / 2;
 
 """
 Λ(v_t, VS, VH)
@@ -55,29 +55,32 @@ This equation related voltage to the rate constant of opening Potassium channels
 - OUTPUTS
     - The potassium rate constant (\$\\Lambda\$)
 """
-Λ(V, V3, V4) = cosh((V-V3)/(2*V4));
-Λ(v::T, V3::T, V4::T) where T <: Real = cosh((v-V3)/(2*V4));
+Λ(V, V3, V4) = cosh((V - V3) / (2 * V4));
+Λ(v::T, V3::T, V4::T) where {T<:Real} = cosh((v - V3) / (2 * V4));
 #CuArrays.@cufunc Λ(v, V3, V4) = cosh((v-V3)/(2*V4));
-Λ(v::CuArray, V3, V4) = LAM.(v, V3, V4) 
+Λ(v::CuArray, V3, V4) = LAM.(v, V3, V4)
 export Λ
 """
 NEED DOC
 """
-Φ(v, κ, V_0) = 1/(1 + exp(-κ * (v - V_0)))
-Φ(v::T, κ::T, V_0::T) where T <: Real = 1/(1 + exp(-κ * (v - V_0)))
+Φ(v, κ, V_0) = 1 / (1 + exp(-κ * (v - V_0)))
+Φ(v::T, κ::T, V_0::T) where {T<:Real} = 1 / (1 + exp(-κ * (v - V_0)))
 #CuArrays.@cufunc Φ(v, κ, V_0) = 1/(1 + exp(-κ * (v - V_0)))
 Φ(v::CuArray, κ, V_0) = Φ.(v, κ, V_0)
 
 """
 NEED DOC
 """
-ħ(e, K_d) = (e^2)/(e^2 + K_d)
-ħ(e::T, K_d::T) where T <: Real = (e^2)/(e^2 + K_d)
+ħ(e, K_d) = (e^2) / (e^2 + K_d)
+ħ(e::T, K_d::T) where {T<:Real} = (e^2) / (e^2 + K_d)
 #CuArrays.@cufunc ħ(e, K_d) = (e^2)/(e^2 + K_d)
 ħ(e::CuArray, K_d) = ħ.(e, K_d)
 
-const tar_pars = [:g_leak, :E_leak, :g_Ca, :V1, :V2, :E_Ca, :g_K, :E_K, :g_TREK, :g_ACh, :k_d, :E_ACh, :I_app, :C_m, :V3, :V4, :τn, :C_0, :λ, :δ, :τc, :α, :τa, :β, :τb, :ρ, :τACh, :k, :V0,  :D, :τw, :σ]
+const tar_pars = [:g_leak, :E_leak, :g_Ca, :V1, :V2, :E_Ca, :g_K, :E_K, :g_TREK, :g_ACh, :k_d, :E_ACh, :I_app, :C_m, :V3, :V4, :τn, :C_0, :λ, :δ, :τc, :α, :τa, :β, :τb, :ρ, :τACh, :k, :V0, :D, :τw, :σ]
 const tar_conds = [:v, :n, :c, :a, :b, :e, :W]
+const GABA_pars = [:g_leak, :E_leak, :g_Ca, :V1, :V2, :E_Ca, :g_K, :E_K, :g_TREK, :g_ACh, :k_ACh, :E_ACh, :g_GABA, :k_GABA, :E_GABA, :I_app, :C_m, :V3, :V4, :τn, :C_0, :λ, :δ, :τc, :α, :τa, :β, :τb, :ρe, :ρi, :τACh, :τGABA, :ke, :ki, :V0e, :V0i, :D, :τw, :σ]
+const GABA_conds = [:v, :n, :c, :a, :b, :e, :i, :W]
+
 const lansdell_pars = [:I_app, :E_Ca, :E_K, :E_Leak, :E_ACh, :V1, :V2, :V3, :V4, :g_Ca, :g_K, :g_Leak, :λ, :g_ACh, :δ, :C_m, :τr, :τs, :τACh, :γ, :α, :β, :k, :V0, :D, :μ]
 const lansdell_conds = [:v, :r, :s, :a, :W]
 
@@ -88,7 +91,7 @@ include("pde_models.jl") #Includes all utilities for diffusion
 include("1D_models.jl") #Includes all 1D models
 include("2D_models.jl") #Includes all 2D models
 
-@register I_n(g_n, R, v, E_n) 
+@register I_n(g_n, R, v, E_n)
 @register M_INF(v, V1, V2)
 @register N_INF(v, V3, V4)
 @register H_INF(v, V5, V6)
@@ -97,8 +100,8 @@ include("2D_models.jl") #Includes all 2D models
 @register ħ(e, k_d)
 
 #Noise models
-noise(du::Array{T,1}, u::Array{T,1}, p, t::T) where T <: Real = du[end] = p[end]
-noise(du::Array{T,3}, u::Array{T,3}, p, t::T) where T <: Real = du[:,:,end] .= p[end]
-noise(du::CuArray{T,3}, u::CuArray{T,3}, p, t::T) where T <: Real = du[:,:,end] .= p[end]
+noise(du::Array{T,1}, u::Array{T,1}, p, t::T) where {T<:Real} = du[end] = p[end]
+noise(du::Array{T,3}, u::Array{T,3}, p, t::T) where {T<:Real} = du[:, :, end] .= p[end]
+noise(du::CuArray{T,3}, u::CuArray{T,3}, p, t::T) where {T<:Real} = du[:, :, end] .= p[end]
 
-lansdell_noise(du, u, p, t) = du[:,:,end] .= p[end]
+lansdell_noise(du, u, p, t) = du[:, :, end] .= p[end]
