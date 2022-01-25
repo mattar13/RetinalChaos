6acae77ee22e48cfd4727fbe040bbb1b8e78d64f
Matthew Tarchick <mtarchick13@gmail.com>
2022-01-25 17:22:59 -0500

    updated 1D models

--- src/1D_models.jl
+++ src/1D_models.jl
@@ -97,7 +97,40 @@ function GABA_ODE(dU::AbstractArray{T2}, U::AbstractArray{T2}, p::AbstractArray{
      di = view(dU, 7)
      dW = view(dU, 8)
 
-     @. dv = (-g_leak*(v - E_leak) - g_Ca*R_INF(v, V1, V2)*(v - E_Ca) - g_K*n*(v - E_K) - g_TREK*b*(v - E_K) - g_ACh*ħ(e, k_ACH)*(v - E_ACh) - g_GABA*ħ(i, k_GABA)*(v - E_GABA) + I_app) / C_m
+     @. dv = (-g_leak*(v - E_leak) - g_Ca*R_INF(v, V1, V2)*(v - E_Ca) - g_K*n*(v - E_K) - g_TREK*b*(v - E_K) - g_ACh*ħ(e, k_ACh)*(v - E_ACh) - g_GABA*ħ(i, k_GABA)*(v - E_GABA) + I_app) / C_m
+     @. dn = (Λ(v, V3, V4) * ((R_INF(v, V3, V4) - n))) / τn
+     @. dc = (C_0 + δ * (-g_Ca * R_INF(v, V1, V2) * (v - E_Ca)) - λ * c) / τc
+     @. da = (α * c^4 * (1 - a) - a) / τa
+     @. db = (β * a^4 * (1 - b) - b) / τb
+     @. de = (ρe * Φ(v, ke, V0e) - e) / τACh
+     @. di = (ρi * Φ(v, ki, V0i) - i) / τGABA
+     @. dW = -W / τw
+     #nothing
+     dU
+end
+
+function GABA_SDE(dU::AbstractArray{T2}, U::AbstractArray{T2}, p::AbstractArray{T}, t::T) where {T<:Real,T2}
+     #Extract all of the parameters first
+     (g_leak, E_leak, g_Ca, V1, V2, E_Ca, g_K, E_K, g_TREK, g_ACh, k_ACh, E_ACh, g_GABA, k_GABA, E_GABA, I_app, C_m, V3, V4, τn, C_0, λ, δ, τc, α, τa, β, τb, ρe, ρi, τACh, τGABA, ke, ki, V0e, V0i, D, τw, σ) = p
+     v = view(U, 1)
+     n = view(U, 2)
+     c = view(U, 3)
+     a = view(U, 4)
+     b = view(U, 5)
+     e = view(U, 6)
+     i = view(U, 7)
+     W = view(U, 8)
+
+     dv = view(dU, 1)
+     dn = view(dU, 2)
+     dc = view(dU, 3)
+     da = view(dU, 4)
+     db = view(dU, 5)
+     de = view(dU, 6)
+     di = view(dU, 7)
+     dW = view(dU, 8)
+
+     @. dv = (-g_leak * (v - E_leak) - g_Ca * R_INF(v, V1, V2) * (v - E_Ca) - g_K * n * (v - E_K) - g_TREK * b * (v - E_K) - g_ACh * ħ(e, k_ACh) * (v - E_ACh) - g_GABA * ħ(i, k_GABA) * (v - E_GABA) + I_app + W) / C_m
      @. dn = (Λ(v, V3, V4) * ((R_INF(v, V3, V4) - n))) / τn
      @. dc = (C_0 + δ * (-g_Ca * R_INF(v, V1, V2) * (v - E_Ca)) - λ * c) / τc
      @. da = (α * c^4 * (1 - a) - a) / τa
