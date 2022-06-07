23ea0303500ecaaad4e1ef173088da66e57ade80
Matthew Tarchick <mtarchick13@gmail.com>
2022-06-07 11:45:01 -0400

    updated debugging

--- test/debugging.jl
+++ test/debugging.jl
@@ -1,16 +1,16 @@
 using Revise
-using RetinalChaos #Load the package
-using BSON, JLD2
-#%%Lets figure out how to extract the waves
-import RetinalChaos.param_path
+using RetinalChaos
+using ABFReader
 
-u_dict = read_JSON(Dict{Symbol, Float32}, "$(param_path)/conds.json")
-p_dict = read_JSON(Dict{Symbol, Float32}, "$(param_path)/params.json")
+#We need to double check the properties of the physiological data
+
+
+
 
 #%% lets try to make a save callback
 results = RetinalChaos.SavedValues(Float64, Vector{Float64})
 cb = RetinalChaos.SavingCallback(
-    (u, t, integrator) -> reshape(u[:,:,1], size(u,1) * size(u,2)), 
+    (u, t, integrator) -> reshape(u[:, :, 1], size(u, 1) * size(u, 2)),
     results
 )
 
@@ -18,17 +18,17 @@ p_dict[:t_warm] = 300e3
 p_dict[:t_run] = 1000.0
 p0 = p_dict |> extract_dict
 u0 = extract_dict(u_dict, p_dict[:nx], p_dict[:ny]) |> cu
-gpu = true 
+gpu = true
 version = :gACh
-net = Network(p_dict[:nx], p_dict[:ny]; μ = p_dict[:μ], version = version, gpu = gpu)
-NetProb = SDEProblem(net, noise, u0, (0f0 , p_dict[:t_warm]), p0)
+net = Network(p_dict[:nx], p_dict[:ny]; μ=p_dict[:μ], version=version, gpu=gpu)
+NetProb = SDEProblem(net, noise, u0, (0.0f0, p_dict[:t_warm]), p0)
 print("[$(now())]: Warming up the solution... ")
 
-@time sol = solve(NetProb, SOSRI(), 
-    callback = cb, 
-    abstol = 2e-2, reltol = 2e-2, maxiters = 1e7,
-    progress = true, progress_steps = 1, 
-    save_everystep = false
+@time sol = solve(NetProb, SOSRI(),
+    callback=cb,
+    abstol=2e-2, reltol=2e-2, maxiters=1e7,
+    progress=true, progress_steps=1,
+    save_everystep=false
 )
 #We can the null out the solution because GPU is expensive
 #%%
@@ -41,33 +41,33 @@ duration = 10.0
 level = 1.0
 cb = RetinalChaos.IC_callback(step_begin, duration, level)
 
-prob = SDEProblem(T_sde, noise, u0 , tspan, p0, callback = cb);
-@time sol = solve(prob, SOSRI(), progress = true)
-plot(sol, vars = 1)
+prob = SDEProblem(T_sde, noise, u0, tspan, p0, callback=cb);
+@time sol = solve(prob, SOSRI(), progress=true)
+plot(sol, vars=1)
 #%% Set up a ensemble function
 n_sims = 50
 test_rng = repeat([p[:σ]], n_sims)
 prob_func(prob, i, repeat) = ensemble_func(prob, i, repeat, :σ, test_rng)
 #make the ensemble problem and only save the voltage
-simProb = EnsembleProblem(prob, prob_func = prob_func);
+simProb = EnsembleProblem(prob, prob_func=prob_func);
 #%%
-@time sim = solve(simProb, trajectories = n_sims, save_idxs = 1, EnsembleThreads(), callback = i -> cb_i(i));
+@time sim = solve(simProb, trajectories=n_sims, save_idxs=1, EnsembleThreads(), callback=i -> cb_i(i));
 
 #%%
-plot(sim[1], vars = 1)
+plot(sim[1], vars=1)
 #%%
-plot(sol, vars = 1)
-plot!([step_begin], st = :vline)
-plot!([step_begin+duration], st = :vline)
-plot!([step_begin2], st = :vline)
-plot!([step_begin2+duration2], st = :vline)
+plot(sol, vars=1)
+plot!([step_begin], st=:vline)
+plot!([step_begin + duration], st=:vline)
+plot!([step_begin2], st=:vline)
+plot!([step_begin2 + duration2], st=:vline)
 
 
 #%%
 load_path = "F:\\Data\\Modelling\\mu_0\\"
-p_dict = read_JSON("$(load_path)\\params.json", is_type = Dict{Symbol, Float32})
-u_dict = read_JSON("$(load_path)\\iconds.json", is_type = Dict{Symbol, Float32})
-sol = load_model(load_path, p_dict, u_dict, gpu = false)
+p_dict = read_JSON("$(load_path)\\params.json", is_type=Dict{Symbol,Float32})
+u_dict = read_JSON("$(load_path)\\iconds.json", is_type=Dict{Symbol,Float32})
+sol = load_model(load_path, p_dict, u_dict, gpu=false)
 #%%
 RetinalChaos.save_solution(sol, load_path)
 #%%
