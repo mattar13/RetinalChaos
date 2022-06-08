d535b6b6c50807dbf57e5af676bedcabc0cccd7c
Matthew Tarchick <mtarchick13@gmail.com>
2022-06-08 01:42:24 -0400

    added some figures to fitting_GABA_params

--- test/fitting_GABA_params.jl
+++ test/fitting_GABA_params.jl
@@ -1,9 +1,7 @@
 using Revise
 using RetinalChaos #Load the package
 using LinearAlgebra
-using StaticArrays
 using Plots
-#%%Lets figure out how to extract the waves
 import RetinalChaos: GABA_conds, GABA_pars, T_ODE, GABA_ODE, GABA_SDE
 
 #%% Lets run a baseline experiment to compare traces
@@ -12,7 +10,7 @@ u0 = extract_dict(conds_dict_GABA, GABA_conds)
 
 #Step 2: Import the parameters
 pars_dict = read_JSON("params/GABA_params.json")
-pars_dict[:g_GABA] = 15.0
+pars_dict[:g_GABA] = 5.0
 p_GABA = extract_dict(pars_dict, GABA_pars)
 
 pars_dict[:g_GABA] = 0.0 #Basically removes the influence of the GABA receptors
@@ -22,16 +20,22 @@ tspan = (0.0, 300e3);
 
 #Step 4 Set up the two problems
 prob = ODEProblem(GABA_ODE, u0, tspan, p); #1 is the Plain ODE problem
-probGABA = ODEProblem(GABA_ODE, u0, tspan, p_GABA); #1 is the Plain ODE problem
+probGABA = ODEProblem(GABA_ODE, u0, tspan, p_GABA); #2 is the GABA ODE problem
+probSDE = SDEProblem(GABA_SDE, noise, u0, tspan, p); #3 is the Plain SDE problem
+probSDE_GABA = SDEProblem(GABA_SDE, noise, u0, tspan, p_GABA); #4 is the GABA SDE problem
 
 @time sol = solve(prob, progress=true);
 @time solGABA = solve(probGABA, progress=true);
+@time solSDE = solve(probSDE, SOSRI());
+@time solSDE_GABA = solve(probSDE_GABA, SOSRI());
 
 # Plotting results
-plt_a = plot(sol, vars=[1, 6, 7], layout=(3, 1))
-plot!(plt_a, solGABA, vars=[1, 6, 7], layout=(3, 1))
+plt_a = plot(sol, vars=[1, 8], layout=(2, 1))
+plot!(plt_a, solGABA, vars=[1, 8], layout=(2, 1))
+plt_b = plot(solSDE, vars=[1, 8], layout=(2, 1))
+plot!(plt_b, solSDE_GABA, vars=[1, 8], layout=(2, 1))
 
-
+plot(plt_a, plt_b)
 #%% Lets run a range of different g_GABA params
 n = 10
 probGABA = ODEProblem(GABA_ODE, u0, tspan, p_GABA); #1 is the Plain ODE problem
@@ -51,51 +55,37 @@ for (sol_idx, sol_i) in enumerate(sim)
 end
 plot(plt_a, plt_b, layout=2)
 
-#%% Lets run this as a Stochastic problem
-conds_dict_GABA = read_JSON("params/GABA_conds.json")
-u0 = extract_dict(conds_dict_GABA, GABA_conds)
+#%% Now lets run through several parameters using the wave model
+b = Binomial(1, 0.65) #We take these two things out so the NUllification is constant
+null = Array{Float64}(rand(b, nx, ny))
+param = :g_GABA
+for val in LinRange(5.0, 20.0, 10) #This is the range of values
+     print("[$(now())]: Setting up binomal nullification... ")
+     println(" [$(now())]: Completed")
+     net = (dU, U, p, t) -> GABA_PDE_gNULL(dU, U, p, t, null)
+     print("[$(now())]: Loading Parameters... ")
+     conds_dict = read_JSON("params/GABA_conds.json")
+     u0 = extract_dict(conds_dict, GABA_conds, dims=(nx, ny))
+     pars_dict = read_JSON("params/GABA_params.json")
+     #CHANGE ANY PARAMETER HERE
+     pars_dict[param] = val
+     p = extract_dict(pars_dict, GABA_pars)
+     tspan = (0.0, 60e3)
+     println(" [$(now())]: Completed")
+     print("[$(now())]: Running Model... ")
+     prob = SDEProblem(net, noise, u0, tspan, p)
+     @time NetSol = solve(prob, SOSRI(), abstol=2e-2, reltol=0.2, maxiters=1e7, progress=true, progress_steps=1, save_idxs=[1:(nx*ny)...])
+     println(" [$(now())]: Completed")
 
-pars_dict = read_JSON("params/GABA_params.json")
-pars_dict[:g_GABA] = 13
-p_GABA = extract_dict(pars_dict, GABA_pars)
-pars_dict[:g_GABA] = 0.0 #Basically removes the influence of the GABA receptors
-p = extract_dict(pars_dict, GABA_pars)
-tspan = (0.0, 300e3);
-
-probSDE = SDEProblem(GABA_SDE, noise, u0, tspan, p); #1 is the Plain ODE problem
-probSDE_GABA = SDEProblem(GABA_SDE, noise, u0, tspan, p_GABA); #1 is the Plain ODE problem
-
-@time solSDE = solve(probSDE, SOSRI());
-@time solSDE_GABA = solve(probSDE_GABA, SOSRI());
-
-plt_a = plot(solSDE, vars=[1, 6, 7, 8], layout=(4, 1))
-plot!(plt_a, solSDE_GABA, vars=[1, 6, 7, 8], layout=(4, 1))
-
-
-#%% We need to adjust the diffusion stencils
-import RetinalChaos.GABA_PDE
-nx = ny = 50;
-p_dict = read_JSON(params_file) #set up parameters
-p_dict[:g_GABA] = 1.2
-u_dict = read_JSON(conds_file) #Set up the initial conditions
-p_net = extract_dict(p_dict, GABA_pars)
-u0_net = extract_dict(u_dict, GABA_conds, (nx, ny))
-tspan = (0.0, 120e3)
-probPDE = SDEProblem(GABA_PDE, noise, u0_net, tspan, p_net)
-@time NetSol = solve(probPDE, SOSRI(),
-     abstol=2e-2, reltol=0.2, maxiters=1e7,
-     save_idxs=[1:(nx*ny)...],
-     progress=true, progress_steps=1
-)
-
-#%%
-dFrame = 100.0
-anim = @animate for t in NetSol.t[1]:dFrame:NetSol.t[end]
-     println(t)
-     frame = reshape(NetSol(t), (nx, ny))
-     plot(frame, st=:heatmap, ratio=:equal, c=:jet, clims=(-70.0, 10.0))
+     # Step 7: Animate the solution
+     print("[$(now())]: Animating solution... ")
+     loc = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\Modelling\GABA_experiments"
+     animate_dt = 60.0
+     anim = @animate for t = 1.0:animate_dt:NetSol.t[end]
+          println("[$(now())]: Animating simulation...")
+          frame_i = reshape(NetSol(t) |> Array, (nx, ny))
+          heatmap(frame_i, ratio=:equal, grid=false, xaxis="", yaxis="", xlims=(0, nx), ylims=(0, ny), c=:curl, clims=(-70.0, 0.0))
+     end
+     gif(anim, "$(loc)\\g_GABA_$(round(val, digits = 1))animation.gif", fps=1000.0 / animate_dt)
+     println(" [$(now())]: Completed")
 end
-
-gif(anim, fps=10.0)
-
-plot(Array(NetSol)[2, :])
