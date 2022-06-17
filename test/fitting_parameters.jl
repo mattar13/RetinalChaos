aaa49de016ae1205feedb7ee3b7a077adf779503
Matthew Tarchick <mtarchick13@gmail.com>
2022-06-17 11:56:54 -0400

    making a new script for fitting parameters

--- test/fitting_parameters.jl
+++ test/fitting_parameters.jl
@@ -1,48 +1,11 @@
 using Revise
 using RetinalChaos, NeuroPhys
 
-#%% Lets load a file from google drive
-using GoogleDrive
-drive_link = "https://drive.google.com/drive/folders/1AmUCVDjMbmg-zF_Me89F1PdV9Yxqt7C0?usp=sharing"
-target_file = "E:\\Data\\Patching\\2019_11_03_Patch\\Animal_2\\Cell_3\\"
-google_download(drive_link, target_file)
+#These parameters are the most likely to need to be fitted: 
+#1) gGABA = [0.7 -> 1.0]
+#2) gACh = [0.1 -> 0.215]
+#2) gCa = [7.5 -> 8.0]
+#3) gK = [3.0 -> 5.0]
+#4) μ = [0.7-0.8]
 
-
-
-#%% Load some data
-target_file = "E:\\Data\\Patching\\2019_11_03_Patch\\Animal_2\\Cell_3\\19n03042.abf"
-data = extract_abf(target_file)
-#Conduct the timescale analysis on this file
-#reduce the size of the file
-truncate_data!(data, 
-     t_pre = 140.0, t_post = 200.0, 
-     truncate_based_on = :time_range)
-data-25
-plot(data)
-#%%
-
-#%% Now simulate some traces
-p = read_JSON("params/params.json") 
-u0 = read_JSON("params/conds.json")
-tspan = (0.0, 120e3);
-prob_sde = SDEProblem(T_sde, noise, u0|>extract_dict, tspan, p|>extract_dict);
-#Inject a current
-#p[:I_app] = 0.42
-prob_basic = ODEProblem(T_ode, u0|>extract_dict, tspan, p|>extract_dict)
-sol_sde = solve(prob_sde, progress = true)
-sol_basic = solve(prob_basic, progress = true)
-#%%
-plot(sol_sde, vars = [1])
-plot!(sol_basic, vars = [1])
-#%%
-n = 10
-prob_func(prob, i, repeat) = ensemble_func(prob, i, repeat, :n, LinRange(0.0, 1.0, n))
-ensemble_prob = EnsembleProblem(prob_sde, prob_func = prob_func);
-@time sim = solve(ensemble_prob, trajectories = n, EnsembleThreads());
-
-#%%
-plt = plot()
-for sol in sim
-     plot!(plt, sol)
-end
-plt
+#How does each parameter change the dataspace
