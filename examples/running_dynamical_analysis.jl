ae23c887a8f545b48dd774509b2b5c3b77b7d19e
Matthew Tarchick <mtarchick13@gmail.com>
2022-06-27 10:22:47 -0400

    changed some dynamical analysis parameters

--- examples/running_dynamical_analysis.jl
+++ examples/running_dynamical_analysis.jl
@@ -11,6 +11,7 @@ pars_dict = read_JSON("params\\params.json")
 pars_dict[:I_app] = 10.0
 pars_dict[:ρe] = 0.0
 pars_dict[:ρi] = 0.0
+pars_dict[:g_TREK] = 0.0 #Remove the sAHP
 p = pars_dict |> extract_dict #Parameters
 tspan = (0.0, 2e3) #Timespan
 prob = ODEProblem(T_ODE, u0, tspan, p) #ODE problem
@@ -18,22 +19,22 @@ prob = ODEProblem(T_ODE, u0, tspan, p) #ODE problem
 #Step 2: Determine the number of trajectories and the parameter to adjust
 n_trajectories = 10
 par_idx = p_find(:g_K; list_p=t_pars); #Point to the index of the parameter
-test_rng = LinRange(3.0, 5.0, n_trajectories); #Determine the range of the parameters (specified above)
+test_rng = LinRange(3.0, 3.5, n_trajectories); #Determine the range of the parameters (specified above)
 
 #Step 3: Set up the ensemble problem
 prob_func(prob, i, repeat) = ensemble_func(prob, i, repeat, par_idx, test_rng) #Set up the problem function to indicate that the voltage will be altered
 ensemble_prob = EnsembleProblem(prob, prob_func=prob_func); #Set up the problem
 
 #Step 4: Run the simulation
-@time sim = solve(ensemble_prob, saveat = 0.10, trajectories=n_trajectories, EnsembleThreads());
+@time sim = solve(ensemble_prob, saveat=0.10, trajectories=n_trajectories, EnsembleThreads());
 
 #[OPTIONAL]: Plot the solutions 
 plt_a = plot(sim[1], vars=[1], c=:jet, line_z=1, clims=(test_rng[1], test_rng[end]))
-plt_b = plot(sim[1], vars=(1, 2), c=:jet, line_z=1, xlims = (-70.0, 10.0), clims=(test_rng[1], test_rng[end]))
+plt_b = plot(sim[1], vars=(1, 2), c=:jet, line_z=1, xlims=(-70.0, 10.0), clims=(test_rng[1], test_rng[end]))
 for (sol_idx, sol_i) in enumerate(sim)
     println(test_rng[sol_idx])
     plot!(plt_a, sol_i, vars=[1], c=:jet, line_z=test_rng[sol_idx], clims=(test_rng[1], test_rng[end]), legend=false)
-    plot!(plt_b, sol_i, vars=(1, 2), c=:jet, xlims = (-70.0, 10.0), line_z=test_rng[sol_idx], clims=(test_rng[1], test_rng[end]), legend=false)
+    plot!(plt_b, sol_i, vars=(1, 2), c=:jet, xlims=(-70.0, 10.0), line_z=test_rng[sol_idx], clims=(test_rng[1], test_rng[end]), legend=false)
 end
 plot(plt_a, plt_b, layout=2)
 
@@ -44,25 +45,43 @@ pars_dict = read_JSON("params\\params.json")
 pars_dict[:I_app] = 10.0 #Set initial applied current to 0
 pars_dict[:ρi] = 0.0 #remove GABA influence
 pars_dict[:ρe] = 0.0 #remove ACh influence
+pars_dict[:g_K] = 3.2
 pars_dict[:g_TREK] = 0.0 #Remove the sAHP
 p = pars_dict |> extract_dict
 
 #Step 3: determine the timespan
-tspan = (0.0, 2e3);
+tspan = (0.0, 300e3);
 
 #Step 4: set up the equilibria problem
 prob_eq = ODEProblem(T_ODE, u0, tspan, p)
-prob_sde = SDEProblem(T_SDE, noise, u0, tspan, p);
+sol = solve(prob_eq, progress=true, progress_steps=1)
+plot(sol, vars=(1, 2), plotdensity=Int64(1e6))
+
+#%% Conduct the equilibria analysis
 @time eq_analysis = find_equilibria(prob_eq)
 print(eq_analysis)
+plot(eq_analysis)
 
+@time eq_analysis = find_equilibria(prob_eq, equilibrium_resolution = 9)
+plot(eq_analysis)
+
+
+print(eq_analysis)
+plot(eq_analysis)
 #%% Step 5: Running a Codimensional-1 analysis
 codim1 = (:I_app)
 print("Codimensional analysis over parameter $codim1")
-c1_lims = (-4.0, 10.0)
+c1_lims = (-10.0, 200.0)
 print("On parameter range: $c1_lims beginning:")
 @time c1_map = codim_map(prob_eq, codim1, c1_lims, equilibrium_resolution=10)
 println("Complete")
+
+#%% Plot the bifurcation analysis
+eq_plot = plot(c1_map, xlabel="Injected Current", ylabel="Membrane Voltage")
+bif_val, bif_eq = find_bifurcation(c1_map)
+saddle_vs = map(x -> x.saddle[1][1], bif_eq)
+plot!(eq_plot, bif_val, saddle_vs, marker=:square, c=:blue, seriestype=:scatter, label="Saddle node bif")
+
 
 #%% Step 6: Run out all of the solutions contained in the codimensional analysis
 prob_eq = ODEProblem(T_ODE, u0, tspan, p) #First we need to reset the function
@@ -71,15 +90,9 @@ par_idx = codim1 |> p_find #Point to the index of the parameter
 prob_func(prob, i, repeat) = ensemble_func(prob, i, repeat, par_idx, test_rng) #Set up the problem function to indicate that the voltage will be altered
 ensemble_prob = EnsembleProblem(prob_eq, prob_func=prob_func); #Set up the problem
 print("Running a ensemble simulation for :")
-@time sim = solve(ensemble_prob, trajectories=length(test_rng), EnsembleThreads(), progress = true, progress_steps = 1);
+@time sim = solve(ensemble_prob, trajectories=length(test_rng), EnsembleThreads(), progress=true, progress_steps=1);
 
-#%% Plot the bifurcation analysis
-eq_plot = plot(c1_map, xlabel="Injected Current", ylabel="Membrane Voltage")
-bif_val, bif_eq = find_bifurcation(c1_map)
-println(bif_val)
-saddle_vs = map(x -> x.saddle[1][1], bif_eq)
-plot!(eq_plot, bif_val, saddle_vs, marker=:square, c=:blue, seriestype=:scatter, label="Saddle node bif")
-
+#%% Plot Codim Solutions
 for (sol_idx, sol) in enumerate(sim)
     dt = 100.0
     vt = map(t -> sol(t)[1], collect(5e3:dt:tspan[2]))
@@ -89,9 +102,9 @@ end
 eq_plot
 
 #%% Step 6 Codim 2 analysis
-codim2 = (:I_app, :g_Ca)
-c1_lims = (-60.0, 50.0);
-c2_lims = (0.0, 20.0);
+codim2 = (:g_K, :g_Ca)
+c1_lims = (1.0, 5.0);
+c2_lims = (5.0, 15.0);
 print("Codimensional analysis time to complete:")
 @time c2_map = codim_map(prob_eq, codim2, c1_lims, c2_lims);
 #%% Find the 2D bifurcation
