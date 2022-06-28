0eb177c2a0cacd5cae051fd547b9c10139550663
Matthew Tarchick <mtarchick13@gmail.com>
2022-06-28 12:42:16 -0400

    added some more plotting options

--- examples/running_data_analysis.jl
+++ examples/running_data_analysis.jl
@@ -17,9 +17,41 @@ prob = SDEProblem(T_SDE, noise, u0, tspan, p)
 #%% Step 2: Conduct the analysis
 thresh = calculate_threshold(solSDE, idxs=-1)
 spike_tstamps = get_timestamps(solSDE, thresh, (solSDE.t[1], solSDE.t[end]), idxs=-1)
-spike_durs, isi = extract_interval(spike_tstamps)
+spike_durs, isi = extract_interval(spike_tstamps, max_duration=100, max_interval=100)
 burst_tstamps, SPB = max_interval_algorithim(spike_tstamps)
 burst_durs, ibi = extract_interval(burst_tstamps)
+
+#%% Lets plot things to see more
+plt_a = plot(solSDE, vars=[1]) #Plot the solution
+
+# Plot each parameter in a histogram
+plt_b = plot(layout=(2, 2))
+
+#Plot spike durations (Best range is )
+hfit = fit(Histogram, spike_durs, LinRange(1, 25, 50))
+weights = hfit.weights / maximum(hfit.weights)
+edges = collect(hfit.edges[1])[1:length(hfit.weights)]
+plot!(plt_b[1, 1], edges, weights)
+
+#Plot interspike interval
+hfit = fit(Histogram, isi, LinRange(1, 100, 50))
+weights = hfit.weights / maximum(hfit.weights)
+edges = collect(hfit.edges[1])[1:length(hfit.weights)]
+plot!(plt_b[1, 2], edges, weights)
+
+#Plot burst durations
+hfit = fit(Histogram, burst_durs, LinRange(100, 2000, 100))
+weights = hfit.weights / maximum(hfit.weights)
+edges = collect(hfit.edges[1])[1:length(hfit.weights)]
+plot!(plt_b[2, 1], edges, weights)
+
+#Plot burst durations
+hfit = fit(Histogram, ibi, LinRange(10e3, 100e3, 50))
+weights = hfit.weights / maximum(hfit.weights)
+edges = collect(hfit.edges[1])[1:length(hfit.weights)]
+plot!(plt_b[2, 2], edges, weights)
+
+plot(plt_a, plt_b, layout=(1, 2))
 
 #%% Running the data analysis on a 2D wave sim
 #1) Set up the model
@@ -42,58 +74,52 @@ prob = SDEProblem(T_PDE, noise, warmup[end], tspan, p) #Use the last solution fr
 @time solPDE = solve(prob, SOSRI(), abstol=2e-2, reltol=0.2, maxiters=1e7, progress=true, progress_steps=1)#, save_idxs=[1:(nx*ny)...])
 println("  [$(now())]: Completed")
 
-#%% Lets do a quick Animation
-using Plots
-t = solPDE.t
-thresh[50,50]
-spike_tstamps[50*50]
-vt = solPDE(t, idxs = 50*50)
-
-get_timestamps(vt, t)
-
-plot(t, vt)
- 
-
-reshape(spike_tstamps, nx, ny)
-#%% Debugging
-var = 1
-data_test = solPDE(1.0:100.0:5000.0, idxs=[1+(var-1)*(nx*ny):(var)*(nx*ny)...])
-data_test = reshape(data_test, nx, ny, 1, nt)
 #%% Step 2: Conduct the analysis
 thresh = calculate_threshold(solPDE, idxs=1)
 spike_tstamps = get_timestamps(solPDE, idxs=1)
-spike_durs, isi = extract_interval(spike_tstamps)
+spike_durs, isi = extract_interval(spike_tstamps, max_duration=100, max_interval=100)
 burst_tstamps, SPB = max_interval_algorithim(spike_tstamps)
 burst_durs, ibi = extract_interval(burst_tstamps)
 
+#Pull out an example trace
+t = solPDE.t
+vt1 = solPDE(t, idxs=1)
+vt2 = solPDE(t, idxs=10)
+vt3 = solPDE(t, idxs=100)
+vt4 = solPDE(t, idxs=1000)
 #%% Lets plot things to see more
-plt_a = plot(sol, vars=[1]) #Plot the solution
+plt_a = plot(t, vt1) #Plot the solution
+plot!(t, vt2)
+plot!(t, vt3)
+plot!(t, vt4)
 
-#%% Plot each parameter in a histogram
+# Plot each parameter in a histogram
 plt_b = plot(layout=(2, 2))
 
-#Plot spike durations
-hfit = fit(Histogram, spike_durs, LinRange(1, 10, 50))
+#Plot spike durations (Best range is )
+hfit = fit(Histogram, spike_durs, LinRange(1, 100, 50))
 weights = hfit.weights / maximum(hfit.weights)
 edges = collect(hfit.edges[1])[1:length(hfit.weights)]
 plot!(plt_b[1, 1], edges, weights)
 
 #Plot interspike interval
-hfit = fit(Histogram, isi, LinRange(1, 10, 50))
+hfit = fit(Histogram, isi, LinRange(1, 100, 50))
 weights = hfit.weights / maximum(hfit.weights)
 edges = collect(hfit.edges[1])[1:length(hfit.weights)]
 plot!(plt_b[1, 2], edges, weights)
 
 #Plot burst durations
-hfit = fit(Histogram, burst_durs, LinRange(1, 10, 50))
+hfit = fit(Histogram, burst_durs, LinRange(100, 2000, 100))
 weights = hfit.weights / maximum(hfit.weights)
 edges = collect(hfit.edges[1])[1:length(hfit.weights)]
-plot!(plt_b[1, 2], edges, weights)
+plot!(plt_b[2, 1], edges, weights)
 
 #Plot burst durations
-hfit = fit(Histogram, ibi, LinRange(1, 10, 50))
+hfit = fit(Histogram, ibi, LinRange(10e3, 100e3, 50))
 weights = hfit.weights / maximum(hfit.weights)
 edges = collect(hfit.edges[1])[1:length(hfit.weights)]
-plot!(plt_b[1, 2], edges, weights)
+plot!(plt_b[2, 2], edges, weights)
 
 plot(plt_a, plt_b, layout=(1, 2))
+
+
