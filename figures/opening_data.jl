238fdcdca510cef902ea2b79f3c0ba061f3c05fe
Matthew Tarchick <mtarchick13@gmail.com>
2022-08-01 23:24:03 -0400

    updated opening data

--- figures/opening_data.jl
+++ figures/opening_data.jl
@@ -1,5 +1,9 @@
 #%% Walk through each file analyzing using timescale analysis
-max_spike_duration = 50.0
+spike_dur = 25
+burst_dur = 1000
+IBI_dur = 60e3
+
+max_spike_duration = 100.0
 max_spike_interval = 100.0
 max_burst_duration = 10e5
 max_burst_interval = 10e5
@@ -21,16 +25,17 @@ for path in paths[good_paths]
           stimulus_name=nothing, flatten_episodic=true
      )
      data_i = downsample(data_raw, 1 / 1.0) #downsample the data
-     data_i = highpass_filter(data_i, freq=0.01)
-     timestamps, data = timeseries_analysis(data_i)
-
+     #These have to be done before highpass filtering the data
      push!(
           phys_baseline,
           sum(data_i.data_array[1, :, 1]) / length(data_i.data_array[1, :, 1])
      )
-
      push!(phys_min, minimum(data_i))
      push!(phys_max, maximum(data_i))
+
+     data_i = highpass_filter(data_i, freq=0.01)
+     timestamps, data = timeseries_analysis(data_i)
+
      #println(min_phys)
 
      push!(phys_spike_durs, data["SpikeDurs"]...)
@@ -40,13 +45,14 @@ for path in paths[good_paths]
 
      println("Success")
 end
-
+#Filter out any spike duration under 5.0
+phys_spike_durs = phys_spike_durs[phys_spike_durs.>5.0]
 #%% Extract all of the histograms
-sdur_hfit = fit(Histogram, phys_spike_durs, LinRange(0.0, 50.0, 50))
+sdur_hfit = fit(Histogram, phys_spike_durs, LinRange(0.0, 100.0, 50))
 sdur_weights = sdur_hfit.weights / maximum(sdur_hfit.weights)
 sdur_edges = collect(sdur_hfit.edges[1])[1:length(sdur_weights)]
 
-isi_hfit = fit(Histogram, phys_isis, LinRange(0.0, 50.0, 50))
+isi_hfit = fit(Histogram, phys_isis, LinRange(0.0, 100.0, 50))
 isi_weights = isi_hfit.weights / maximum(isi_hfit.weights)
 isi_edges = collect(isi_hfit.edges[1])[1:length(isi_weights)]
 
@@ -63,141 +69,184 @@ n_recordings = length(paths)
 
 #Average the stats
 phys_baseline_avg = sum(phys_baseline) / n_recordings
-phys_baseline_sem = std(phys_baseline) / sqrt(n_recordings)
+phys_baseline_SEM = std(phys_baseline) / sqrt(n_recordings)
 
 phys_min_avg = sum(phys_min) / n_recordings
-phys_min_sem = std(phys_min) / sqrt(n_recordings)
+phys_min_SEM = std(phys_min) / sqrt(n_recordings)
 
 phys_max_avg = sum(phys_max) / n_recordings
-phys_max_sem = std(phys_max) / sqrt(n_recordings)
+phys_max_SEM = std(phys_max) / sqrt(n_recordings)
 
 phys_spike_avg = sum(phys_spike_durs) / length(phys_spike_durs)
-phys_spike_sem = std(phys_spike_durs) / sqrt(length(phys_spike_durs))
+phys_spike_SEM = std(phys_spike_durs) / sqrt(length(phys_spike_durs))
 
 phys_burst_avg = sum(phys_burst_durs) / length(phys_burst_durs)
-phys_burst_sem = std(phys_burst_durs) / sqrt(length(phys_burst_durs))
+phys_burst_SEM = std(phys_burst_durs) / sqrt(length(phys_burst_durs))
 
 phys_ibis_avg = sum(phys_ibis) / length(phys_ibis)
-phys_ibis_sem = std(phys_ibis) / sqrt(length(phys_ibis))
+phys_ibis_SEM = std(phys_ibis) / sqrt(length(phys_ibis))
 
 #%%1) open the physiological data loaded from the single cell recordings I made
+print("Opening physiological data... ")
 dt = 0.5
 file_loc = "C:/Users/mtarc/OneDrive - The University of Akron/Data/Patching"
 target_file = "$(file_loc)/2019_11_03_Patch/Animal_2/Cell_3/19n03042.abf"
-data = readABF(target_file, channels=["Vm_prime4"], stimulus_name=nothing, time_unit=:ms)
-#data - 25.0
-example_timestamps, example_data = timeseries_analysis(data)
-ex_bursts = example_timestamps["Bursts"][1]
+dataŶ  = readABF(target_file, channels=["Vm_prime4"], stimulus_name=nothing, time_unit=:ms)
+downsample!(dataŶ, 1 / dt)
+tsPHYS, dataPHYS = timeseries_analysis(dataŶ)
+println("Complete")
 
-spike_t_PHYS, spike_vt_PHYS = extract_spike_trace(tsPHYS, dataPHYS, idx=3, dt=dt, normalize=false)
+print("Calculating the loss... ")
+spike_t_PHYS, spike_vt_PHYS = extract_spike_trace(tsPHYS, dataPHYS, idx=5, dt=dt, normalize=false)
 burst_t_PHYS, burst_vt_PHYS = extract_burst_trace(tsPHYS, dataPHYS, idx=2, dt=dt, normalize=false)
 IBI_t_PHYS, IBI_vt_PHYS = extract_IBI_trace(tsPHYS, dataPHYS, idx=2, dt=dt, normalize=false)
-
+println("Complete")
 #%% Open all of the data for the wave models
 data_root = "C:/Users/mtarc/OneDrive - The University of Akron/Data/Modelling/figure_data"
-region = (-100, 2500)
 #eXTRACT THE ISOLATED PATH 
 isolated_path = "$(data_root)/isolated_model"
-isolated_data = load("$(isolated_path)/data.jld2")
-isolated_timestamps = load("$(isolated_path)/timestamps.jld2")
+dataISO = load("$(isolated_path)/data.jld2")
+tsISO = load("$(isolated_path)/timestamps.jld2")
+tsISO = convert(Dict{String,Vector{Matrix{Float64}}}, tsISO)
 
-iso_bursts = isolated_timestamps["Bursts"]
-#iso_xIdx = rand(findall(iso_bursts .!= nothing))
-iso_xIdx = 3213
+#iso_xIdx = rand(findall(map(x -> size(x, 1) > 1, isolated_timestamps["Bursts"])))
+iso_xIdx = 636
+spike_t_ISO, spike_vt_ISO = extract_spike_trace(tsISO, dataISO, normalize=false, cell_n=iso_xIdx, idx=1)
+burst_t_ISO, burst_vt_ISO = extract_burst_trace(tsISO, dataISO, normalize=false, cell_n=iso_xIdx, idx=1)
+IBI_t_ISO, IBI_vt_ISO = extract_IBI_trace(tsISO, dataISO, normalize=false, cell_n=iso_xIdx, idx=1)
 
-spike_t_ISO, spike_vt_ISO = extract_spike_trace(isolated_timestamps, isolated_data, normalize=false, cell_n=iso_xIdx, idx=1)
-burst_t_ISO, burst_vt_ISO = extract_burst_trace(isolated_timestamps, isolated_data, normalize=false, cell_n=iso_xIdx, idx=1)
-IBI_t_ISO, IBI_vt_ISO = extract_IBI_trace(isolated_timestamps, isolated_data, normalize=false, cell_n=iso_xIdx, idx=1)
+print("Calculating loss of Isolated data... ")
 
-spikeMSE_ISO = SpikeLoss(example_timestamps, example_data, isolated_timestamps, isolated_data, normalize=true)
-burstMSE_ISO = BurstLoss(example_timestamps, example_data, isolated_timestamps, isolated_data, normalize=true)
-IBIMSE_ISO = IBILoss(example_timestamps, example_data, isolated_timestamps, isolated_data, normalize=true)
+#spikeMSE_ISO = IntervalLoss(tsPHYS, dataPHYS, tsISO, dataISO, tstamps="Spikes", duration=spike_dur, normalize=true)
+#burstMSE_ISO = IntervalLoss(tsPHYS, dataPHYS, tsISO, dataISO, tstamps="Bursts", duration=burst_dur, normalize=true)
+#IBIMSE_ISO = IntervalLoss(tsPHYS, dataPHYS, tsISO, dataISO, tstamps="Bursts", duration=IBI_dur, normalize=true)
+ISO_loss = TimescaleLoss(tsPHYS, dataPHYS, tsISO, dataISO)
+println("Complete")
 
-iso_sdur_hfit = fit(Histogram, isolated_data["SpikeDurs"], LinRange(0.0, 50.0, 50))
+
+iso_baselines = calculate_threshold(dataISO["DataArray"], Z=1.0, dims=2)
+iso_baseline_avg = sum(iso_baselines) / length(iso_baselines)
+iso_baseline_SEM = std(iso_baselines) / sqrt(length(iso_baselines))
+
+iso_min = minimum(dataISO["DataArray"], dims=2)
+iso_min_avg = sum(iso_min) / length(iso_min)
+iso_min_SEM = std(iso_min) / sqrt(length(iso_min))
+
+iso_max = maximum(dataISO["DataArray"], dims=2)
+iso_max_avg = sum(iso_max) / length(iso_max)
+iso_max_SEM = std(iso_max) / sqrt(length(iso_max))
+
+iso_sdur_hfit = fit(Histogram, dataISO["SpikeDurs"], LinRange(0.0, 100.0, 50))
 iso_sdur_weights = iso_sdur_hfit.weights / maximum(iso_sdur_hfit.weights)
 iso_sdur_edges = collect(iso_sdur_hfit.edges[1])[1:length(iso_sdur_weights)]
 
-iso_isi_hfit = fit(Histogram, isolated_data["ISIs"], LinRange(0.0, 50.0, 50))
+iso_isi_hfit = fit(Histogram, dataISO["ISIs"], LinRange(0.0, 100.0, 50))
 iso_isi_weights = iso_isi_hfit.weights / maximum(iso_isi_hfit.weights)
 iso_isi_edges = collect(iso_isi_hfit.edges[1])[1:length(iso_isi_weights)]
 
-iso_bdur_hfit = fit(Histogram, isolated_data["BurstDurs"], LinRange(0.0, 2000.0, 50))
+iso_bdur_hfit = fit(Histogram, dataISO["BurstDurs"], LinRange(0.0, 2000.0, 50))
 iso_bdur_weights = iso_bdur_hfit.weights / maximum(iso_bdur_hfit.weights)
 iso_bdur_edges = collect(iso_bdur_hfit.edges[1])[1:length(iso_bdur_weights)]
 
-iso_ibi_hfit = fit(Histogram, isolated_data["IBIs"], LinRange(0.0, 120000, 50))
+iso_ibi_hfit = fit(Histogram, dataISO["IBIs"], LinRange(0.0, 120000, 50))
 iso_ibi_weights = iso_ibi_hfit.weights / maximum(iso_ibi_hfit.weights)
 iso_ibi_edges = collect(iso_ibi_hfit.edges[1])[1:length(iso_ibi_weights)]
 
 #We need to run this one again and for a longer period
+print("Opening No GABA data... ")
 noGABA_path = "$(data_root)/no_GABA_model"
-noGABA_data = load("$(noGABA_path)/data.jld2")
-noGABA_timestamps = load("$(noGABA_path)/timestamps.jld2")
+dataNG = load("$(noGABA_path)/data.jld2")
+tsNG = load("$(noGABA_path)/timestamps.jld2")
+tsNG = convert(Dict{String,Vector{Matrix{Float64}}}, tsNG)
+#ng_xIdx = rand(findall(tsNG["Bursts"] .!= nothing))
+ng_xIdx = 2130
 
+spike_t_NG, spike_vt_NG = extract_spike_trace(tsNG, dataNG, normalize=false, cell_n=ng_xIdx, idx=1)
+burst_t_NG, burst_vt_NG = extract_burst_trace(tsNG, dataNG, normalize=false, cell_n=ng_xIdx, idx=1)
+IBI_t_NG, IBI_vt_NG = extract_IBI_trace(tsNG, dataNG, normalize=false, cell_n=ng_xIdx, idx=1)
+println("Complete")
 
-ng_bursts = noGABA_timestamps["Bursts"]
-ng_xIdx = rand(findall(ng_bursts .!= nothing))
-ng_xIdx = 4065
-ng_bursts[ng_xIdx]
-noGABA_data["Time"]
-noGABA_timestamps["Bursts"][ng_xIdx]
+print("Calculating loss of No GABA data... ")
+#spikeMSE_NG = IntervalLoss(tsPHYS, dataPHYS, tsNG, dataNG, tstamps="Spikes", duration=spike_dur, normalize=true)
+#burstMSE_NG = IntervalLoss(tsPHYS, dataPHYS, tsNG, dataNG, tstamps="Bursts", duration=burst_dur, normalize=true)
+#IBIMSE_NG = IntervalLoss(tsPHYS, dataPHYS, tsNG, dataNG, tstamps="Bursts", duration=IBI_dur, normalize=true)
+NG_loss = TimescaleLoss(tsPHYS, dataPHYS, tsNG, dataNG)
+println("Complete")
 
-spike_t_NG, spike_vt_NG = extract_spike_trace(noGABA_timestamps, noGABA_data, normalize=false, cell_n=ng_xIdx, idx=1)
-burst_t_NG, burst_vt_NG = extract_burst_trace(noGABA_timestamps, noGABA_data, normalize=false, cell_n=ng_xIdx, idx=1)
-IBI_t_NG, IBI_vt_NG = extract_IBI_trace(noGABA_timestamps, noGABA_data, normalize=false, cell_n=ng_xIdx, idx=1)
-spikeMSE_NG = SpikeLoss(example_timestamps, example_data, noGABA_timestamps, noGABA_data, normalize=true)
-burstMSE_NG = BurstLoss(example_timestamps, example_data, noGABA_timestamps, noGABA_data, normalize=true)
-IBIMSE_NG = IBILoss(example_timestamps, example_data, noGABA_timestamps, noGABA_data, normalize=true)
 #Calculate the MSE between each of the data points
-#spikeMSE_NG
+ng_baselines = calculate_threshold(dataNG["DataArray"], Z=1.0, dims=2)
+ng_baseline_avg = sum(ng_baselines) / length(ng_baselines)
+ng_baseline_SEM = std(ng_baselines) / sqrt(length(ng_baselines))
 
-noGABA_sdur_hfit = fit(Histogram, noGABA_data["SpikeDurs"], LinRange(0.0, 50.0, 50))
+ng_min = minimum(dataNG["DataArray"], dims=2)
+ng_min_avg = sum(ng_min) / length(ng_min)
+ng_min_SEM = std(ng_min) / sqrt(length(ng_min))
+
+ng_max = maximum(dataNG["DataArray"], dims=2)
+ng_max_avg = sum(ng_max) / length(ng_max)
+ng_max_SEM = std(ng_max) / sqrt(length(ng_max))
+
+noGABA_sdur_hfit = fit(Histogram, dataNG["SpikeDurs"], LinRange(0.0, 100.0, 50))
 noGABA_sdur_weights = noGABA_sdur_hfit.weights / maximum(noGABA_sdur_hfit.weights)
 noGABA_sdur_edges = collect(noGABA_sdur_hfit.edges[1])[1:length(noGABA_sdur_weights)]
 
-noGABA_isi_hfit = fit(Histogram, noGABA_data["ISIs"], LinRange(0.0, 50.0, 50))
+noGABA_isi_hfit = fit(Histogram, dataNG["ISIs"], LinRange(0.0, 100.0, 50))
 noGABA_isi_weights = noGABA_isi_hfit.weights / maximum(noGABA_isi_hfit.weights)
 noGABA_isi_edges = collect(noGABA_isi_hfit.edges[1])[1:length(noGABA_isi_weights)]
 
-noGABA_bdur_hfit = fit(Histogram, noGABA_data["BurstDurs"], LinRange(0.0, 2000.0, 50))
+noGABA_bdur_hfit = fit(Histogram, dataNG["BurstDurs"], LinRange(0.0, 2000.0, 50))
 noGABA_bdur_weights = noGABA_bdur_hfit.weights / maximum(noGABA_bdur_hfit.weights)
 noGABA_bdur_edges = collect(noGABA_bdur_hfit.edges[1])[1:length(noGABA_bdur_weights)]
 
-noGABA_ibi_hfit = fit(Histogram, noGABA_data["IBIs"], LinRange(0.0, 120000, 50))
+noGABA_ibi_hfit = fit(Histogram, dataNG["IBIs"], LinRange(0.0, 120000, 50))
 noGABA_ibi_weights = noGABA_ibi_hfit.weights / maximum(noGABA_ibi_hfit.weights)
 noGABA_ibi_edges = collect(noGABA_ibi_hfit.edges[1])[1:length(noGABA_ibi_weights)]
 
 
 # Extract the wave model
+print("Opening wave data... ")
 wave_path = "$(data_root)/wave_model"
-wave_data = load("$(wave_path)/data.jld2")
-wave_timestamps = load("$(wave_path)/timestamps.jld2")
-
-
-wave_bursts = wave_timestamps["Bursts"]
-wave_xIdx = rand(findall(wave_bursts .!= nothing))
+dataWAVE = load("$(wave_path)/data.jld2")
+tsWAVE = load("$(wave_path)/timestamps.jld2")
+tsWAVE = convert(Dict{String,Vector{Matrix{Float64}}}, tsWAVE)
+#wave_xIdx = rand(findall(tsWAVE["Bursts"] .!= nothing))
 wave_xIdx = 1838
 
-spike_t_WAVE, spike_vt_WAVE = extract_spike_trace(wave_timestamps, wave_data, normalize=false, cell_n=wave_xIdx, idx=1)
-burst_t_WAVE, burst_vt_WAVE = extract_burst_trace(wave_timestamps, wave_data, normalize=false, cell_n=wave_xIdx, idx=1)
-IBI_t_WAVE, IBI_vt_WAVE = extract_IBI_trace(wave_timestamps, wave_data, normalize=false, cell_n=wave_xIdx, idx=1)
-spikeMSE_WAVE = SpikeLoss(example_timestamps, example_data, wave_timestamps, wave_data, normalize=true)
-burstMSE_WAVE = BurstLoss(example_timestamps, example_data, wave_timestamps, wave_data, normalize=true)
-IBIMSE_WAVE = IBILoss(example_timestamps, example_data, wave_timestamps, wave_data, normalize=true)
+spike_t_WAVE, spike_vt_WAVE = extract_spike_trace(tsWAVE, dataWAVE, normalize=false, cell_n=wave_xIdx, idx=1)
+burst_t_WAVE, burst_vt_WAVE = extract_burst_trace(tsWAVE, dataWAVE, normalize=false, cell_n=wave_xIdx, idx=1)
+IBI_t_WAVE, IBI_vt_WAVE = extract_IBI_trace(tsWAVE, dataWAVE, normalize=false, cell_n=wave_xIdx, idx=1)
+println("Complete")
 
-wave_sdur_hfit = fit(Histogram, wave_data["SpikeDurs"], LinRange(0.0, 50.0, 50))
+#spikeMSE_WAVE = IntervalLoss(tsPHYS, dataPHYS, tsWAVE, dataWAVE, tstamps="Spikes", duration=spike_dur, normalize=true)
+#burstMSE_WAVE = IntervalLoss(tsPHYS, dataPHYS, tsWAVE, dataWAVE, tstamps="Bursts", duration=burst_dur, normalize=true)
+#IBIMSE_WAVE = IntervalLoss(tsPHYS, dataPHYS, tsWAVE, dataWAVE, tstamps="Bursts", duration=IBI_dur, normalize=true)
+WAVE_loss = TimescaleLoss(tsPHYS, dataPHYS, tsWAVE, dataWAVE)
+
+wave_baselines = calculate_threshold(dataWAVE["DataArray"], Z=1.0, dims=2)
+wave_baseline_avg = sum(wave_baselines) / length(wave_baselines)
+wave_baseline_SEM = std(wave_baselines) / sqrt(length(wave_baselines))
+
+wave_min = minimum(dataWAVE["DataArray"], dims=2)
+wave_min_avg = sum(wave_min) / length(wave_min)
+wave_min_SEM = std(wave_min) / sqrt(length(wave_min))
+
+wave_max = maximum(dataWAVE["DataArray"], dims=2)
+wave_max_avg = sum(wave_max) / length(wave_max)
+wave_max_SEM = std(wave_max) / sqrt(length(wave_max))
+
+wave_sdur_hfit = fit(Histogram, dataWAVE["SpikeDurs"], LinRange(0.0, 100.0, 50))
 wave_sdur_weights = wave_sdur_hfit.weights / maximum(wave_sdur_hfit.weights)
 wave_sdur_edges = collect(wave_sdur_hfit.edges[1])[1:length(wave_sdur_weights)]
 
-wave_isi_hfit = fit(Histogram, wave_data["ISIs"], LinRange(0.0, 50.0, 50))
+wave_isi_hfit = fit(Histogram, dataWAVE["ISIs"], LinRange(0.0, 100.0, 50))
 wave_isi_weights = wave_isi_hfit.weights / maximum(wave_isi_hfit.weights)
 wave_isi_edges = collect(wave_isi_hfit.edges[1])[1:length(wave_isi_weights)]
 
-wave_bdur_hfit = fit(Histogram, wave_data["BurstDurs"], LinRange(0.0, 2000.0, 50))
+wave_bdur_hfit = fit(Histogram, dataWAVE["BurstDurs"], LinRange(0.0, 2000.0, 50))
 wave_bdur_weights = wave_bdur_hfit.weights / maximum(wave_bdur_hfit.weights)
 wave_bdur_edges = collect(wave_bdur_hfit.edges[1])[1:length(wave_bdur_weights)]
 
-wave_ibi_hfit = fit(Histogram, wave_data["IBIs"], LinRange(0.0, 120e3, 50))
+wave_ibi_hfit = fit(Histogram, dataWAVE["IBIs"], LinRange(0.0, 120e3, 50))
 wave_ibi_weights = wave_ibi_hfit.weights / maximum(wave_ibi_hfit.weights)
 wave_ibi_edges = collect(wave_ibi_hfit.edges[1])[1:length(wave_ibi_weights)]
 
@@ -233,11 +282,11 @@ ax[2, 1].vlines(timestamps["Spikes"][1][:, 1], ymin=-10.0, ymax=10.0, color=:gre
 ax[2, 1].vlines(timestamps["Spikes"][1][:, 2], ymin=-10.0, ymax=10.0, color=:red)
 
 
-sdur_hfit = fit(Histogram, data["SpikeDurs"], LinRange(0.0, 50.0, 50))
+sdur_hfit = fit(Histogram, data["SpikeDurs"], LinRange(0.0, 100.0, 50))
 sdur_weights = sdur_hfit.weights / maximum(sdur_hfit.weights)
 sdur_edges = collect(sdur_hfit.edges[1])[1:length(sdur_weights)]
 
-isi_hfit = fit(Histogram, data["ISIs"], LinRange(0.0, 50.0, 50))
+isi_hfit = fit(Histogram, data["ISIs"], LinRange(0.0, 100.0, 50))
 isi_weights = isi_hfit.weights / maximum(isi_hfit.weights)
 isi_edges = collect(isi_hfit.edges[1])[1:length(isi_weights)]
 
