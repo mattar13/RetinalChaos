8f88cf3845c679f923455b5bb0fce048abfd48b7
Matthew Tarchick <mtarchick13@gmail.com>
2022-10-20 00:03:36 -0400

    running dynamic analysis updated

--- examples/running_dynamical_analysis.jl
+++ examples/running_dynamical_analysis.jl
@@ -1,25 +1,197 @@
 #%% This shows you how to run a Codimensional analysis
-using Revise
+#using Revise
 using RetinalChaos
 include("../figures/figure_setup.jl")
+import BifurcationKit.@set
+#reload_parameters()
+#Step 1: Import:
+import RetinalChaos.SDEModel #the SDEModel
+import RetinalChaos.ODEModel #the ODEModel
+import RetinalChaos.u0 #the initial condutions 
+import RetinalChaos.parameters #the parameters
 
-#%% Part 1: Running an ensemble problems
-#Setp 1: Import all parameters and make the model
-import RetinalChaos.ODEModel #import the ODEModel
-import RetinalChaos.SDEModel #import the SDEModel
-import RetinalChaos.u0 #import the 
-import RetinalChaos.parameters
-
-#Step 3: determine the timespan
+#Step 2: determine the timespan
 tmin = 0.0
-tmax = 30e3
+tmax = 500.0
+
+#%% Run the analysis without sodium influence
+#Step 3: set up the problem making sure enable jacobian is true
+reload_parameters()
+parameters[I_app] = 0.0
+parameters[g_Na] = 0.0
+parameters[g_ACh] = 0.0
+parameters[g_GABA] = 0.0
+parameters[g_TREK] = 0.0 #Remove the sAHP
+
+@time ODEProb = ODEProblem(ODEModel, u0, (tmin, tmax), parameters, jac=true)
+sol = solve(ODEProb)
+initial_plot = plot(sol, idxs=[v])
+
+# Part A: Determining jacobians and gradients
+#Step 1: Make the function and Jacobian
+ODEFunc = ODEProb.f
+F = (u, p) -> ODEFunc(u, p, 0.0)
+J = (u, p) -> ODEFunc.jac(u, p, 0.0)
+
+id_par = indexof(I_app)
+par_tm = ODEProb.p
+
+prob = BK.BifurcationProblem(
+    F, ODEProb.u0, par_tm, (@lens _[id_par]);
+    J=J,
+    recordFromSolution=(x, p) -> (V=x[1], N=x[2])
+)
+
+# continuation options (Theses need to be tuned)
+opts_br = BK.ContinuationPar(
+    pMin=-100.0, pMax=100.0, # parameters to have a smooth result
+    ds=0.04, dsmax=1.0,# this is to detect bifurcation points precisely with bisection
+    detectBifurcation=3, # Optional: bisection options for locating bifurcations
+    nInversion=8, maxBisectionSteps=50,
+    maxSteps=500,
+    nev=3
+)
+
+continuation_method = BK.PALC(tangent=BK.Bordered())
+
+br = BK.continuation(prob, continuation_method, opts_br;
+    normC=norminf,
+    verbosity=3,
+    bothside=true, plot=true
+)
+
+# This section determines the periodic orbits 
+optn_po = BK.NewtonPar(verbose=true, tol=1e-8, maxIter=100)
+opts_po_cont = BK.ContinuationPar(
+    dsmin=0.04, ds=0.1, dsmax=2.0,
+    pMin=-1.0, pMax=50.0,
+    maxSteps=110,
+    newtonOptions=(BK.@set optn_po.tol = 1e-7),
+    nev=3, plotEveryStep=2, detectBifurcation=0,
+    saveSolEveryStep=1
+)
+
+#This is for printing
+args_po = (recordFromSolution=(x, p) -> begin
+        xtt = BK.getPeriodicOrbit(p.prob, x, @set par_tm[id_par] = p.p)
+        return (max=maximum(xtt[1, :]),
+            min=minimum(xtt[1, :]),
+            period=BK.getPeriod(p.prob, x, @set par_tm[id_par] = p.p))
+    end,
+    plotSolution=(x, p; k...) -> begin
+        xtt = BK.getPeriodicOrbit(p.prob, x, @set par_tm[id_par] = p.p)
+        plot!(xtt.t, xtt[1, :]; label="E", k...)
+        plot!(xtt.t, xtt[2, :]; label="x", k...)
+        plot!(br; subplot=1, putspecialptlegend=false)
+    end,
+    normC=norminf
+)
+
+
+Mt = 30 # number of time sections
+@time br_pocoll = BK.continuation(
+    br, 4, opts_po_cont, # we want to branch form the 4th bif. point
+    BK.PeriodicOrbitOCollProblem(Mt, 5), # we want to use the Collocation method to locate PO, with polynomial degree 5
+    plot=true, verbosity=3;
+    args_po... # regular continuation options
+)
+scene = plot(br, br_pocoll, markersize=3)
+plot!(scene, br_pocoll.param, br_pocoll.min, label="")
+
+periodic_orbits = plot()
+# fetch the saved solutions
+for sol in br_pocoll.sol
+    # periodic orbit
+    po = sol.x
+    # get the mesh and trajectory
+    traj = BK.getPeriodicOrbit(br_pocoll.prob, po, @set par_tm[id_par] = sol.p)
+    plot!(periodic_orbits, traj[1, :], traj[2, :], xlabel="E", ylabel="x", label="")
+end
+periodic_orbits
+
+#%% Determine the sodium conductance: Running an ensemble problems
+#Step 3: set up the problem making sure enable jacobian is true
+reload_parameters()
+parameters[I_app] = -50.0 #In order for convergence to occur, I_app needs to be set low
+parameters[g_ACh] = 0.0
+parameters[g_GABA] = 0.0
+parameters[g_TREK] = 0.0 #Remove the sAHP
+
+@time ODEProb = ODEProblem(ODEModel, u0, (tmin, tmax), parameters, jac=true)
+sol = solve(ODEProb)
+initial_plot = plot(sol, idxs=[v])
+#%% Part A: Determining jacobians and gradients
+#Step 1: Make the function and Jacobian
+ODEFunc = ODEProb.f
+F = (u, p) -> ODEFunc(u, p, 0.0)
+J = (u, p) -> ODEFunc.jac(u, p, 0.0)
+
+id_par = indexof(I_app)
+par_tm = ODEProb.p
+
+prob = BK.BifurcationProblem(
+    F, ODEProb.u0, par_tm, (@lens _[id_par]);
+    J=J,
+    recordFromSolution=(x, p) -> (V=x[1], N=x[2])
+)
+
+# continuation options (Theses need to be tuned)
+opts_br = BK.ContinuationPar(
+    pMin=-150.0, pMax=100.0, #When plotting the bifurction for sodium, increase to intercepts to -150
+    ds=0.04, dsmax=1.0,# this is to detect bifurcation points precisely with bisection
+    detectBifurcation=3, # Optional: bisection options for locating bifurcations
+    nInversion=8, maxBisectionSteps=50,
+    maxSteps=500,
+    nev=3
+)
+
+continuation_method = BK.PALC(tangent=BK.Bordered())
+
+br = BK.continuation(prob, continuation_method, opts_br;
+    normC=norminf,
+    verbosity=3,
+    bothside=true, plot=true
+)
+
+#Determine where the periodic orbits exist
+optn_po = BK.NewtonPar(verbose=true, tol=1e-8, maxIter=10)
+opts_po_cont = BK.ContinuationPar(
+    dsmin=0.04, ds=0.1, dsmax=2.0,
+    pMin=-50.0, pMax=50.0,
+    maxSteps=110,
+    newtonOptions=(BK.@set optn_po.tol = 1e-7),
+    nev=3, plotEveryStep=2, detectBifurcation=0,
+    saveSolEveryStep=1
+)
+
+Mt = 60 # number of time sections
+@time br_pocoll = BK.continuation(
+    br, 4, opts_po_cont, # we want to branch form the 4th bif. point
+    BK.PeriodicOrbitOCollProblem(Mt, 5), # we want to use the Collocation method to locate PO, with polynomial degree 5
+    plot=true, verbosity=3;
+    args_po... # regular continuation options
+)
+
+#Plot the results
+scene = plot(br, br_pocoll, markersize=3)
+plot!(scene, br_pocoll.param, br_pocoll.min, label="")
+
+periodic_orbits = plot()
+# fetch the saved solutions
+for sol in br_pocoll.sol
+    # periodic orbit
+    po = sol.x
+    # get the mesh and trajectory
+    traj = BK.getPeriodicOrbit(br_pocoll.prob, po, @set par_tm[id_par] = sol.p)
+    plot!(periodic_orbits, traj[1, :], traj[2, :], xlabel="E", ylabel="x", label="")
+end
+periodic_orbits
+
+#%% Codimensional analysis
+
 
 #Step 4: set the parameters
 reload_parameters()
-#parameters[I_app] = 10.0
-#parameters[ρe] = 0.0
-#parameters[ρi] = 0.0
-#parameters[g_TREK] = 0.0 #Remove the sAHP
 parameters[E_Cl]
 prob = SDEProblem(SDEModel, u0, (tmin, tmax), parameters) #ODE problem
 #prob = SDEProblem(T_SDE, noise, u0, tspan, p) #ODE problem
@@ -47,7 +219,10 @@ for (sol_idx, sol_i) in enumerate(sim)
 end
 plot(plt_a, plt_b, layout=2)
 
-#%% Part 2: Running a equilibria analysis
+#%% Part 2: Using bifurcation kits
+using Bifuc
+
+
 parameters[I_app] = 10.0 #Set initial applied current to 0
 parameters[ρi] = 0.0 #remove GABA influence
 parameters[ρe] = 0.0 #remove ACh influence
