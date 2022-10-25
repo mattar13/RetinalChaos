247321e94b0d59e4022952f5db894c6f886f5397
Matthew Tarchick <mtarchick13@gmail.com>
2022-10-25 10:47:02 -0400

    updated run SDE model

--- examples/running_model_SDE.jl
+++ examples/running_model_SDE.jl
@@ -2,18 +2,11 @@ using Revise
 using RetinalChaos
 using Plots
 
-import RetinalChaos.SDEModel #import the ODEModel
 import RetinalChaos.u0 #import the 
 import RetinalChaos.parameters
 reload_parameters()
 
-#Step 3: determine the timespan
-tmin = 0.0
-tmax = 60e3
-
-#Step 4: set up the problem
-probSDE = SDEProblem(SDEModel, u0, (tmin, tmax), parameters)
-
 #Step 5: Solve the problem
+probSDE = RetinalChaos.loadSDE(u0, parameters)
 @time sol = solve(probSDE, SOSRI()); #So far the best method is SOSRI
 plot(sol, idxs=[v, W], layout=(2, 1))
