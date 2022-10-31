21961d66a987d39cef7d606a4f863f60c12bd4fe
Matthew Tarchick <mtarchick13@gmail.com>
2022-10-30 23:05:00 -0400

    comparing runtimes with SOSRA vs SOSRI

--- figures/making_model_data.jl
+++ figures/making_model_data.jl
@@ -9,8 +9,8 @@ loc = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\Modelling\figur
 u_dict = read_JSON("params/conds.json")
 p_dict = read_JSON("params/params.json")
 p_dict[:C_m] = 13.6
-@time run_model(u_dict, p_dict, loc, tmax = 1000.0, SOSRI(), abstol=2e-2, reltol=2e-2, maxiters=1e7)
-@time run_model(u_dict, p_dict, loc, tmax = 1000.0, SOSRA(), abstol=2e-2, reltol=2e-2, maxiters=1e7)
+@time run_model(u_dict, p_dict, loc, tmax=1000.0, SOSRI(), abstol=2e-2, reltol=2e-2, maxiters=1e7)
+@time run_model(u_dict, p_dict, loc, tmax=1000.0, SOSRA(), abstol=2e-2, reltol=2e-2, maxiters=1e7)
 
 #%% Model 2: Blocked Neurotransmission 
 loc = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\Modelling\figure_data\isolated_model"
@@ -146,3 +146,68 @@ p_dict = read_JSON("params/params.json")
 p_dict[:Di] = 0.001 #Make synaptic transmission faster
 p_dict[:De] = 0.001 #Make synaptic transmission faster
 run_model(u_dict, p_dict, loc, SOSRI(), abstol=2e-2, reltol=2e-2, maxiters=1e7)
+
+#%% Save the isolated data model
+data_root = "C:/Users/mtarc/OneDrive - The University of Akron/Data/Modelling/figure_data"
+FASTe_path = "$(data_root)/isolated_model"
+FASTi_path = "$(data_root)/no_GABA_model"
+FASTei_path = "$(data_root)/wave_model"
+
+
+ECl55_path = "$(data_root)/ECl55_model"
+
+dataISO = load("$(isolated_path)/data.jld2")
+dataNG = load("$(noGABA_path)/data.jld2")
+dataWAVE = load("$(wave_path)/data.jld2")
+dataECl = load("$(ECl55_path)/data.jld2")
+
+solISO = reshape(dataISO["DataArray"], (nx, ny, size(dataISO["DataArray"], 2)))
+solNG = reshape(dataNG["DataArray"], (nx, ny, size(dataNG["DataArray"], 2)))
+solWAVE = reshape(dataWAVE["DataArray"], (nx, ny, size(dataWAVE["DataArray"], 2)))
+solECl = reshape(dataECl["DataArray"], (nx, ny, size(dataECl["DataArray"], 2)))
+
+
+save_loc = "C:\\Users\\mtarc\\The University of Akron\\Renna Lab - General\\Journal Submissions\\2022 A Computational Model - Sci. Rep\\Submission 1\\Figures\\"
+
+anim = @animate for t = 1.0:animate_dt:size(solISO, 3)
+     println("[$(now())]: Animating simulation $(t) out of $(size(solISO, 2))...")
+     frame_i = solISO[:, :, round(Int64, t)]
+     heatmap(frame_i, ratio=:equal, grid=false,
+          xaxis="", yaxis="", xlims=(0, nx), ylims=(0, ny), c=:curl, clims=(-90.0, 0.0),
+          colorbar_title="Voltage (mV)"
+     )
+end
+gif(anim, "$(save_loc)/S3 Neurotransmission Blocked Simulation.gif", fps=1000.0 / animate_dt)
+
+#%% Plot the no GABA simulation
+anim = @animate for t = 1.0:animate_dt:size(solNG, 3)
+     println("[$(now())]: Animating simulation $(t) out of $(size(solNG, 2))...")
+     frame_i = solNG[:, :, round(Int64, t)]
+     heatmap(frame_i, ratio=:equal, grid=false,
+          xaxis=false, yaxis=false, xlims=(0, nx), ylims=(0, ny), c=:curl, clims=(-90.0, 0.0),
+          colorbar_title="Voltage (mV)"
+     )
+end
+gif(anim, "$(save_loc)/S4 NoGABA Simulation.gif", fps=1000.0 / animate_dt)
+
+#%% Plot the wave simulation
+anim = @animate for t = 1.0:animate_dt:size(solWAVE, 3)
+     println("[$(now())]: Animating simulation $(t) out of $(size(solNG, 2))...")
+     frame_i = solWAVE[:, :, round(Int64, t)]
+     heatmap(frame_i, ratio=:equal, grid=false,
+          xaxis=false, yaxis=false, xlims=(0, nx), ylims=(0, ny), c=:curl, clims=(-90.0, 0.0),
+          colorbar_title="Voltage (mV)"
+     )
+end
+gif(anim, "$(save_loc)/S5 Wave Simulation.gif", fps=1000.0 / animate_dt)
+
+#%% Plot the ECl-55 simulation
+anim = @animate for t = 1.0:animate_dt:size(solECl, 3)
+     println("[$(now())]: Animating simulation $(t) out of $(size(solECl, 2))...")
+     frame_i = solECl[:, :, round(Int64, t)]
+     heatmap(frame_i, ratio=:equal, grid=false,
+          xaxis=false, yaxis=false, xlims=(0, nx), ylims=(0, ny), c=:curl, clims=(-90.0, 0.0),
+          colorbar_title="Voltage (mV)"
+     )
+end
+gif(anim, "$(save_loc)/S6 ECl -55 Simulation.gif", fps=1000.0 / animate_dt)
