93a2fd45947c609f03b7e1053e39980ca59795ef
Matthew Tarchick <mtarchick13@gmail.com>
2022-12-21 14:36:57 -0500

    updated model figures

--- figures/making_figure5.jl
+++ figures/making_figure5.jl
@@ -11,65 +11,60 @@ using Revise
 using RetinalChaos
 import RetinalChaos: Φ, ħ, ∇
 import RetinalChaos: T_ODE_NT_Release, get_timestamps, max_interval_algorithim
-import Plots: contourf
-pyplot()
+using PyPlot
 include("figure_setup.jl")
-println("Running the plotting script for figure 3")
+#%% Simulating model
+println("Simulating model for giure 5")
 
-#%% Set up modelling data
+import RetinalChaos.ODEModel #import the ODEModel
+import RetinalChaos.u0 #import the 
+import RetinalChaos.parameters
+
 print("[$(now())]: Setting up modelling data... ")
-#Step 1: Import the initial conditions
-conds_dict = read_JSON("params\\conds.json")
-u0 = conds_dict |> extract_dict
-#Step 2: Import the parameters
-pars_dict = read_JSON("params\\params.json")
-pars_dict[:I_app] = 15.0
-pars_dict[:g_ACh] = 0.0
-pars_dict[:g_GABA] = 0.0
-p = pars_dict |> extract_dict
-#Step 3: determine the timespan
-tspan = (0.0, 300e3)
+parameters[I_app] = 15.0
+
+tmin = 0.0
+dt = 1.0; #Set the time differential
+tmax = 300e3
 #Step 4: set up the problem
-prob = ODEProblem(T_ODE_NT_Release, u0, tspan, p)
+prob = ODEProblem(ODEModel, u0, (tmin, tmax), parameters)
 #Step 5: Solve the problem
-sol = solve(prob, progress=true, progress_steps=1);
-println(" Completed")
+sol = solve(prob, progress=true, progress_steps=1)
 
 #%% Run the analysis
 print("[$(now())]: Running analysis... ")
-dt = 1.0; #Set the time differential
 ts, data = timeseries_analysis(sol)
-burst_tstamps = ts["Bursts"][1] #Extract the bursts
+burst_tstamps = ts["Bursts"][2] #Extract the bursts
 println(" Completed")
 
 
 #%% Extract plotting data
 print("[$(now())]: Extracting data... ")
 offset = 2000
-t = burst_tstamps[2, 1]-offset:dt:burst_tstamps[2, 2]+offset
-vt = sol(t, idxs=1)
-et = sol(t, idxs=6)
-it = sol(t, idxs=7)
-t = (t .- t[1]) ./ 1000
+tstops = burst_tstamps[2, 1]-offset:dt:burst_tstamps[2, 2]+offset
+vt = sol(tstops, idxs=2).u
+et = sol(tstops, idxs=9).u
+it = sol(tstops, idxs=10).u
+tstops = (tstops .- tstops[1]) ./ 1000
 
 v_rng = -80.0:1.0:0.0
-Vse = pars_dict[:Vse]
-V0e = pars_dict[:V0e]
-Vsi = pars_dict[:Vsi]
-V0i = pars_dict[:V0i]
-ρe = pars_dict[:ρe]
-ρi = pars_dict[:ρi]
-ACH_r = Φ.(v_rng, Vse, V0e) * ρe
-GABA_r = Φ.(v_rng, Vsi, V0i) * ρi
+pVSe = parameters[VSe]
+pV0e = parameters[V0e]
+pVSi = parameters[VSi]
+pV0i = parameters[V0i]
+pρe = parameters[ρe]
+pρi = parameters[ρi]
+ACH_r = Φ.(v_rng, pVSe, pV0e) * pρe
+GABA_r = Φ.(v_rng, pVSi, pV0i) * pρi
 println(" Completed")
 
 # Generate a release map
 print("[$(now())]: Generating a neurotransmitter release map... ")
-De = pars_dict[:De]
-Di = pars_dict[:Di]
+pDe = parameters[De]
+pDi = parameters[Di]
 nx = ny = 25
-ACH_map = zeros(nx, ny, length(t))
-GABA_map = zeros(nx, ny, length(t))
+ACH_map = zeros(nx, ny, length(tstops))
+GABA_map = zeros(nx, ny, length(tstops))
 
 center_x = round(Int64, nx / 2) #Because the index starts from 0 subtract 1
 center_y = round(Int64, ny / 2) #Because the indexing starts from 0
@@ -78,7 +73,7 @@ dXe = (1.0, 1.0)
 dYe = (1.0, 1.0)
 dXi = (1.0, 1.0)
 dYi = (0.1, 1.9)
-for i = 1:length(t)
+for i = 1:length(tstops)
      if i == 1
           ACH_map[center_x+1, center_y+1, 1] = et[i] #Set intial map
           GABA_map[center_x+1, center_y+1, 1] = it[i] #Set initial map
@@ -88,8 +83,8 @@ for i = 1:length(t)
           i_i = GABA_map[:, :, i-1]
           de = zeros(size(e_i))
           di = zeros(size(i_i))
-          ∇(de, e_i, De, dX=dXe, dY=dYe)
-          ∇(di, i_i, Di, dX=dXi, dY=dYi)
+          ∇(de, e_i, pDe, dX=dXe, dY=dYe)
+          ∇(di, i_i, pDi, dX=dXi, dY=dYi)
           println(sum(e_i))
           ACH_map[:, :, i] += de + e_i
           GABA_map[:, :, i] += di + i_i
@@ -101,26 +96,26 @@ end
 # Set the frame stops for Images
 frame_stops = LinRange(burst_tstamps[2, 1] + dt, burst_tstamps[2, 2] + 200, 4) .- burst_tstamps[2, 1] .+ (offset)
 frame_stops = round.(Int64, frame_stops)
-avg_ACH = sum(ACH_map, dims=3)[:, :, 1] ./ length(t)
-avg_GABA = sum(GABA_map, dims=3)[:, :, 1] ./ length(t)
+avg_ACH = sum(ACH_map, dims=3)[:, :, 1] ./ length(tstops)
+avg_GABA = sum(GABA_map, dims=3)[:, :, 1] ./ length(tstops)
 
 #Generate current maps
-pars_dict = read_JSON("params\\params.json") #Might need to reload parameters
-gACh = pars_dict[:g_ACh]
-EACh = pars_dict[:E_ACh]
-kACh = pars_dict[:k_ACh]
-gGABA = pars_dict[:g_GABA]
-EGABA = pars_dict[:E_GABA]
-kGABA = pars_dict[:k_GABA]
+reload_parameters() #Might need to reload parameters
+pgACh = parameters[g_ACh]
+pEACh = parameters[E_ACh]
+pkACh = parameters[k_ACh]
+pgGABA = parameters[g_GABA]
+pECl = parameters[E_Cl]
+pkGABA = parameters[k_GABA]
 V_CLAMP = -40.0
-I_ACh = -gACh .* ħ.(ACH_map, kACh) .* (V_CLAMP - EACh)
-I_GABA = -gGABA .* ħ.(GABA_map, kGABA) .* (V_CLAMP - EGABA)
+I_ACh = -pgACh .* ħ.(ACH_map, pkACh) .* (V_CLAMP - pEACh)
+I_GABA = -pgGABA .* ħ.(GABA_map, pkGABA) .* (V_CLAMP - pECl)
 I_TOTAL = I_ACh + I_GABA
 avg_I_TOTAL = sum(I_TOTAL, dims=3)[:, :, 1] ./ size(I_TOTAL, 3)
 
 #Maximal calculations
-I_ACh_max = -gACh .* 1 .* (V_CLAMP - EACh)
-I_GABA_max = -gGABA .* 1 .* (V_CLAMP - EGABA)
+I_ACh_max = -pgACh .* 1 .* (V_CLAMP - pEACh)
+I_GABA_max = -pgGABA .* 1 .* (V_CLAMP - pECl)
 
 # Finally we need to extract some data from the X or y
 dY_D = 3:2:center_y
@@ -147,18 +142,18 @@ col2_ylabel = -0.25
 gsA = gs[1, 1].subgridspec(ncols=1, nrows=2)
 
 axA1 = fig3.add_subplot(gsA[1, 1])
-axA1.plot(t, vt, c=v_color, lw=lw_standard)
+axA1.plot(tstops, vt, c=v_color, lw=lw_standard)
 ylabel("Vt (mV)")
 axA1.xaxis.set_visible(false) #Turn off the bottom axis
 axA1.yaxis.set_label_coords(col1_ylabel, 0.5)
 axA1.spines["bottom"].set_visible(false)
 
 axA2 = fig3.add_subplot(gsA[2, 1])
-ylim(0.0, 4.0)
-axA2.plot(t, et, c=:green, lw=lw_standard)
-axA2.plot(t, it, c=:red, lw=lw_standard)
-ylabel("NT. Rel. \n (mM)")
-xlabel("Time (s)")
+axA2.plot(tstops, et, c=:green, lw=lw_standard)
+axA2.plot(tstops, it, c=:red, lw=lw_standard)
+axA2.set_xlabel("Time (s)")
+axA2.set_ylabel("NT. Rel. \n (mM)")
+axA2.set_ylim(0.0, 2.0)
 #axA2.xaxis.set_visible(false) #Turn off the bottom axis
 axA2.yaxis.set_label_coords(col1_ylabel, 0.5)
 axA2.legend(["ACh", "GABA"],
@@ -166,12 +161,12 @@ axA2.legend(["ACh", "GABA"],
      bbox_to_anchor=(0.65, 1.1), fontsize=9.0, markerscale=0.5, handletextpad=0.3
 )
 #Plot all of the frame stops
-axA2.plot(t[frame_stops], et[frame_stops], c=:blue, markersize = 2.0, linewidth=0.0, marker="o")
-axA2.plot(t[frame_stops], it[frame_stops], c=:red, markersize = 2.0, linewidth=0.0, marker="o")
+axA2.plot(tstops[frame_stops], et[frame_stops], c=:blue, markersize=2.0, linewidth=0.0, marker="o")
+axA2.plot(tstops[frame_stops], it[frame_stops], c=:red, markersize=2.0, linewidth=0.0, marker="o")
 
 axAR = fig3.add_subplot(gs[1, 2])
-xlabel("Voltage (mV)")
-ylabel("Release")
+axAR.set_xlabel("Voltage (mV)")
+axAR.set_ylabel("Release")
 axAR.plot(v_rng, ACH_r, c=:green, lw=lw_standard)
 axAR.plot(v_rng, GABA_r, c=:red, lw=lw_standard)
 
@@ -222,7 +217,7 @@ for (idx, fr) in enumerate(frame_stops)
      end
      axCi.set_xticks([])
      axCi.set_aspect("equal", "box")
-     xlabel("t = $(round(t[fr], digits = 1))")
+     xlabel("t = $(round(tstops[fr], digits = 1))")
      axCi.spines["left"].set_visible(false)
      axCi.spines["bottom"].set_visible(false)
      #Can we put a text box below each frame 
@@ -247,47 +242,47 @@ cmapYV = plt.get_cmap("Blues")
 cmapYD = plt.get_cmap("Greens")
 cmapXN = plt.get_cmap("Reds")
 cmapXT = plt.get_cmap("Purples")
-ylim(-15.0, 10.0)
 #Plot the Nasal direction
 for (i, dyv) in enumerate(dY_V)
      axD1.plot(I_TOTAL[dyv, center_x, :], c=cmapYV(i / length(dY_V)), lw=lw_standard)
 end
+axD1.set_ylim(-15.0, 10.0)
 
 axD1.xaxis.set_visible(false) #Turn off the bottom axis
 axD1.spines["bottom"].set_visible(false)
 
 axD2 = fig3.add_subplot(gsDL[3, 2])
-ylim(-15.0, 10.0)
 #Plot the Nasal direction
 for (i, dyd) in enumerate(reverse(dY_D))
-     axD2.plot(t, I_TOTAL[dyd, center_x, :], c=cmapYD(i / length(dY_D)), lw=lw_standard)
+     axD2.plot(tstops, I_TOTAL[dyd, center_x, :], c=cmapYD(i / length(dY_D)), lw=lw_standard)
 end
-xlabel("Time (s)")
+axD2.set_xlabel("Time (s)")
+axD2.set_ylim(-15.0, 10.0)
 
 axDC = fig3.add_subplot(gsDL[2, 2])
-ylim(-15.0, 10.0)
-axDC.plot(t, I_TOTAL[center_y, center_x, :], c=:black, lw=lw_standard)
+axDC.plot(tstops, I_TOTAL[center_y, center_x, :], c=:black, lw=lw_standard)
+axDC.set_ylim(-15.0, 10.0)
 axDC.xaxis.set_visible(false) #Turn off the bottom axis
 axDC.spines["bottom"].set_visible(false)
+axDC.yaxis.set_visible(false)
 axDC.spines["left"].set_visible(false)
-axDC.yaxis.set_visible(false) 
 
 axD3 = fig3.add_subplot(gsDL[2, 1])
-ylim(-15.0, 10.0)
-ylabel("Current (pA)")
 for (i, dxn) in enumerate(reverse(dX_N))
-     axD3.plot(t, I_TOTAL[center_y, dxn, :], c=cmapXN(i / length(dX_N)), lw=lw_standard)
+     axD3.plot(tstops, I_TOTAL[center_y, dxn, :], c=cmapXN(i / length(dX_N)), lw=lw_standard)
 end
-xlabel("Time (s)")
+axD3.set_xlabel("Time (s)")
+axD3.set_ylim(-15.0, 10.0)
+axD3.set_ylabel("Current (pA)")
 #axD3.xaxis.set_visible(false) #Turn off the bottom axis
 #axD3.spines["bottom"].set_visible(false)
 
 axD4 = fig3.add_subplot(gsDL[2, 3])
 ylim(-15.0, 10.0)
 for (i, dxt) in enumerate(dX_T)
-     axD4.plot(t, I_TOTAL[center_y, dxt, :], c=cmapXT(i / length(dX_T)), lw=lw_standard)
+     axD4.plot(tstops, I_TOTAL[center_y, dxt, :], c=cmapXT(i / length(dX_T)), lw=lw_standard)
 end
-xlabel("Time (s)")
+axD4.set_xlabel("Time (s)")
 
 axDR = fig3.add_subplot(gs[4, 2])
 ctr_i = axDR.contourf(avg_I_TOTAL, cmap="RdYlGn", levels=-5.0:0.5:5.0, extend="both")
@@ -305,8 +300,8 @@ axDR.set_facecolor("none")
 
 axDR.set_xticks([])
 axDR.yaxis.set_visible(false) #Turn off the bottom axis
-xlim(0, nx)
-ylim(0, ny)
+axDR.set_xlim(0, nx)
+axDR.set_ylim(0, ny)
 axDR.set_aspect("equal", "box")
 axDR.spines["bottom"].set_visible(false)
 axDR.spines["left"].set_visible(false)
@@ -319,51 +314,9 @@ axDR.annotate("C", (0.01, 0.50), xycoords="figure fraction", annotation_clip=fal
 axDR.annotate("D", (0.01, 0.25), xycoords="figure fraction", annotation_clip=false, fontsize=20.0, fontweight="bold")
 println(" Completed")
 
-
 #%% Save the plot
-loc = raw"C:\Users\mtarc\The University of Akron\Renna Lab - General\Journal Submissions\2022 A Computational Model - Sci. Rep\Submission 1\Figures"
+loc = raw"C:\Users\mtarc\The University of Akron\Renna Lab - General\Journal Submissions\2022 A Computational Model - Sci. Rep\Submission 2\Figures"
 print("[$(now())]: Saving the figure 3...")
 fig3.savefig("$(loc)/Figure5_Neurotransmitters.jpg")
 plt.close("all")
 println(" Completed")
-
-#%% Generate a diffusion animation
-fig, ax = plt.subplots(2)
-anim = @animate for i = 1:10
-     ax[1].plot(rand(10))
-     ax[2].plot(rand(10))
-end
-     
-     
-
-#%%
-anim = @animate for i = 1:100:length(t)
-     print("[$(now())]: Animating simulation...")
-     println(i)
-     e_frame = ACH_map[:, :, i]
-     i_frame = GABA_map[:, :, i]
-     I_frame = I_TOTAL[:, :, i]
-     #contourf(frame_e, e_frame, clims=(0.0, 1.0))
-     contour_e = contourf(e_frame,
-          ratio=:equal, grid=false,
-          ylabel="t = $(round(t[i], digits = 2))",
-          xaxis=false, yaxis=false, xlims=(0, nx), ylims=(0, ny),
-          c=:curl, clims=(0.0, 1.0), levels=0.0:0.05:3.0,
-          colorbar_title="[ACh] (μM)"
-     )
-     xlabel("")
-     contour_i = contourf(i_frame,
-          ratio=:equal, grid=false, xaxis=false, yaxis=false, xlims=(0, nx), ylims=(0, ny),
-          c=:jet, clims=(0.0, 1.0), levels=0.0:0.05:3.0, 
-          colorbar_title = "[GABA] (μM)"
-     )
-
-     contour_I = contourf(I_frame,
-          ratio=:equal, grid=false, xaxis=false, yaxis=false, xlims=(0, nx), ylims=(0, ny),
-          c=:RdYlGn, clims=(-8.0, 8.0), levels=-15.0:0.5:15.0, 
-          colorbar_title = "Induced Current (pA)"
-     )
-     plot(contour_e, contour_i, contour_I, layout=3)
-end
-
-gif(anim, "$(loc)/S1 Neurotransmitter Conductance.gif")
--- figures/making_figure6.jl
+++ figures/making_figure6.jl
@@ -4,8 +4,49 @@ using Dates
 using RetinalChaos
 using StatsBase, Statistics
 using ePhys
+using DataFrames
 import ePhys: dwt_filter
-include("opening_data.jl")
+#include("opening_data.jl")
+
+print("Extract all data points")
+spike_t_PHYS, spike_vt_PHYS = extract_spike_trace(tsPHYS, dataPHYS, idx=5, dt=dt, normalize=false)
+burst_t_PHYS, burst_vt_PHYS = extract_burst_trace(tsPHYS, dataPHYS, idx=2, dt=dt, normalize=false)
+IBI_t_PHYS, IBI_vt_PHYS = extract_IBI_trace(tsPHYS, dataPHYS, idx=2, dt=dt, normalize=false)
+
+spike_t_WAVE, spike_vt_WAVE = extract_spike_trace(tsWAVE, dataWAVE, normalize=false, cell_n=wave_xIdx, idx=1)
+burst_t_WAVE, burst_vt_WAVE = extract_burst_trace(tsWAVE, dataWAVE, normalize=false, cell_n=wave_xIdx, idx=1)
+IBI_t_WAVE, IBI_vt_WAVE = extract_IBI_trace(tsWAVE, dataWAVE, normalize=false, cell_n=wave_xIdx, idx=1)
+
+spike_t_ISO, spike_vt_ISO = extract_spike_trace(tsISO, dataISO, normalize=false, cell_n=iso_xIdx, idx=1)
+burst_t_ISO, burst_vt_ISO = extract_burst_trace(tsISO, dataISO, normalize=false, cell_n=iso_xIdx, idx=1)
+IBI_t_ISO, IBI_vt_ISO = extract_IBI_trace(tsISO, dataISO, normalize=false, cell_n=iso_xIdx, idx=1)
+
+spike_t_NG, spike_vt_NG = extract_spike_trace(tsNG, dataNG, normalize=false, cell_n=ng_xIdx, idx=1)
+burst_t_NG, burst_vt_NG = extract_burst_trace(tsNG, dataNG, normalize=false, cell_n=ng_xIdx, idx=1)
+IBI_t_NG, IBI_vt_NG = extract_IBI_trace(tsNG, dataNG, normalize=false, cell_n=ng_xIdx, idx=1)
+
+spike_t_EC, spike_vt_EC = extract_spike_trace(tsEC, dataEC, normalize=false, cell_n=ec_xIdx, idx=1)
+burst_t_EC, burst_vt_EC = extract_burst_trace(tsEC, dataEC, normalize=false, cell_n=ec_xIdx, idx=1)
+IBI_t_EC, IBI_vt_EC = extract_IBI_trace(tsEC, dataEC, normalize=false, cell_n=ec_xIdx, idx=1)
+println("Complete")
+
+print("Extract wave rasters")
+WAVE_t = dataWAVE["Time"][1000:61000]
+WAVE_raster = dataWAVE["DataArray"][1:200, 1000:61000]
+
+ISO_t = dataISO["Time"][1000:61000]
+ISO_raster = dataISO["DataArray"][1:200, 1000:61000]
+
+EC_t = dataEC["Time"][1000:61000]
+EC_raster = dataEC["DataArray"][1:200, 1000:61000]
+println("Complete")
+
+#%% Open the wave stats from excel
+
+#Need to open wave stats data
+wavesWAVE = DataFrame(XLSX.readtable("$(wave_path)\\wave_data.xlsx", "Waves"))
+wavesISO = DataFrame(XLSX.readtable("$(isolated_path)\\wave_data.xlsx", "Waves"))
+wavesECl = DataFrame(XLSX.readtable("$(ECl55_path)\\wave_data.xlsx", "Waves"))
 
 #%%
 print("[$(now())]: Plotting... ")
@@ -13,15 +54,16 @@ width_inches = 7.5
 height_inches = 7.5
 fig4 = plt.figure("Physiology Data", figsize=(width_inches, height_inches))
 
-col1_ylabel = -0.21
+col1_ylabel = -0.31
 col2_ylabel = -0.11
+levels = -95.0:10.0:5.0
 
-gs = fig4.add_gridspec(5, 3,
+gs = fig4.add_gridspec(5, 4,
      #width_ratios=(0.50, 0.50),
      #height_ratios=(0.16, 0.16, 0.15, 0.15, 0.15, 0.15),
      right=0.95, left=0.15,
-     top=0.95, bottom=0.08,
-     wspace=0.20, hspace=0.5)
+     top=0.95, bottom=0.03,
+     wspace=0.30, hspace=0.5)
 
 #axA = fig4.add_subplot(py"""$(gs)[0, :]""")
 axA1 = fig4.add_subplot(gs[1, 1])
@@ -81,6 +123,15 @@ axB3.spines["bottom"].set_visible(false)
 axB3.yaxis.set_major_locator(MultipleLocator(40.0))
 axB3.yaxis.set_minor_locator(MultipleLocator(20.0))
 
+axB4 = fig4.add_subplot(gs[2, 4])
+axB4.contourf((ISO_t .- ISO_t[1]) ./ 1000, 1:200, ISO_raster, levels=levels, extend="both", color=:curl)
+#axB4.set_ylabel("Cell Number")
+axB4.xaxis.set_visible(false) #Turn off the bottom axis
+axB4.spines["bottom"].set_visible(false)
+axB4.xaxis.set_major_locator(MultipleLocator(20.0))
+axB4.xaxis.set_minor_locator(MultipleLocator(10.0))
+#axB4.set_xlabel("Time (s)")
+
 # Figure part C Normal Wave model
 axC1 = fig4.add_subplot(gs[3, 1])
 ylim(-90.0, 10.0)
@@ -109,6 +160,14 @@ axC3.spines["bottom"].set_visible(false)
 axC3.yaxis.set_major_locator(MultipleLocator(40.0))
 axC3.yaxis.set_minor_locator(MultipleLocator(20.0))
 
+axC4 = fig4.add_subplot(gs[3, 4])
+axC4.contourf((EC_t .- EC_t[1]) ./ 1000, 1:200, EC_raster, levels=levels, extend="both", color=:curl)
+#axB4.set_ylabel("Cell Number")
+axC4.xaxis.set_visible(false) #Turn off the bottom axis
+axC4.spines["bottom"].set_visible(false)
+axC4.xaxis.set_major_locator(MultipleLocator(20.0))
+axC4.xaxis.set_minor_locator(MultipleLocator(10.0))
+
 #axC = fig4.add_subplot(py"""$(gs)[2, :]""")
 axD1 = fig4.add_subplot(gs[4, 1])
 ylim(-90.0, 10.0)
@@ -132,53 +191,98 @@ xlabel("Time (s)")
 axD3.plot((IBI_t_WAVE.-IBI_t_WAVE[1]) ./ 1000, IBI_vt_WAVE, c=:green, lw=lw_standard)
 axD3.yaxis.set_major_locator(MultipleLocator(40.0))
 axD3.yaxis.set_minor_locator(MultipleLocator(20.0))
-#Make Bar Graphs showing the MSE loss between each RandomNumbers
 
-jitter_dataPHYS_spike = 0.5 .* rand(length(phys_spike_durs))
-jitter_dataISO_spike = 0.5 .* rand(length(dataISO["SpikeDurs"])) .+ 1
-jitter_dataEC_spike = 0.5 .* rand(length(dataEC["SpikeDurs"])) .+ 2
-jitter_dataWAVE_spike = 0.5 .* rand(length(dataWAVE["SpikeDurs"])) .+ 3
+axD4 = fig4.add_subplot(gs[4, 4])
+ctrWAVE = axD4.contourf((WAVE_t .- WAVE_t[1]) ./ 1000, 1:200, WAVE_raster, levels=levels, extend="both", color=:curl)
+#axB4.set_ylabel("Cell Number")
+#axD4.xaxis.set_visible(false) #Turn off the bottom axis
+#axD4.spines["bottom"].set_visible(false)
+axD4.set_xlabel("Time (s)")
+axD4.xaxis.set_major_locator(MultipleLocator(20.0))
+axD4.xaxis.set_minor_locator(MultipleLocator(10.0))
 
-labels = ["PHYS", "ISO", "ECl", "WAV"]
-pos = [0.25, 1.25, 2.25, 3.25]
+axA4 = fig4.add_subplot(gs[1, 4])
+axA4.set_title("Model Rasters")
+cbarSE = fig4.colorbar(ctrWAVE, cax=axA4, ticks=[-90.0, 5.0])
+cbarSE.ax.set_ylabel("Voltage (mV)")
+#axA1.annotate("A", (0.01, 0.90), xycoords="figure fraction", annotation_clip=false, fontsize=30.0, fontweight="bold")
+
+#Make violin Graphs showing the MSE loss between each RandomNumbers
+labels = ["P", "I", "D", "H"]
+colors = [:black, :red, :blue, :green]
+pos = [1.0, 2.0, 3.0, 4.0]
+
 axE1 = fig4.add_subplot(gs[5, 1])
-#ylim(0.0, 100.0)
-#axE1.plot(sdur_weights, sdur_edges, c = :black)
-axE1.scatter(jitter_dataPHYS_spike, phys_spike_durs./1000, s=5.0, c=:black)
-axE1.scatter(jitter_dataISO_spike, dataISO["SpikeDurs"]./1000, s=5.0, c=:red)
-axE1.scatter(jitter_dataEC_spike, dataEC["SpikeDurs"]./1000, s=5.0, c=:blue)
-axE1.scatter(jitter_dataWAVE_spike, dataWAVE["SpikeDurs"]./1000, s=5.0, c=:green)
+datasetSPIKE = [
+     phys_spike_durs./1000, 
+     dataISO["SpikeDurs"]./1000, 
+     dataEC["SpikeDurs"]./1000, 
+     dataWAVE["SpikeDurs"]./1000
+]
+vpSPIKE = axE1.violinplot(datasetSPIKE, widths = 0.9, showmeans = true, showextrema = true)
+for (vp_idx, vp) in enumerate(vpSPIKE["bodies"])
+     vp.set_facecolor(colors[vp_idx])
+     vp.set_edgecolor(:black)
+     vp.set_linewidth(1.0)
+     vp.set_alpha(0.95)
+end
 axE1.set_xticks(pos)
 axE1.set_xticklabels(labels)
-#axE1.bar(labels, [dataPHYS["SpikeDurAvg"], eMSE_ISO, spikeMSE_NG, spikeMSE_WAVE], color=[:black, :red, :blue, :green])
-ylabel("Duration (s)")
-
-jitter_dataPHYS_burst = 0.5 .* rand(length(phys_burst_durs))
-jitter_dataISO_burst = 0.5 .* rand(length(dataISO["BurstDurs"])) .+ 1
-jitter_dataEC_burst = 0.5 .* rand(length(dataEC["BurstDurs"])) .+ 2
-jitter_dataWAVE_burst = 0.5 .* rand(length(dataWAVE["BurstDurs"])) .+ 3
+axE1.set_ylabel("Duration (s)")
 
 axE2 = fig4.add_subplot(gs[5, 2])
-axE2.scatter(jitter_dataPHYS_burst, phys_burst_durs ./ 1000, s=5.0, c=:black)
-axE2.scatter(jitter_dataISO_burst, dataISO["BurstDurs"] ./ 1000, s=5.0, c=:red)
-axE2.scatter(jitter_dataEC_burst, dataEC["BurstDurs"] ./ 1000, s=5.0, c=:blue)
-axE2.scatter(jitter_dataWAVE_burst, dataWAVE["BurstDurs"] ./ 1000, s=5.0, c=:green)
+datasetBURST = [
+     phys_burst_durs ./ 1000,
+     dataISO["BurstDurs"] ./ 1000,
+     dataEC["BurstDurs"] ./ 1000, 
+     dataWAVE["BurstDurs"] ./ 1000
+]
+vpBURST = axE2.violinplot(datasetBURST, widths = 0.9, showmeans = true, showextrema = true)
+for (vp_idx, vp) in enumerate(vpBURST["bodies"])
+     vp.set_facecolor(colors[vp_idx])
+     vp.set_edgecolor(:black)
+     vp.set_linewidth(1.0)
+     vp.set_alpha(0.95)
+end
 axE2.set_xticks(pos)
 axE2.set_xticklabels(labels)
-#axE2.bar(labels, [burstMSE_ISO, burstMSE_NG, burstMSE_WAVE], color=[:red, :blue, :green])
-
-jitter_dataPHYS_ibi = 0.5 .* rand(length(phys_ibis))
-jitter_dataISO_ibi = 0.5 .* rand(length(dataISO["IBIs"])) .+ 1
-jitter_dataEC_ibi = 0.5 .* rand(length(dataEC["IBIs"])) .+ 2
-jitter_dataWAVE_ibi = 0.5 .* rand(length(dataWAVE["IBIs"])) .+ 3
 
 axE3 = fig4.add_subplot(gs[5, 3])
-axE3.scatter(jitter_dataPHYS_ibi, phys_ibis ./ 1000, s=5.0, c=:black)
-axE3.scatter(jitter_dataISO_ibi, dataISO["IBIs"] ./ 1000, s=5.0, c=:red)
-axE3.scatter(jitter_dataEC_ibi, dataEC["IBIs"] ./ 1000, s=5.0, c=:blue)
-axE3.scatter(jitter_dataWAVE_ibi, dataWAVE["IBIs"] ./ 1000, s=5.0, c=:green)
+datasetIBI = [
+     phys_ibis ./ 1000,
+     dataISO["IBIs"] ./ 1000,
+     dataEC["IBIs"] ./ 1000,
+     dataWAVE["IBIs"] ./ 1000,
+]
+vpIBI = axE3.violinplot(datasetIBI, widths = 0.9, showmeans = true, showextrema = true)
+for (vp_idx, vp) in enumerate(vpIBI["bodies"])
+     vp.set_facecolor(colors[vp_idx])
+     vp.set_edgecolor(:black)
+     vp.set_linewidth(1.0)
+     vp.set_alpha(0.95)
+end
 axE3.set_xticks(pos)
 axE3.set_xticklabels(labels)
+
+datasetWAVE = [
+     [0.0], #Phys data is empty
+     wavesWAVE.WaveTime./1000, 
+     wavesISO.WaveTime./1000, 
+     wavesECl.WaveTime./1000
+]
+
+axE4 = fig4.add_subplot(gs[5, 4])
+vpWAVE = axE4.violinplot(datasetWAVE, widths = 0.9, showmeans = true, showextrema = true)
+for (vp_idx, vp) in enumerate(vpWAVE["bodies"])
+     vp.set_facecolor(colors[vp_idx])
+     vp.set_edgecolor(:black)
+     vp.set_linewidth(1.0)
+     vp.set_alpha(0.95)
+end
+axE4.set_xticks(pos)
+axE4.set_xticklabels(labels)
+axE4.set_ylim(-0.1, 10.0)
+#axE4.set_ylabel("Wave Duration(s)")
 #axE3.bar(labels, [IBIMSE_ISO, IBIMSE_NG, IBIMSE_WAVE], color=[:red, :blue, :green])
 
 axE3.annotate("A", (0.01, 0.95), xycoords="figure fraction", annotation_clip=false, fontsize=20.0, fontweight="bold")
@@ -188,55 +292,9 @@ axE3.annotate("D", (0.01, 0.40), xycoords="figure fraction", annotation_clip=fal
 axE3.annotate("E", (0.01, 0.20), xycoords="figure fraction", annotation_clip=false, fontsize=20.0, fontweight="bold")
 
 #%%
-loc = raw"C:\Users\mtarc\The University of Akron\Renna Lab - General\Journal Submissions\2022 A Computational Model - Sci. Rep\Submission 1\Figures"
+loc = raw"C:\Users\mtarc\The University of Akron\Renna Lab - General\Journal Submissions\2022 A Computational Model - Sci. Rep\Submission 2\Figures"
 #loc = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2021 A Computational Model - Sci. Rep\Figures"
 print("[$(now())]: Saving the figure 4...")
 fig4.savefig("$(loc)/Figure6_ModelComparison.png")
 plt.close("all")
 println(" Completed")
-
-#%% Print the results of all the traces
-println("Data Values")
-println("-----------------------------------------------")
-println("""
-
-ISO_n = $(length(iso_baselines))
-ISO Baseline Avg: $(iso_baseline_avg|>x->round(x, digits = 2))±$(iso_baseline_SEM|>x->round(x, digits = 2))
-ISO Minimum Avg: $(iso_min_avg|>x->round(x, digits = 2))±$(iso_min_SEM|>x->round(x, digits = 2))
-ISO Maximum Avg: $(iso_max_avg|>x->round(x, digits = 2))±$(iso_max_SEM|>x->round(x, digits = 2))
-ISO Spike Duration Avg: $(dataISO["SpikeDurAvg"]|>x->round(x, digits = 2))±$(dataISO["SpikeDurSEM"]|>x->round(x, digits = 2))
-ISO Burst Duration Avg: $(dataISO["BurstDurAvg"]|>x->round(x, digits = 2))±$(dataISO["BurstDurSEM"]|>x->round(x, digits = 2))
-ISO IBI_Avg: $(dataISO["IBIAvg"]./1000|>x->round(x, digits = 2))±$(dataISO["IBISEM"]./1000|>x->round(x, digits = 2))
-
-NO GABA n = $(length(ng_baselines))
-NO GABA Baseline Avg: $(ng_baseline_avg|>x->round(x, digits = 2))±$(ng_baseline_SEM|>x->round(x, digits = 2))
-NO GABA Minimum Avg: $(ng_min_avg|>x->round(x, digits = 2))±$(ng_min_SEM|>x->round(x, digits = 2))
-NO GABA Maximum Avg: $(ng_max_avg|>x->round(x, digits = 2))±$(ng_max_SEM|>x->round(x, digits = 2))
-NO GABA Spike Duration Avg: $(dataNG["SpikeDurAvg"]|>x->round(x, digits = 2))±$(dataNG["SpikeDurSEM"]|>x->round(x, digits = 2))
-NO GABA Burst Duration Avg: $(dataNG["BurstDurAvg"]|>x->round(x, digits = 2))±$(dataNG["BurstDurSEM"]|>x->round(x, digits = 2))
-NO GABA IBI_Avg: $(dataNG["IBIAvg"]./1000|>x->round(x, digits = 2))±$(dataNG["IBISEM"]./1000|>x->round(x, digits = 2))
-
-ECl -55 n = $(length(ec_baselines))
-ECl -55 Baseline Avg: $(ec_baseline_avg|>x->round(x, digits = 2))±$(ec_baseline_SEM|>x->round(x, digits = 2))
-ECl -55 Minimum Avg: $(ec_min_avg|>x->round(x, digits = 2))±$(ec_min_SEM|>x->round(x, digits = 2))
-ECl -55 Maximum Avg: $(ec_max_avg|>x->round(x, digits = 2))±$(ec_max_SEM|>x->round(x, digits = 2))
-ECl -55 Spike Duration Avg: $(dataEC["SpikeDurAvg"]|>x->round(x, digits = 2))±$(dataEC["SpikeDurSEM"]|>x->round(x, digits = 2))
-ECl -55 Burst Duration Avg: $(dataEC["BurstDurAvg"]|>x->round(x, digits = 2))±$(dataEC["BurstDurSEM"]|>x->round(x, digits = 2))
-ECl -55 IBI_Avg: $(dataEC["IBIAvg"]./1000|>x->round(x, digits = 2))±$(dataEC["IBISEM"]./1000|>x->round(x, digits = 2))
-
-WAVE n = $(length(wave_baselines))
-WAVE Baseline Avg: $(wave_baseline_avg|>x->round(x, digits = 2))±$(wave_baseline_SEM|>x->round(x, digits = 2))
-WAVE Minimum Avg: $(wave_min_avg|>x->round(x, digits = 2))±$(wave_min_SEM|>x->round(x, digits = 2))
-WAVE Maximum Avg: $(wave_max_avg|>x->round(x, digits = 2))±$(wave_max_SEM|>x->round(x, digits = 2))
-WAVE Spike Duration Avg: $(dataWAVE["SpikeDurAvg"]|>x->round(x, digits = 2))±$(dataWAVE["SpikeDurSEM"]|>x->round(x, digits = 2))
-WAVE Burst Duration Avg: $(dataWAVE["BurstDurAvg"]|>x->round(x, digits = 2))±$(dataWAVE["BurstDurSEM"]|>x->round(x, digits = 2))
-WAVE IBI_Avg: $(dataWAVE["IBIAvg"]./1000|>x->round(x, digits = 2))±$(dataWAVE["IBISEM"]./1000|>x->round(x, digits = 2))
-
-PHYS n = $(length(phys_baseline))
-PHYS Baseline Avg: $(phys_baseline_avg|>x->round(x, digits = 2))±$(phys_baseline_SEM|>x->round(x, digits = 2))
-PHYS Minimum Avg: $(phys_min_avg|>x->round(x, digits = 2))±$(phys_min_SEM|>x->round(x, digits = 2))
-PHYS Maximum Avg: $(phys_max_avg|>x->round(x, digits = 2))±$(phys_max_SEM|>x->round(x, digits = 2))
-PHYS Spike Duration Avg: $(phys_spike_avg|>x->round(x, digits = 2))±$(phys_spike_SEM |>x->round(x, digits = 2))
-PHYS Burst Duration Avg: $(phys_burst_avg|>x->round(x, digits = 2))±$(phys_burst_SEM|>x->round(x, digits = 2))
-PHYS IBI_Avg: $(phys_ibis_avg./1000|>x->round(x, digits = 2))±$(phys_ibis_SEM./1000|>x->round(x, digits = 2))
-""")
